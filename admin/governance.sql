e2908a78edc6cabe8d069d9addd7946a244860a2
thomasyu888 <thomas.yu@sagebase.org>
2023-09-07 16:58:43 -0700

    Add in governance layer

---
+++ admin/governance.sql
@@ -0,0 +1,39 @@
+use role accountadmin;
+
+// https://docs.snowflake.com/en/user-guide/security-column-ddm-use
+
+use role useradmin;
+CREATE ROLE masking_admin;
+use role securityadmin;
+
+GRANT CREATE MASKING POLICY on SCHEMA SYNAPSE_DATA_WAREHOUSE.synapse to ROLE masking_admin;
+GRANT CREATE MASKING POLICY on SCHEMA SYNAPSE_DATA_WAREHOUSE.synapse_raw to ROLE masking_admin;
+
+use role accountadmin;
+GRANT APPLY MASKING POLICY on ACCOUNT to ROLE masking_admin;
+
+GRANT ROLE masking_admin TO USER thomasyu888;
+
+use role masking_admin;
+
+CREATE OR REPLACE MASKING POLICY email_mask AS (val string) returns string ->
+  CASE
+    WHEN current_role() IN ('SYSADMIN') THEN VAL
+    ELSE regexp_replace(val,'.+\@','*****@') -- leave email domain unmasked
+  END;
+
+use database synapse_data_warehouse;
+use schema synapse_raw;
+// ALTER TABLE IF EXISTS userprofilesnapshot MODIFY COLUMN email UNSET MASKING POLICY;
+ALTER TABLE IF EXISTS userprofilesnapshot MODIFY COLUMN email SET MASKING POLICY email_mask;
+use schema synapse;
+use role sysadmin;
+use role public;
+select * from synapse_data_warehouse.synapse.userprofilesnapshot limit 5;
+ALTER VIEW IF EXISTS userprofilesnapshot_raw MODIFY COLUMN email SET MASKING POLICY email_mask;
+
+// Test
+use role public;
+use warehouse compute_org;
+select email from synapse_data_warehouse.test_raw.userprofilesnapshot limit 10;
+select email, count(*) from synapse_data_warehouse.test_raw.userprofilesnapshot group by email;
