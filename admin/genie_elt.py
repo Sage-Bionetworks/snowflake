53a4c68f52d613564243eb98f73fd8926eb2fbdf
thomasyu888 <thomas.yu@sagebase.org>
2023-09-11 20:51:53 -0700

    Clean up GENIE ELT code

--- admin/genie_elt.py
+++ admin/genie_elt.py
@@ -1,26 +1,23 @@
+"""GENIE ELT pipeline"""
 import os
 
 from dotenv import dotenv_values
 from snowflake.snowpark import Session
 import synapseclient
 
-syn = synapseclient.login()
 
-config = dotenv_values("admin/.env")
-
-connection_parameters = {
-    "account": config['snowflake_account'],
-    "user": config['user'],
-    "password": config['password'],
- }
-
-session = Session.builder.configs(connection_parameters).create()
-
-
-def create_table(database: str, schema: str, table: str, stage: str, filepath: str) -> list:
+def create_table(
+        snow_sesh: Session,
+        database: str,
+        schema: str,
+        table: str,
+        stage: str,
+        filepath: str
+    ) -> list:
     """Create a table by uploading a local file to a stage and infering the schema
 
     Args:
+        snow_sesh (Session): Connected Snowflake session
         database (str): Database name
         schema (str): Schema name
         table (str): Table name
@@ -30,16 +27,16 @@ def create_table(database: str, schema: str, table: str, stage: str, filepath: s
     Returns:
         list: List of 10 SQL results
     """
-    session.use_database(database)
-    session.use_schema(schema)
-    session.sql(
+    snow_sesh.use_database(database)
+    snow_sesh.use_schema(schema)
+    snow_sesh.sql(
         f'CREATE TABLE IF NOT EXISTS {table} (demo STRING)'
     ).collect()
 
-    session.sql(
+    snow_sesh.sql(
         f'PUT file://{filepath} @{database}.{schema}.{stage}'
     ).collect()
-    session.sql(
+    snow_sesh.sql(
         f'CREATE OR REPLACE TABLE {table} USING TEMPLATE ('
             'SELECT ARRAY_AGG(OBJECT_CONSTRUCT(*))'
             'WITHIN GROUP (ORDER BY ORDER_ID)'
@@ -48,48 +45,71 @@ def create_table(database: str, schema: str, table: str, stage: str, filepath: s
         ')'
     ).collect()
 
-    session.sql(
+    snow_sesh.sql(
         f'COPY INTO {table} '
         f'FROM @{stage}/{os.path.basename(filepath)} '
         f"file_format = (TYPE=CSV FIELD_DELIMITER='\t' SKIP_HEADER = 1)"
     ).collect()
-    return session.sql(f"select * from {database}.{schema}.{table} limit 10").collect()
+    return snow_sesh.sql(f"select * from {database}.{schema}.{table} limit 10").collect()
 
-releases = syn.getChildren("syn7844529")
-for release in releases:
-    release_name = (release['name']
-        .replace("Release ", "")
-        .replace(".", "_")
-        .replace("-public", "")
-    )
-    release_id = release['id']
-    release_files = syn.getChildren(release_id)
-    release_file_map = {
-        release_file['name']: syn.get(release_file['id'], followLink=True)
-        for release_file in release_files
-        if release_file['name'].startswith("data_clinical")
+
+def main():
+    """GENIE ELT pipeline"""
+    syn = synapseclient.login()
+
+    config = dotenv_values("admin/.env")
+
+    connection_parameters = {
+        "account": config['snowflake_account'],
+        "user": config['user'],
+        "password": config['password'],
     }
 
-    session.use_role("sysadmin")
-    session.use_database("genie")
-    session.sql(
-        f"CREATE SCHEMA IF NOT EXISTS public_{release_name} "
-            "WITH MANAGED ACCESS;"
-    ).collect()
-    session.use_schema(f"public_{release_name}")
-    session.sql(
-        'CREATE TEMPORARY STAGE IF NOT EXISTS RELEASE_FILES'
-    ).collect()
-    session.sql(
-        'CREATE TEMPORARY FILE FORMAT IF NOT EXISTS TSV '
-        "TYPE=CSV FIELD_DELIMITER='\t' RECORD_DELIMITER='\n' "
-        "PARSE_HEADER=TRUE"
-    ).collect()
+    session = Session.builder.configs(connection_parameters).create()
 
-    for release_file_key, release_file_ent in release_file_map.items():
-        tbl_name = release_file_key.replace("data_", "").replace(".txt", "")
-        create_table(database="GENIE",
-                     schema=f"public_{release_name}",
-                     table=tbl_name,
-                     stage="RELEASE_FILES",
-                     filepath=release_file_ent.path)
+    releases = syn.getChildren("syn7844529")
+    for release in releases:
+        release_name = (release['name']
+            .replace("Release ", "")
+            .replace(".", "_")
+            .replace("-public", "")
+        )
+        release_id = release['id']
+        release_files = syn.getChildren(release_id)
+        release_file_map = {
+            release_file['name']: syn.get(release_file['id'], followLink=True)
+            for release_file in release_files
+            if release_file['name'].startswith("data_clinical") and
+            release_file['name'].endswith("txt")
+        }
+
+        session.use_role("sysadmin")
+        session.use_database("genie")
+        session.sql(
+            f"CREATE SCHEMA IF NOT EXISTS public_{release_name} "
+                "WITH MANAGED ACCESS;"
+        ).collect()
+        session.use_schema(f"public_{release_name}")
+        session.sql(
+            'CREATE TEMPORARY STAGE IF NOT EXISTS RELEASE_FILES'
+        ).collect()
+        session.sql(
+            'CREATE TEMPORARY FILE FORMAT IF NOT EXISTS TSV '
+            "TYPE=CSV FIELD_DELIMITER='\t' RECORD_DELIMITER='\n' "
+            "PARSE_HEADER=TRUE"
+        ).collect()
+
+        for release_file_key, release_file_ent in release_file_map.items():
+            tbl_name = release_file_key.replace("data_", "").replace(".txt", "")
+            create_table(
+                snow_sesh=session,
+                database="GENIE",
+                schema=f"public_{release_name}",
+                table=tbl_name,
+                stage="RELEASE_FILES",
+                filepath=release_file_ent.path
+            )
+
+
+if __name__ == "__main__":
+    main()
