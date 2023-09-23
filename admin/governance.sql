11569973e89b7c4ca27e296216df56d2ccc86521
thomasyu888 <thomas.yu@sagebase.org>
2023-09-23 09:38:04 -0700

    Use small warehouse

--- admin/governance.sql
+++ admin/governance.sql
@@ -1,37 +1,25 @@
-use role accountadmin;
 // https://docs.snowflake.com/en/user-guide/security-column-ddm-use
-
 use role useradmin;
-CREATE ROLE masking_admin;
+USE WAREHOUSE COMPUTE_ORG;
+CREATE ROLE IF NOT EXISTS masking_admin;
 use role securityadmin;
-GRANT CREATE MASKING POLICY on SCHEMA SYNAPSE_DATA_WAREHOUSE.synapse
-to ROLE masking_admin;
-GRANT CREATE MASKING POLICY on SCHEMA SYNAPSE_DATA_WAREHOUSE.synapse_raw
-to ROLE masking_admin;
-
-use role accountadmin;
-GRANT APPLY MASKING POLICY on ACCOUNT
-to ROLE masking_admin;
 GRANT ROLE masking_admin
-TO USER thomasyu888;
-
+TO ROLE useradmin;
 GRANT ROLE masking_admin
 TO USER "thomas.yu@sagebase.org";
+GRANT CREATE MASKING POLICY ON SCHEMA SYNAPSE_DATA_WAREHOUSE.synapse
+TO ROLE masking_admin;
+USE ROLE accountadmin;
+GRANT APPLY MASKING POLICY on ACCOUNT
+TO ROLE masking_admin;
 use role masking_admin;
-CREATE OR REPLACE MASKING POLICY email_mask AS (val string) returns string ->
+use database synapse_data_warehouse;
+use schema synapse;
+CREATE MASKING POLICY IF NOT EXISTS email_mask AS (val string) returns string ->
   CASE
     WHEN current_role() IN ('SYSADMIN') THEN VAL
     ELSE regexp_replace(val,'.+\@','*****@') -- leave email domain unmasked
   END;
-
-use database synapse_data_warehouse;
-use schema synapse;
 ALTER TABLE IF EXISTS userprofile_latest
-MODIFY COLUMN email
-SET MASKING POLICY email_mask;
-
-use database synapse_data_warehouse;
-use schema synapse_raw;
-ALTER TABLE IF EXISTS userprofilesnapshot_raw
 MODIFY COLUMN email
 SET MASKING POLICY email_mask;
