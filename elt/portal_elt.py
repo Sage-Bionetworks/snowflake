c01669b06afb38d8cd97823f3919c675dad20e03
Thomas Yu <thomas.yu@sagebase.org>
2023-09-24 18:03:59 -0700

    Add GENIE file view

--- elt/portal_elt.py
+++ elt/portal_elt.py
@@ -25,7 +25,8 @@ cs = ctx.cursor()
 portals = {
     "AD": "syn11346063",
     "PSYCHENCODE": "syn20821313.16",
-    "NF": "syn16858331"
+    "NF": "syn16858331",
+    "GENIE": "syn23625806"
 }
 for portal_name, synapse_id in portals.items():
     portal = syn.tableQuery(f"select * from {synapse_id}")
