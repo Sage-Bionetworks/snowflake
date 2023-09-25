367f5281a90cfc6bb0cf0c0d3a4d2d29c8e906c9
Thomas Yu <thomas.yu@sagebase.org>
2023-09-24 21:31:00 -0700

    Add in portal ELT

--- elt/portal_elt.py
+++ elt/portal_elt.py
@@ -1,12 +1,9 @@
 from dotenv import dotenv_values
-import pandas as pd
 import snowflake.connector
 from snowflake.connector.pandas_tools import write_pandas
 import synapseclient
 
-# SELECT id, parentId, createdOn, name, fileFormat, resourceType, dataSubtype, modifiedBy, individualID, modifiedOn, benefactorId, createdBy, libraryPrep FROM syn16858331
-# UNION SELECT id, parentId, createdOn, name, fileFormat, resourceType, dataSubtype, modifiedBy, individualID, modifiedOn, benefactorId, createdBy, libraryPrep FROM syn11346063
-# UNION SELECT id, parentId, createdOn, name, fileFormat, resourceType, dataSubtype, modifiedBy, individualID, modifiedOn, benefactorId, createdBy, libraryPrep FROM syn20821313.15
+
 syn = synapseclient.login()
 
 config = dotenv_values(".env")
@@ -44,31 +41,39 @@ for portal_name, synapse_id in portals.items():
     # If the table is empty, auto create it, otherwise, truncake and overwrite
     # The rationale for this is that some tables have "grant" and "group" as
     # and those are reserved column headers.
-    auto_create_table = opt.empty
-    write_pandas(ctx, portal_df, portal_name, auto_create_table=auto_create_table, overwrite=True)
+    # auto_create_table = opt.empty
+    # write_pandas(ctx, portal_df, portal_name, auto_create_table=auto_create_table, overwrite=True)
 
-    # if opt.empty:
-    #     write_pandas(ctx, portal_df, portal_name, auto_create_table=True)
-    # else:
-    #     # Create temporary table so we can upsert
-    #     target_table = f"{portal_name}_TEMP"
-    #     write_pandas(ctx, portal_df, target_table, auto_create_table=True, table_type="transient")
-    #     # Upsert into non-temporary tables
-    #     upset_set = [f'"{target_table}"."{col}" = "{portal_name}"."{col}"' for col in portal_df.columns]
-    #     upset_set_str = ",".join(upset_set)
-    #     col_str = ",".join(f'"{col}"' for col in portal_df.columns)
-    #     src_value_str = ",".join([f'"{portal_name}"."{col}"' for col in portal_df.columns])
-    #     merge_sql = f"""
-    #     MERGE INTO {portal_name} USING {target_table}
-    #         ON {portal_name}.id = {target_table}.id
-    #         when matched then
-    #             update set {upset_set_str}
-    #         when not matched then
-    #         insert
-    #         ({col_str}) values({src_value_str})
-    #     """
-    #     print(merge_sql)
-    #     cs.execute(merge_sql)
+    if opt.empty:
+        write_pandas(ctx, portal_df, portal_name, auto_create_table=True)
+    else:
+        # Create temporary table so we can upsert
+        target_table = f"{portal_name}_TEMP"
+        write_pandas(
+            ctx,
+            portal_df,
+            target_table,
+            auto_create_table=True,
+            table_type="transient",
+            overwrite=True
+        )
+        # Upsert into non-temporary tables
+        update_set = [f'"{portal_name}"."{col}" = "{target_table}"."{col}"' for col in portal_df.columns]
+        update_set_str = ",".join(update_set)
+        col_str = ",".join(f'"{col}"' for col in portal_df.columns)
+        to_insert_str = ",".join([f'"{target_table}"."{col}"' for col in portal_df.columns])
+        merge_sql = f"""
+        MERGE INTO {portal_name} USING {target_table}
+            ON {portal_name}."id" = {target_table}."id"
+            when matched then
+                update set {update_set_str}
+            when not matched then
+            insert
+            ({col_str}) values({to_insert_str});
+        """
+        print(merge_sql)
+        cs.execute(merge_sql)
+        cs.execute(f"DROP TABLE {target_table}")
 
     query = f"select * from {portal_name} limit 10;"
     cs.execute(query)
