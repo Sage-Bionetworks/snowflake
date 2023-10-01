8c6492abd9170bf7922cf0cd7d95ba268fff1a6b
Thomas Yu <thomas.yu@sagebase.org>
2023-10-01 13:07:57 -0700

    Copy in processed access record

--- elt/synapse_raw_setup.sql
+++ elt/synapse_raw_setup.sql
@@ -192,6 +192,79 @@ from (
 pattern='.*certifiedquizquestionrecords/record_date=.*/.*'
 ;
 
+
+CREATE TABLE IF NOT EXISTS processedaccess (
+	session_id STRING,
+    timestamp TIMESTAMP,
+    user_id NUMBER,
+    method STRING,
+    request_url STRING,
+    user_agent STRING,
+    host STRING,
+    origin STRING,
+	  x_forwarded_for STRING,
+    via STRING,
+    thread_id NUMBER,
+    elapse_ms NUMBER,
+    success BOOLEAN,
+    stack STRING,
+    instance STRING,
+    vm_id STRING,
+    return_object_id STRING,
+    query_string STRING,
+    response_status NUMBER,
+    oauth_client_id STRING,
+    basic_auth_username STRING,
+    auth_method STRING,
+    normalized_method_signature STRING,
+    client STRING,
+    client_version STRING,
+    entity_id NUMBER,
+    record_date DATE
+);
+
+USE WAREHOUSE COMPUTE_MEDIUM;
+
+// Follow this blog https://www.snowflake.com/blog/how-to-load-terabytes-into-snowflake-speeds-feeds-and-techniques/#:~:text=Best%20Practices%20for%20Parquet%20and%20ORC
+copy into processedaccess from (
+  select
+     $1:session_id as session_id,
+     $1:timestamp as timestamp,
+     $1:user_id as user_id,
+     $1:method as method,
+     $1:request_url as request_url,
+     $1:user_agent as user_agent,
+     $1:host as host,
+     $1:origin as origin,
+     $1:x_forwarded_for as x_forwarded_for,
+     $1:via as via,
+     $1:thread_id as thread_id,
+     $1:elapse_ms as elapse_ms,
+     $1:success as success,
+     $1:stack as stack,
+     $1:instance as instance,
+     $1:vm_id as vm_id,
+     $1:return_object_id as return_object_id,
+     $1:query_string as query_string,
+     $1:response_status as response_status,
+     $1:oauth_client_id as oauth_client_id,
+     $1:basic_auth_username as basic_auth_username,
+     $1:auth_method as auth_method,
+     $1:normalized_method_signature as normalized_method_signature,
+     $1:client as client,
+     $1:client_version as client_version,
+     $1:entity_id as entity_id,
+     NULLIF(
+       regexp_replace (
+       METADATA$FILENAME,
+       '^processedaccessrecord\/record_date\=(.*)\/.*',
+       '\\1'),
+       '__HIVE_DEFAULT_PARTITION__'
+     )                         as record_date
+   from @my_test_s3_stage/processedaccessrecord)
+   pattern='.*processedaccessrecord/record_date=.*/.*'
+;
+
 create table filedownload (
     timestamp TIMESTAMP,
     user_id NUMBER,
@@ -385,74 +458,4 @@ copy into filesnapshots from (
      )                         as snapshot_date
    from @filesnapshots/)
    pattern='.*snapshot_date=.*/.*'
-;
-
-CREATE TABLE IF NOT EXISTS processedaccess (
-	session_id STRING,
-    timestamp TIMESTAMP,
-    user_id NUMBER,
-    method STRING,
-    request_url STRING,
-    user_agent STRING,
-    host STRING,
-    origin STRING,
-	  x_forwarded_for STRING,
-    via STRING,
-    thread_id NUMBER,
-    elapse_ms NUMBER,
-    success BOOLEAN,
-    stack STRING,
-    instance STRING,
-    vm_id STRING,
-    return_object_id STRING,
-    query_string STRING,
-    response_status NUMBER,
-    oauth_client_id STRING,
-    basic_auth_username STRING,
-    auth_method STRING,
-    normalized_method_signature STRING,
-    client STRING,
-    client_version STRING,
-    entity_id NUMBER,
-    record_date DATE
-);
-
-// Follow this blog https://www.snowflake.com/blog/how-to-load-terabytes-into-snowflake-speeds-feeds-and-techniques/#:~:text=Best%20Practices%20for%20Parquet%20and%20ORC
-copy into processedaccess from (
-  select
-     $1:session_id as session_id,
-     $1:timestamp as timestamp,
-     $1:user_id as user_id,
-     $1:method as method,
-     $1:request_url as request_url,
-     $1:user_agent as user_agent,
-     $1:host as host,
-     $1:origin as origin,
-     $1:x_forwarded_for as x_forwarded_for,
-     $1:via as via,
-     $1:thread_id as thread_id,
-     $1:elapse_ms as elapse_ms,
-     $1:success as success,
-     $1:stack as stack,
-     $1:instance as instance,
-     $1:vm_id as vm_id,
-     $1:return_object_id as return_object_id,
-     $1:query_string as query_string,
-     $1:response_status as response_status,
-     $1:oauth_client_id as oauth_client_id,
-     $1:basic_auth_username as basic_auth_username,
-     $1:auth_method as auth_method,
-     $1:normalized_method_signature as normalized_method_signature,
-     $1:client as client,
-     $1:client_version as client_version,
-     $1:entity_id as entity_id,
-     NULLIF(
-       regexp_replace (
-       METADATA$FILENAME,
-       '.*\=(.*)\/.*',
-       '\\1'),
-       '__HIVE_DEFAULT_PARTITION__'
-     )                         as record_date
-   from @processedaccess/)
-   pattern='.*record_date=.*/.*'
 ;
