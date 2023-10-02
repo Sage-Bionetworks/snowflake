9dd332d2ca05685377539688dbc721427d74b400
Thomas Yu <thomas.yu@sagebase.org>
2023-10-02 10:21:43 -0700

    Add email mask policy

--- admin/policies.sql
+++ admin/policies.sql
@@ -1,6 +1,8 @@
 // https://docs.snowflake.com/en/user-guide/security-column-ddm-use
 use role masking_admin;
-CREATE MASKING POLICY IF NOT EXISTS synapse_data_warehouse.synapse.email_mask AS (val string) returns string ->
+USE DATABASE synapse_data_warehouse;
+USE SCHEMA synapse;
+CREATE MASKING POLICY IF NOT EXISTS email_mask AS (val string) returns string ->
   CASE
     WHEN current_role() IN ('SYSADMIN') THEN VAL
     ELSE regexp_replace(val,'.+\@','*****@') -- leave email domain unmasked
