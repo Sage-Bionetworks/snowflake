3c444971eb38ab3cd55ccdef98a362eb474b02bf
Thomas Yu <thomas.yu@sagebase.org>
2023-10-13 00:36:58 -0700

    Add in scheduled task for file downloads

--- elt/synapse_elt.sql
+++ elt/synapse_elt.sql
@@ -138,6 +138,37 @@ pattern='.*certifiedquizquestionrecords/record_date=.*/.*'
 ;
 ALTER TASK certifiedquizquestion_task RESUME;
 
+CREATE OR REPLACE TASK filedownload_task
+    SCHEDULE = 'USING CRON 0 0 * * * America/Los_Angeles'
+    USER_TASK_MANAGED_INITIAL_WAREHOUSE_SIZE = 'SMALL'
+AS
+copy into
+  filedownload
+from (
+  select
+     $1:timestamp as timestamp,
+     $1:user_id as user_id,
+     $1:project_id as project_id,
+     $1:file_handle_id as file_handle_id,
+     $1:downloaded_file_handle_id as downloaded_file_handle_id,
+     $1:association_object_id as association_object_id,
+     $1:association_object_type as association_object_type,
+     $1:stack as stack,
+     $1:instance as instance,
+     NULLIF(
+       regexp_replace (
+       METADATA$FILENAME,
+       '.*filedownloadrecords\/record_date\=(.*)\/.*',
+       '\\1'),
+       '__HIVE_DEFAULT_PARTITION__'
+     )                         as record_date
+  from
+    @synapse_prod_warehouse_s3_stage/filedownloadrecords
+  )
+pattern='.*filedownloadrecords/record_date=.*/.*'
+;
+ALTER TASK filedownload_task RESUME;
+
 // zero copy clone of processed access records
 CREATE OR REPLACE TABLE synapse_data_warehouse.synapse.processedaccess
 CLONE synapse_data_warehouse.synapse_raw.processedaccess;
