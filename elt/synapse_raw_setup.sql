a48e0fa86bfedea2272a67fd7f98eab5c3f2a031
Thomas Yu <thomas.yu@sagebase.org>
2023-10-12 22:27:40 -0700

    Executed initial commands to load data

--- elt/synapse_raw_setup.sql
+++ elt/synapse_raw_setup.sql
@@ -241,82 +241,6 @@ from (
 pattern='.*certifiedquizquestionrecords/record_date=.*/.*'
 ;
 
-CREATE TABLE IF NOT EXISTS processedaccess (
-	session_id STRING,
-  timestamp TIMESTAMP,
-  user_id NUMBER,
-  method STRING,
-  request_url STRING,
-  user_agent STRING,
-  host STRING,
-  origin STRING,
-  x_forwarded_for STRING,
-  via STRING,
-  thread_id NUMBER,
-  elapse_ms NUMBER,
-  success BOOLEAN,
-  stack STRING,
-  instance STRING,
-  vm_id STRING,
-  return_object_id STRING,
-  query_string STRING,
-  response_status NUMBER,
-  oauth_client_id STRING,
-  basic_auth_username STRING,
-  auth_method STRING,
-  normalized_method_signature STRING,
-  client STRING,
-  client_version STRING,
-  entity_id NUMBER,
-  record_date DATE
-)
-CLUSTER BY (record_date);
-
--- TODO Processed access doesn't work for some reason
-copy into processedaccess from (
-  select
-     $1:session_id as session_id,
-     $1:timestamp as timestamp,
-     $1:user_id as user_id,
-     $1:method as method,
-     $1:request_url as request_url,
-     $1:user_agent as user_agent,
-     $1:host as host,
-     $1:origin as origin,
-     $1:x_forwarded_for as x_forwarded_for,
-     $1:via as via,
-     $1:thread_id as thread_id,
-     $1:elapse_ms as elapse_ms,
-     $1:success as success,
-     $1:stack as stack,
-     $1:instance as instance,
-     $1:vm_id as vm_id,
-     $1:return_object_id as return_object_id,
-     $1:query_string as query_string,
-     $1:response_status as response_status,
-     $1:oauth_client_id as oauth_client_id,
-     $1:basic_auth_username as basic_auth_username,
-     $1:auth_method as auth_method,
-     $1:normalized_method_signature as normalized_method_signature,
-     $1:client as client,
-     $1:client_version as client_version,
-     $1:entity_id as entity_id,
-     NULLIF(
-       regexp_replace (
-       METADATA$FILENAME,
-       '.*processaccessrecord\/record_date\=(.*)\/.*',
-       '\\1'),
-       '__HIVE_DEFAULT_PARTITION__'
-     )                         as record_date
-   from @synapse_prod_warehouse_s3_stage/processaccessrecord)
-   pattern='.*processaccessrecord/record_date=.*/.*'
-;
--- TODO Processed access doesn't work for some reason
-
-LIST '@synapse_prod_warehouse_s3_stage'
-PATTERN = '.*process.*';
--- TODO processed access record
-
 CREATE TABLE IF NOT EXISTS filedownload (
     timestamp TIMESTAMP,
     user_id NUMBER,
@@ -539,7 +463,6 @@ copy into teammembersnapshots from (
   )
 pattern='.*teammembersnapshots/snapshot_date=.*/.*'
 ;
--- TODO process below
 
 CREATE TABLE IF NOT EXISTS fileupload (
 	timestamp TIMESTAMP,
@@ -633,3 +556,79 @@ copy into filesnapshots from (
   )
 pattern='.*filesnapshots/snapshot_date=.*/.*'
 ;
+
+
+CREATE TABLE IF NOT EXISTS processedaccess (
+	session_id STRING,
+  timestamp TIMESTAMP,
+  user_id NUMBER,
+  method STRING,
+  request_url STRING,
+  user_agent STRING,
+  host STRING,
+  origin STRING,
+  x_forwarded_for STRING,
+  via STRING,
+  thread_id NUMBER,
+  elapse_ms NUMBER,
+  success BOOLEAN,
+  stack STRING,
+  instance STRING,
+  vm_id STRING,
+  return_object_id STRING,
+  query_string STRING,
+  response_status NUMBER,
+  oauth_client_id STRING,
+  basic_auth_username STRING,
+  auth_method STRING,
+  normalized_method_signature STRING,
+  client STRING,
+  client_version STRING,
+  entity_id NUMBER,
+  record_date DATE
+)
+CLUSTER BY (record_date);
+
+-- TODO Processed access no longer exists in warehouse?
+copy into processedaccess from (
+  select
+    $1:session_id as session_id,
+    $1:timestamp as timestamp,
+    $1:user_id as user_id,
+    $1:method as method,
+    $1:request_url as request_url,
+    $1:user_agent as user_agent,
+    $1:host as host,
+    $1:origin as origin,
+    $1:x_forwarded_for as x_forwarded_for,
+    $1:via as via,
+    $1:thread_id as thread_id,
+    $1:elapse_ms as elapse_ms,
+    $1:success as success,
+    $1:stack as stack,
+    $1:instance as instance,
+    $1:vm_id as vm_id,
+    $1:return_object_id as return_object_id,
+    $1:query_string as query_string,
+    $1:response_status as response_status,
+    $1:oauth_client_id as oauth_client_id,
+    $1:basic_auth_username as basic_auth_username,
+    $1:auth_method as auth_method,
+    $1:normalized_method_signature as normalized_method_signature,
+    $1:client as client,
+    $1:client_version as client_version,
+    $1:entity_id as entity_id,
+    NULLIF(
+      regexp_replace (
+      METADATA$FILENAME,
+      '.*processaccessrecord\/record_date\=(.*)\/.*',
+      '\\1'),
+      '__HIVE_DEFAULT_PARTITION__'
+    )                         as record_date
+  from @synapse_prod_warehouse_s3_stage/processaccessrecord)
+  pattern='.*processaccessrecord/record_date=.*/.*'
+;
+-- TODO Processed access doesn't work for some reason
+
+LIST '@synapse_prod_warehouse_s3_stage'
+PATTERN = '.*process.*';
