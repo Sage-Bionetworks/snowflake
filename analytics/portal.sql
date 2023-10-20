221e5b54b06fcc942102054f7a1cf6a566976d65
Thomas Yu <thomas.yu@sagebase.org>
2023-10-20 10:19:48 -0700

    Separate out table creation code from analytics code

--- analytics/portal.sql
+++ analytics/portal.sql
@@ -1,333 +1,171 @@
-USE ROLE SYSADMIN;
+USE ROLE PUBLIC;
 USE DATABASE SYNAPSE_DATA_WAREHOUSE;
 USE SCHEMA SYNAPSE;
 -- Data up to October 18th for now
 
 -- Total number of downloads in synapse
-WITH dedup_filehandle AS (
-    select
-        distinct user_id, file_handle_id as fd_file_handle_id, record_date
-    from
-        synapse_data_warehouse.synapse.filedownload
+WITH DEDUP_FILEHANDLE AS (
+    SELECT DISTINCT
+        USER_ID,
+        FILE_HANDLE_ID AS FD_FILE_HANDLE_ID,
+        RECORD_DATE
+    FROM
+        SYNAPSE_DATA_WAREHOUSE.SYNAPSE.FILEDOWNLOAD
 )
-select
-    count(*)
-from
-    dedup_filehandle;
-WITH dedup_filehandle AS (
-    select
-        distinct user_id, file_handle_id as fd_file_handle_id, record_date
-    from
-        synapse_data_warehouse.synapse.filedownload
+
+SELECT count(*)
+FROM
+    DEDUP_FILEHANDLE;
+
+-- Download count per month in Synapse
+
+WITH DEDUP_FILEHANDLE AS (
+    SELECT DISTINCT
+        USER_ID,
+        FILE_HANDLE_ID AS FD_FILE_HANDLE_ID,
+        RECORD_DATE
+    FROM
+        SYNAPSE_DATA_WAREHOUSE.SYNAPSE.FILEDOWNLOAD
 )
-select
-    DATE_TRUNC('MONTH', record_date) as month,
-    count(*) as number_of_downloads
-from
-    dedup_filehandle
-group by
-    month
-order by
-    month DESC;
--- * Number of files within each portal in snowflake
-USE SCHEMA sage.portal_raw;
+
 SELECT
-    table_name,
-    row_count
+    date_trunc('MONTH', RECORD_DATE) AS MONTH,
+    count(*) AS NUMBER_OF_DOWNLOADS
+FROM
+    DEDUP_FILEHANDLE
+GROUP BY
+    MONTH
+ORDER BY
+    MONTH DESC;
+-- * Number of files within each portal in snowflake
+USE SCHEMA SAGE.PORTAL_RAW;
+SELECT
+    TABLE_NAME,
+    ROW_COUNT
 FROM
     INFORMATION_SCHEMA.TABLES
 WHERE
     TABLE_SCHEMA = 'PORTAL_RAW'
-order by
-    row_count DESC;
+ORDER BY
+    ROW_COUNT DESC;
 
 -- When do file download records begin?
-select
-    distinct record_date
-from
-    synapse_data_warehouse.synapse_raw.filedownload
-order by
-    record_date ASC;
+SELECT DISTINCT RECORD_DATE
+FROM
+    SYNAPSE_DATA_WAREHOUSE.SYNAPSE_RAW.FILEDOWNLOAD
+ORDER BY
+    RECORD_DATE ASC;
 
 -- * Metrics for AD portal
-CREATE TABLE ad_downloads AS (
-with dedup_filedownload AS (
-    select
-        distinct user_id, file_handle_id as fd_file_handle_id, record_date
-    from
-        synapse_data_warehouse.synapse.filedownload
-)
-select
-    *
-from
-    sage.portal_raw.ad
-LEFT JOIN
-    dedup_filedownload fd
-ON
-    ad."dataFileHandleId" = fd.fd_file_handle_id
-);
+USE SCHEMA SAGE.PORTAL_DOWNLOADS;
 -- Total download count for AD portal
-select
-    count(*)
-from
-    ad_downloads;
+SELECT count(*)
+FROM
+    AD_DOWNLOADS;
 -- distribution of AD portal downloads per month
-select
-    DATE_TRUNC('MONTH', record_date) as month,
-    count(*) as number_of_downloads
-from
-    ad_downloads
-group by
-    month
-order by
-    month DESC;
+SELECT
+    date_trunc('MONTH', RECORD_DATE) AS MONTH,
+    count(*) AS NUMBER_OF_DOWNLOADS
+FROM
+    AD_DOWNLOADS
+GROUP BY
+    MONTH
+ORDER BY
+    MONTH DESC;
 
-select
-    count(distinct user_id)
-from 
-    ad_downloads;
+SELECT count(DISTINCT USER_ID)
+FROM
+    AD_DOWNLOADS;
 
 -- * GENIE
 -- All download counts over time
-CREATE TABLE genie_downloads AS (
-with dedup_filedownload AS (
-    select
-        distinct user_id, file_handle_id as fd_file_handle_id, record_date
-    from
-        synapse_data_warehouse.synapse.filedownload
-)
-select
-    *
-from
-    sage.portal_raw.genie genie
-LEFT JOIN
-    dedup_filedownload fd
-ON
-    genie."dataFileHandleId" = fd.fd_file_handle_id
-);
-select
-    count(*)
-from
-    genie_downloads;
-
-select
-    DATE_TRUNC('MONTH', record_date) as month,
-    count(*) as number_of_downloads
-from
-    genie_downloads
-group by
-    month
-order by
-    month DESC;
-
-select
-    count(distinct user_id)
-from 
-    genie_downloads;
--- * ELITE
-CREATE TABLE elite_downloads AS (
-WITH elite_transform AS (
-    SELECT
-        *, CAST(REPLACE("id", 'syn', '') AS INTEGER) as syn_id
-    FROM
-        sage.portal_raw.elite
-),
-dedup_filedownload AS (
-    select
-        distinct user_id, file_handle_id as fd_file_handle_id, record_date
-    from
-        synapse_data_warehouse.synapse.filedownload
-),
-download_count AS (
-    SELECT *
-    FROM elite_transform
-    LEFT JOIN
-        synapse_data_warehouse.synapse.node_latest node_latest
-    ON
-        elite_transform.syn_id = node_latest.id
-    INNER JOIN
-        dedup_filedownload fd
-    ON
-        node_latest.file_handle_id = fd.fd_file_handle_id
-)
-SELECT
-    *
+SELECT count(*)
 FROM
-    download_count
-);
+    GENIE_DOWNLOADS;
 
-select
-    count(*)
-from    
-    elite_downloads;
-select
-    DATE_TRUNC('MONTH', record_date) as month,
-    count(*) as number_of_downloads
-from
-    elite_downloads
-group by
-    month
-order by
-    month DESC;
+-- Distribution of downloads over months
+SELECT
+    date_trunc('MONTH', RECORD_DATE) AS MONTH,
+    count(*) AS NUMBER_OF_DOWNLOADS
+FROM
+    GENIE_DOWNLOADS
+GROUP BY
+    MONTH
+ORDER BY
+    MONTH DESC;
 
-select
-    count(distinct user_id)
-from 
-    elite_downloads;
+-- Number of unique users downloading the data
+SELECT count(DISTINCT USER_ID)
+FROM
+    GENIE_DOWNLOADS;
+-- * ELITE
+SELECT count(*)
+FROM
+    ELITE_DOWNLOADS;
+SELECT
+    date_trunc('MONTH', RECORD_DATE) AS MONTH,
+    count(*) AS NUMBER_OF_DOWNLOADS
+FROM
+    ELITE_DOWNLOADS
+GROUP BY
+    MONTH
+ORDER BY
+    MONTH DESC;
+
+SELECT count(DISTINCT USER_ID)
+FROM
+    ELITE_DOWNLOADS;
 
 -- * NF
--- Total downloads
-CREATE TABLE nf_downloads AS (
-WITH nf_transform AS (
-    SELECT
-        *, CAST(REPLACE("id", 'syn', '') AS INTEGER) as syn_id
-    FROM
-        sage.portal_raw.nf
-),
-dedup_filedownload AS (
-    select
-        distinct user_id, file_handle_id as fd_file_handle_id, record_date
-    from
-        synapse_data_warehouse.synapse.filedownload
-),
-download_count AS (
-    SELECT
-        *
-    FROM
-        nf_transform
-    LEFT JOIN
-        synapse_data_warehouse.synapse.node_latest node_latest
-    ON
-        nf_transform.syn_id = node_latest.id
-    INNER JOIN
-        dedup_filedownload fd
-    ON
-        node_latest.file_handle_id = fd.fd_file_handle_id
-)
-SELECT
-    *
+-- Total download
+SELECT count(*)
 FROM
-    download_count
-);
-
-select
-    count(*)
-from
-    nf_downloads;
-select
-    DATE_TRUNC('MONTH', record_date) as month,
-    count(*) as number_of_downloads
-from
-    nf_downloads
-group by
-    month
-order by
-    month DESC;
-select
-    count(distinct user_id)
-from 
-    nf_downloads;
+    NF_DOWNLOADS;
+SELECT
+    date_trunc('MONTH', RECORD_DATE) AS MONTH,
+    count(*) AS NUMBER_OF_DOWNLOADS
+FROM
+    NF_DOWNLOADS
+GROUP BY
+    MONTH
+ORDER BY
+    MONTH DESC;
+SELECT count(DISTINCT USER_ID)
+FROM
+    NF_DOWNLOADS;
 
 -- psychencode
-CREATE TABLE psychencode_downloads AS (
-WITH psychencode_transform AS (
-    SELECT
-        *, CAST(REPLACE("id", 'syn', '') AS INTEGER) as syn_id
-    FROM
-        sage.portal_raw.psychencode
-),
-dedup_filedownload AS (
-    select
-        distinct user_id, file_handle_id as fd_file_handle_id, record_date
-    from
-        synapse_data_warehouse.synapse.filedownload
-),
-download_count AS (
-    SELECT *
-    FROM
-        psychencode_transform
-    LEFT JOIN
-        synapse_data_warehouse.synapse.node_latest node_latest
-    ON
-        psychencode_transform.syn_id = node_latest.id
-    INNER JOIN
-        dedup_filedownload fd
-    ON
-        node_latest.file_handle_id = fd.fd_file_handle_id
-)
-SELECT
-    *
+SELECT count(*)
 FROM
-    download_count
-);
-select
-    count(*)
-from
-    psychencode_downloads;
+    PSYCHENCODE_DOWNLOADS;
 
-select
-    DATE_TRUNC('MONTH', record_date) as month,
-    count(*) as number_of_downloads
-from
-    psychencode_downloads
-group by
-    month
-order by
-    month DESC;
-select
-    count(distinct user_id)
-from 
-    psychencode_downloads;
+SELECT
+    date_trunc('MONTH', RECORD_DATE) AS MONTH,
+    count(*) AS NUMBER_OF_DOWNLOADS
+FROM
+    PSYCHENCODE_DOWNLOADS
+GROUP BY
+    MONTH
+ORDER BY
+    MONTH DESC;
+SELECT count(DISTINCT USER_ID)
+FROM
+    PSYCHENCODE_DOWNLOADS;
 -- HTAN
-CREATE TABLE htan_downloads AS (
-WITH htan_transform AS (
-    SELECT
-        *, CAST(REPLACE("entityId", 'syn', '') AS INTEGER) as syn_id
-    FROM
-        sage.portal_raw.htan
-),
-dedup_filedownload AS (
-    select
-        distinct user_id, file_handle_id as fd_file_handle_id, record_date
-    from
-        synapse_data_warehouse.synapse.filedownload
-),
-download_count AS (
-    SELECT
-        *
-    FROM
-        htan_transform
-    LEFT JOIN
-        synapse_data_warehouse.synapse.node_latest node_latest
-    ON
-        htan_transform.syn_id = node_latest.id
-    INNER JOIN
-        dedup_filedownload fd
-    ON
-        node_latest.file_handle_id = fd.fd_file_handle_id
-)
-SELECT
-    *
+SELECT count(*)
 FROM
-    download_count
-);
+    HTAN_DOWNLOADS;
 
-select
-    count(*)
-from
-    htan_downloads;
+SELECT
+    date_trunc('MONTH', RECORD_DATE) AS MONTH,
+    count(*) AS NUMBER_OF_DOWNLOADS
+FROM
+    HTAN_DOWNLOADS
+GROUP BY
+    MONTH
+ORDER BY
+    MONTH DESC;
 
-select
-    DATE_TRUNC('MONTH', record_date) as month,
-    count(*) as number_of_downloads
-from
-    htan_downloads
-group by
-    month
-order by
-    month DESC;
-
-select
-    count(distinct user_id)
-from 
-    htan_downloads;
+SELECT count(DISTINCT USER_ID)
+FROM
+    HTAN_DOWNLOADS;
