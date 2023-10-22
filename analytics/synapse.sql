3e6b0b0442ea99c0f4db12da7eb1e35df983f351
Thomas Yu <thomas.yu@sagebase.org>
2023-10-21 17:05:15 -0700

    Find out more about mp4s

--- analytics/synapse.sql
+++ analytics/synapse.sql
@@ -2,19 +2,50 @@ USE ROLE PUBLIC;
 USE WAREHOUSE COMPUTE_ORG;
 USE DATABASE synapse_data_warehouse;
 
-SELECT distinct(FILE_NAME)
-FROM synapse_data_warehouse.synapse_raw.filesnapshots;
-
 // File extensions
+select * from synapse_data_warehouse.synapse.node_latest limit 10;
 with file_extensions as (
-    select split_part(FILE_NAME,'.',-1) as fileext
-    from synapse_data_warehouse.synapse.file_latest
+    select split_part(name,'.',-1) as fileext
+    from synapse_data_warehouse.synapse.node_latest
 )
 select fileext, count(*) AS number_of_files
 from file_extensions
 group by fileext
 ORDER BY number_of_files DESC;
 
+-- TODO: Can you extract which projects have these mp4's and the folder
+-- names under which they are stored, as a next step?
+with file_extensions as (
+    select
+        project_id,
+        parent_id,
+        split_part(name,'.',-1) as fileext
+    from
+        synapse_data_warehouse.synapse.node_latest
+)
+select
+    distinct project_id
+from
+    file_extensions
+where
+    fileext ilike 'MP4';
+
+with file_extensions as (
+    select
+        project_id,
+        parent_id,
+        split_part(name,'.',-1) as fileext
+    from
+        synapse_data_warehouse.synapse.node_latest
+)
+select
+    project_id, count(*) as number_of_mp4s
+from
+    file_extensions
+where
+    fileext ilike 'MP4'
+group by project_id
+order by number_of_mp4s DESC;
 
 // Number of change events
 SELECT CHANGE_TYPE, count(*) as number_of_events
@@ -27,5 +58,3 @@ SELECT STATUS, count(*)
 FROM synapse_data_warehouse.synapse.file_latest
 GROUP BY STATUS;
 
--- TODO: Can you extract which projects have these mp4's and the folder
--- names under which they are stored, as a next step?
