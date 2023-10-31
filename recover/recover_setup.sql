9cd7d1af15504f753850aeacf9fc10b7afc970a4
Thomas Yu <thomas.yu@sagebase.org>
2023-10-31 00:28:40 -0700

    Add grants, add sage and recover folders, and add paths to ignore

--- recover/recover_setup.sql
+++ recover/recover_setup.sql
@@ -3,31 +3,14 @@ CREATE SCHEMA IF NOT EXISTS pilot_raw
 WITH MANAGED ACCESS;
 USE SCHEMA recover.pilot_raw;
 
-USE ROLE securityadmin;
-GRANT CREATE SCHEMA, USAGE ON DATABASE recover
-TO ROLE recover_data_engineer;
-GRANT ALL PRIVILEGES ON FUTURE SCHEMAS IN DATABASE recover
-TO ROLE recover_data_engineer;
-GRANT ALL PRIVILEGES ON FUTURE TABLES IN DATABASE recover
-TO ROLE recover_data_engineer;
-GRANT USAGE ON WAREHOUSE recover_xsmall
-TO ROLE recover_data_engineer;
-GRANT USAGE ON DATABASE recover
-TO ROLE recover_data_analytics;
-GRANT USAGE ON FUTURE SCHEMAS IN DATABASE recover
-TO ROLE recover_data_analytics;
-GRANT SELECT ON FUTURE TABLES IN DATABASE recover
-TO ROLE recover_data_analytics;
-
 -- Set up storage integration
-DESC INTEGRATION recover_dev_s3;
-GRANT USAGE ON INTEGRATION recover_dev_s3
-TO ROLE sysadmin;
+
 USE ROLE sysadmin;
 CREATE STAGE IF NOT EXISTS recover_dev
   STORAGE_INTEGRATION = recover_dev_s3 --noqa: LT02,PRS
   URL = 's3://recover-dev-processed-data' --noqa: LT02
   FILE_FORMAT = (TYPE = PARQUET COMPRESSION = AUTO); --noqa: LT02
+DESC INTEGRATION recover_dev_s3;
 
 CREATE STAGE IF NOT EXISTS recover_dev_intermediate
   STORAGE_INTEGRATION = recover_dev_s3 --noqa: LT02
