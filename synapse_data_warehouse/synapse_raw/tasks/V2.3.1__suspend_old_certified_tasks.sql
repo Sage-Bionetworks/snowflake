cab0e10870e89d2716edbf2402615db713c3501f
Thomas Yu <thomas.yu@sagebase.org>
2023-11-06 23:21:00 -0800

    Suspend and resume parent task

--- synapse_data_warehouse/synapse_raw/tasks/V2.3.1__suspend_old_certified_tasks.sql
+++ synapse_data_warehouse/synapse_raw/tasks/V2.3.1__suspend_old_certified_tasks.sql
@@ -1,4 +1,6 @@
 use role accountadmin;
 use schema {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP
+alter task refresh_synapse_warehouse_s3_stage_task suspend;
 alter task certifiedquiz_task suspend;
 alter task certifiedquizquestion_task suspend;
+alter task refresh_synapse_warehouse_s3_stage_task resume;
