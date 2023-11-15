9d5a9778718c974a5809b7840509aa5ae49ddcc5
Thomas Yu <thomas.yu@sagebase.org>
2023-11-14 23:49:35 -0800

    Create GENIE tables

---
+++ genie/genie_elt.py
@@ -0,0 +1,69 @@
+"""GENIE ELT pipeline"""
+import os
+
+from dotenv import dotenv_values
+import pandas as pd
+import snowflake.connector
+from snowflake.connector.pandas_tools import write_pandas
+import synapseclient
+
+
+def main():
+    """GENIE ELT pipeline"""
+    syn = synapseclient.login()
+
+    config = dotenv_values("../.env")
+
+    ctx = snowflake.connector.connect(
+        user=config['user'],
+        password=config['password'],
+        account=config['snowflake_account'],
+        database="genie",
+        role="SYSADMIN",
+        warehouse="compute_xsmall"
+    )
+
+    cs = ctx.cursor()
+
+    releases = syn.getChildren("syn7844529")
+    for release in releases:
+        if release['name'] != "Release 14.1-public":
+            continue
+        release_name = (release['name']
+            .replace("Release ", "")
+            .replace(".", "_")
+            .replace("-public", "")
+        )
+        release_id = release['id']
+        release_files = syn.getChildren(release_id)
+        release_file_map = {
+            release_file['name']: syn.get(release_file['id'], followLink=True)
+            for release_file in release_files
+            if release_file['name'].startswith(("data_clinical", "data_mutations")) and
+            release_file['name'].endswith("txt")
+        }
+
+        cs.execute(
+            f"CREATE SCHEMA IF NOT EXISTS public_{release_name} WITH MANAGED ACCESS;"
+        )
+        for release_file_key, release_file_ent in release_file_map.items():
+            cs.execute(f"USE SCHEMA public_{release_name}")
+            tbl_name = release_file_key.replace("data_", "").replace(".txt", "")
+            table_df = pd.read_csv(
+                release_file_ent.path,
+                sep="\t",
+                comment="#",
+                low_memory=False
+            )
+            write_pandas(
+                ctx,
+                table_df,
+                tbl_name,
+                auto_create_table=True,
+                quote_identifiers=False,
+                overwrite=True
+            )
+
+
+if __name__ == "__main__":
+    main()
