84b35476d8e65c0359cd94b5dec3c2936c523ec5
Thomas Yu <thomas.yu@sagebase.org>
2024-04-22 23:49:25 -0700

    Suspend tasks first

--- synapse_data_warehouse/synapse_raw/tasks/V2.14.0__shift_all_tasks_under_refresh.sql
+++ synapse_data_warehouse/synapse_raw/tasks/V2.14.0__shift_all_tasks_under_refresh.sql
@@ -1,12 +1,16 @@
 USE ROLE accountadmin;
 use schema {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP,CP01
 ALTER TASK refresh_synapse_warehouse_s3_stage_task SUSPEND;
+ALTER TASK aclsnapshots_task SUSPEND;
 ALTER TASK aclsnapshots_task UNSET schedule;
 ALTER TASK aclsnapshots_task ADD AFTER refresh_synapse_warehouse_s3_stage_task;
+ALTER TASK TEAMSNAPSHOTS_TASK SUSPEND;
 ALTER TASK TEAMSNAPSHOTS_TASK UNSET schedule;
 ALTER TASK TEAMSNAPSHOTS_TASK ADD AFTER refresh_synapse_warehouse_s3_stage_task;
+ALTER TASK VERIFICATIONSUBMISSIONSNAPSHOTS_TASK SUSPEND;
 ALTER TASK VERIFICATIONSUBMISSIONSNAPSHOTS_TASK UNSET schedule;
 ALTER TASK VERIFICATIONSUBMISSIONSNAPSHOTS_TASK ADD AFTER refresh_synapse_warehouse_s3_stage_task;
+ALTER TASK USERGROUPSNAPSHOTS_TASK SUSPEND;
 ALTER TASK USERGROUPSNAPSHOTS_TASK UNSET schedule;
 ALTER TASK USERGROUPSNAPSHOTS_TASK ADD AFTER refresh_synapse_warehouse_s3_stage_task;
 // clean up unused tasks
