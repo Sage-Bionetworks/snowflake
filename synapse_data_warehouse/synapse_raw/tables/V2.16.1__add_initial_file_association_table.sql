21f50ca21cfe71f9d6c25f00947b54d49969bde3
Thomas Yu <thomas.yu@sagebase.org>
2024-05-08 19:01:22 -0700

    Add comment

--- synapse_data_warehouse/synapse_raw/tables/V2.16.1__add_initial_file_association_table.sql
+++ synapse_data_warehouse/synapse_raw/tables/V2.16.1__add_initial_file_association_table.sql
@@ -1,13 +1,14 @@
 USE SCHEMA {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP
 USE WAREHOUSE COMPUTE_MEDIUM;
 CREATE TABLE IF NOT EXISTS filehandle_association (
-    associateid int,
-    associatetype string,
-    filehandleid INT,
-    instance string,
-    stack string,
-    timestamp timestamp
+    associateid INT COMMENT 'The unique identifier of the Synapse object that wraps the file.',
+    associatetype STRING COMMENT 'The type of the Synapse object that wraps the file.',
+    filehandleid INT COMMENT 'The unique identifier of the file handle.',
+    instance STRING COMMENT 'The version of the stack that processed the file association.',
+    stack STRING COMMENT 'The stack (prod, dev) on which the file handle association processed.',
+    timestamp TIMESTAMP COMMENT 'The time when the association data was collected.'
 )
+COMMENT='The table contains file handle association records that are weekly scanned. A FileHandleAssociation record is a FileHandle (identified by its id) along with a Synapse object (identified by its id and type).'
 CLUSTER BY (instance);
 
 copy into
