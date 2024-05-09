1508b64c2ade59bc1b77a720bc409a54f591ca7a
Thomas Yu <thomas.yu@sagebase.org>
2024-05-09 09:02:09 -0700

    Update synapse_data_warehouse/synapse_raw/tasks/V2.16.2__add_file_association_tasks.sql

--- synapse_data_warehouse/synapse_raw/tasks/V2.16.2__add_file_association_tasks.sql
+++ synapse_data_warehouse/synapse_raw/tasks/V2.16.2__add_file_association_tasks.sql
@@ -1,7 +1,7 @@
 use role accountadmin;
 use schema {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP
 alter task refresh_synapse_warehouse_s3_stage_task suspend;
-create task if not exists append_to_fileinventory_task
+create task if not exists append_to_fileassociation_task
     user_task_managed_initial_warehouse_size = 'SMALL'
     AFTER refresh_synapse_warehouse_s3_stage_task
 as
