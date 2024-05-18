80c24fb96996e3f93b8dc0f1a8659df54775a26a
Thomas Yu <thomas.yu@sagebase.org>
2024-05-18 14:31:55 -0700

    Add in GX

--- gx/README.md
+++ gx/README.md
@@ -3,3 +3,9 @@
 Follow instructions here: https://docs.greatexpectations.io/docs/oss/tutorials/quickstart/ to get started.
 
 pip install sqlalchemy python-dotenv snowflake-sqlalchemy
+
+1. Be sure to export and create the GX data source environmental variable. Follow these [steps](https://docs.greatexpectations.io/docs/oss/guides/connecting_to_your_data/fluent/database/connect_sql_source_data?sql-database-type=snowflake) to set it up: 
+
+```
+export GX_SNOWFLAKE_CONNECTION="snowflake://<USER_NAME>:<PASSWORD>@<ACCOUNT_NAME_OR_LOCATOR>/<DATABASE_NAME>/<SCHEMA_NAME>?warehouse=<WAREHOUSE_NAME>&role=<ROLE_NAME>"
+```
--- gx/validation.py
+++ gx/validation.py
@@ -1,73 +1,51 @@
-import great_expectations as gx
-from great_expectations.checkpoint import Checkpoint
-
-
 from dotenv import dotenv_values
-config = dotenv_values("../.env")
+import great_expectations as gx
 
-user=config['user']
-password=config['password']
-snow_account = config['snowflake_account']
-database = "synapse_data_warehouse_dev"
 context = gx.get_context()
-
-my_connection_string = f"snowflake://{user}:{password}@{snow_account}/{database}/synapse?warehouse=compute_xsmall&role=SYSADMIN"
+# Define the datasource name
 datasource_name = "synapse_data_warehouse_raw"
 
-datasource = context.sources.add_snowflake(
-    name=datasource_name, 
-    connection_string=my_connection_string, # Or alternatively, individual connection args
-)
+# Retrieve the existing datasource
+try:
+    datasource = context.get_datasource(datasource_name)
+    print(f"Datasource '{datasource_name}' retrieved successfully.")
+except Exception as e:
+    print(f"Failed to retrieve datasource '{datasource_name}': {str(e)}")
+    config = dotenv_values("../.env")
+    user=config['user']
+    password=config['password']
+    snow_account = config['snowflake_account']
+    database = "synapse_data_warehouse_dev"
+    my_connection_string = f"snowflake://{user}:{password}@{snow_account}/{database}/synapse?warehouse=compute_xsmall&role=SYSADMIN"
+
+    datasource = context.sources.add_snowflake(
+        name=datasource_name, 
+        connection_string=my_connection_string, # Or alternatively, individual connection args
+    )
 
 asset_name = "node"
 asset_table_name = "node_latest"
-
-table_asset = datasource.add_table_asset(name=asset_name, table_name=asset_table_name)
+try:
+    table_asset = datasource.get_asset(asset_name)
+    # datasource = context.get_datasource(datasource_name)
+    # print(f"Datasource '{datasource_name}' retrieved successfully.")
+except Exception as e:
+    table_asset = datasource.add_table_asset(name=asset_name, table_name=asset_table_name)
 
 batch_request = table_asset.build_batch_request()
 
-##
 expectation_suite_name = "synapse_data_warehouse_expectations"
-
-context.add_or_update_expectation_suite(expectation_suite_name=expectation_suite_name)
 validator = context.get_validator(
     batch_request=batch_request,
     expectation_suite_name=expectation_suite_name,
 )
-validator.expect_column_values_to_not_be_null("ID")
-# validator.expect_column_values_to_be_between(
-#     "passenger_count", min_value=1, max_value=6
-# )
-validator.save_expectation_suite(discard_failed_expectations=False)
-
-
 
 my_checkpoint_name = "my_snow_checkpoint"
 
-checkpoint = Checkpoint(
+checkpoint = context.add_or_update_checkpoint(
     name=my_checkpoint_name,
-    run_name_template="%Y%m%d-%H%M%S-my-run-name-template",
-    data_context=context,
-    batch_request=batch_request,
-    expectation_suite_name=expectation_suite_name,
-    # action_list=[
-    #     {
-    #         "name": "store_validation_result",
-    #         "action": {"class_name": "StoreValidationResultAction"},
-    #     },
-    #     {"name": "update_data_docs", "action": {"class_name": "UpdateDataDocsAction"}},
-    # ],
+    validator=validator,
 )
-
-context.add_or_update_checkpoint(checkpoint=checkpoint)
-
 checkpoint_result = checkpoint.run()
 
-
-# checkpoint = context.add_or_update_checkpoint(
-#     name="my_quickstart_checkpoint",
-#     validator=validator,
-# )
-# checkpoint_result = checkpoint.run()
-
-# context.view_validation_result(checkpoint_result)
+context.view_validation_result(checkpoint_result)
