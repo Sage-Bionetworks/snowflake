a5bb934ce6c0a5fed57fcd4a453e89254693f317
Thomas Yu <thomas.yu@sagebase.org>
2024-05-25 12:57:36 -0700

    Ran linter

--- sage/ip_lookup.py
+++ sage/ip_lookup.py
@@ -6,6 +6,7 @@ for analysis
 
 This eventually could be an Airflow DAG, so not going spending too much time on it
 """
+
 import requests
 import time
 
@@ -18,17 +19,17 @@ import numpy as np
 
 def get_ip_info(ip_list: list) -> dict:
     """Get IP information from ip-api.com: http://ip-api.com/batch
-    
+
     Args:
         ip_list (list): list of IP addresses
-    
+
     Returns:
         dict: IP information like city, country, region, lat, long, asn, etc
     """
     # HACK this is not great...
     while True:
         try:
-            ip_info_response = requests.post('http://ip-api.com/batch', json=ip_list)
+            ip_info_response = requests.post("http://ip-api.com/batch", json=ip_list)
         except Exception as err:
             print(ip_info_response.status_code)
             if ip_info_response.status_code == 429:
@@ -39,17 +40,18 @@ def get_ip_info(ip_list: list) -> dict:
             return ip_info_response.json()
         # return ip_info
 
+
 def main():
     """Main function"""
     config = dotenv_values("../.env")
     ctx = snowflake.connector.connect(
-        user=config['user'],
-        password=config['password'],
-        account=config['snowflake_account'],
+        user=config["user"],
+        password=config["password"],
+        account=config["snowflake_account"],
         database="synapse_data_warehouse",
         schema="synapse",
         role="SYSADMIN",
-        warehouse="compute_xsmall"
+        warehouse="compute_xsmall",
     )
     cs = ctx.cursor()
 
@@ -70,24 +72,26 @@ def main():
     batch_size = 100
     result = []
 
-    for batch_number, batch_df in unique_ips.groupby(np.arange(len(unique_ips)) // batch_size):
+    for batch_number, batch_df in unique_ips.groupby(
+        np.arange(len(unique_ips)) // batch_size
+    ):
         print(batch_number)
-        ip_list = get_ip_info(batch_df['UNIQUE_IPS'].to_list())
+        ip_list = get_ip_info(batch_df["UNIQUE_IPS"].to_list())
         # API rate limit of 15 per minute
         # Add in sleep to not get throttled
         time.sleep(2.5)
         result.extend(ip_list)
 
     ip_info_df = pd.DataFrame(result)
-    succeeded_ip_info = ip_info_df[ip_info_df['status'] == 'success']
+    succeeded_ip_info = ip_info_df[ip_info_df["status"] == "success"]
 
     # These columns do not add value in a snowflake query
-    del succeeded_ip_info['status']
-    del succeeded_ip_info['message']
+    del succeeded_ip_info["status"]
+    del succeeded_ip_info["message"]
 
     # Renaming columns to be more descriptive or ignoring SQL key words
-    succeeded_ip_info.rename(columns={'query': 'ip', 'as': 'asn'}, inplace=True)
-    succeeded_ip_info.to_csv('ip_info.csv', index=False)
+    succeeded_ip_info.rename(columns={"query": "ip", "as": "asn"}, inplace=True)
+    succeeded_ip_info.to_csv("ip_info.csv", index=False)
 
     write_pandas(
         conn=ctx,
@@ -97,9 +101,10 @@ def main():
         schema="AUDIT",
         auto_create_table=True,
         # overwrite=True,
-        quote_identifiers=False
+        quote_identifiers=False,
     )
     ctx.close()
+
 
 if __name__ == "__main__":
     main()
