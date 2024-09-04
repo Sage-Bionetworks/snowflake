ae91e2acd51a8d73e877f8453a38364571954203
Jenny Medina <medina.stsci@gmail.com>
2024-09-04 16:47:01 -0400

    Revert back to original solution

--- synapse_data_warehouse/synapse/dynamic_tables/R__populate_filehandleassociation_latest.sql
+++ synapse_data_warehouse/synapse/dynamic_tables/R__populate_filehandleassociation_latest.sql
@@ -4,21 +4,30 @@ CREATE OR REPLACE DYNAMIC TABLE filehandleassociation_latest
 TARGET_LAG = '7 days'
 WAREHOUSE = compute_xsmall
 AS
-    WITH latest_filehandleassociations AS (
+    WITH latest_unique_filehandles AS (
         SELECT
-            *,
-            ROW_NUMBER() OVER (
-                PARTITION BY filehandleid, associateid 
-                ORDER BY timestamp DESC
-            ) AS row_num
+            filehandleid,
+            associateid,
+            MAX(timestamp) AS latest_timestamp
         FROM
-            synapse_data_warehouse_jmedina.synapse_raw.filehandleassociationsnapshots
+            synapse_data_warehouse.synapse_raw.filehandleassociationsnapshots
         WHERE
             timestamp >= CURRENT_TIMESTAMP - INTERVAL '14 DAYS'
-        QUALIFY
-            row_num = 1
+        AND
+            stack = 'prod'
+        GROUP BY
+            filehandleid,
+            associateid
     )
     SELECT
-        *
+        filehandleassociationsnapshots.*
     FROM
-        latest_filehandleassociations;
+        synapse_data_warehouse.synapse_raw.filehandleassociationsnapshots
+    JOIN
+        latest_unique_filehandles
+    ON
+        filehandleassociationsnapshots.filehandleid = latest_unique_filehandles.filehandleid
+    AND
+        filehandleassociationsnapshots.timestamp = latest_unique_filehandles.latest_timestamp
+    AND
+        filehandleassociationsnapshots.associateid = latest_unique_filehandles.associateid;
