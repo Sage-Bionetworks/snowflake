f80c7c62ba6930bd888d7b065dbeace5d824555d
Jenny Medina <medina.stsci@gmail.com>
2024-09-04 17:16:48 -0400

    Revert back to CTEs

--- synapse_data_warehouse/synapse/dynamic_tables/R__populate_acl_latest_table.sql
+++ synapse_data_warehouse/synapse/dynamic_tables/R__populate_acl_latest_table.sql
@@ -1,57 +1,55 @@
 -- 1. Initialize the schema that the dynamic table will live under
 USE SCHEMA {{database_name}}.synapse; --noqa: JJ01,PRS,TMP,CP01
 
--- 2. Create the first temporary table which unpacks the resource_access column
---    and extracts the access type and principal ID as new columns
-CREATE TEMPORARY TABLE acl_expanded_temp AS
-    SELECT
-        owner_id,
-        change_timestamp,
-        COALESCE(
-            array_sort(value:"accesstype"::variant),
-            array_sort(value:"accessType"::variant),
-            array_sort(value:"accesstype#1"::variant),
-            array_sort(value:"accesstype#2"::variant),
-            array_sort(value:"accesstype#3"::variant)
-        ) AS access_type,
-        COALESCE(
-            value:"principalId"::number,
-            value:"principalid"::number,
-            value:"principalid#1"::number
-        ) AS principal_id
-    FROM 
-        {{database_name}}.synapse_raw.aclsnapshots,  --noqa: TMP
-        LATERAL FLATTEN(input => parse_json(resource_access), outer => TRUE);
-
--- 3. Create the second temporary table which deduplicates the data from the table
---    created in the previous step based on owner_id, access_type, and principal_id
-CREATE TEMPORARY TABLE dedup_acl_expanded_temp AS
-    SELECT
-        owner_id, 
-        change_timestamp, 
-        access_type, 
-        principal_id,
-        ROW_NUMBER() OVER (
-            PARTITION BY owner_id, access_type, principal_id
-            ORDER BY change_timestamp DESC
-        ) AS row_num
-    FROM
-        acl_expanded_temp
-    QUALIFY row_num = 1;
-
--- 4. Create the dynamic table ACL_LATEST from the deduplicated temporary table
+-- 2. Create the dynamic table ACL_LATEST to be refreshed every 1 day(s)
 CREATE OR REPLACE DYNAMIC TABLE ACL_LATEST
     TARGET_LAG = '1 day'
     WAREHOUSE = compute_xsmall
     AS
+    -- 3. Create the first CTE which unpacks the resource_access column
+    --    and extracts the access type and principal ID as new columns
+    WITH
+    acl_expanded as (
+        SELECT
+            owner_id,
+            change_timestamp,
+            COALESCE(
+                array_sort(value:"accesstype"::variant),
+                array_sort(value:"accessType"::variant),
+                array_sort(value:"accesstype#1"::variant),
+                array_sort(value:"accesstype#2"::variant),
+                array_sort(value:"accesstype#3"::variant)
+            ) AS access_type,
+            COALESCE(
+                value:"principalId"::number,
+                value:"principalid"::number,
+                value:"principalid#1"::number
+            ) AS principal_id
+        FROM 
+            {{database_name}}.synapse_raw.aclsnapshots,  --noqa: TMP
+            LATERAL FLATTEN(input => parse_json(resource_access), outer => TRUE)
+    ),
+    -- 4. Create the second CTE which deduplicates the data from the CTE output
+    --    in the previous step based on owner_id, access_type, and principal_id
+    dedup_acl_expanded AS (
+        SELECT
+            owner_id, 
+            change_timestamp, 
+            access_type, 
+            principal_id,
+            ROW_NUMBER() OVER (
+                PARTITION BY owner_id, access_type, principal_id
+                ORDER BY change_timestamp DESC
+            ) AS row_num
+        FROM
+            acl_expanded_temp
+        QUALIFY row_num = 1
+    )
+    -- 5. Select the data from the CTE output in step 4.
     SELECT
-        owner_id, 
-        change_timestamp, 
-        access_type, 
-        principal_id
-    FROM
-        dedup_acl_expanded_temp;
-
--- 5. Cleanup: Drop the temporary tables
-DROP TABLE IF EXISTS acl_expanded_temp;
-DROP TABLE IF EXISTS dedup_acl_expanded_temp;
+            owner_id, 
+            change_timestamp, 
+            access_type, 
+            principal_id
+        FROM
+            dedup_acl_expanded
