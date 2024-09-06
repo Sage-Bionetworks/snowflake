2eb3975ab25f667052d667bb0dcd03d9282300d3
Jenny Medina <medina.stsci@gmail.com>
2024-09-06 09:57:46 -0400

    fix CTE reference

--- synapse_data_warehouse/synapse/dynamic_tables/R__populate_filehandleassociation_latest.sql
+++ synapse_data_warehouse/synapse/dynamic_tables/R__populate_filehandleassociation_latest.sql
@@ -1,6 +1,6 @@
 use schema {{database_name}}.synapse; --noqa: JJ01,PRS,TMP,CP01
 
-CREATE OR REPLACE DYNAMIC TABLE filehandleassociation_latest
+CREATE OR REPLACE DYNAMIC TABLE FILEHANDLEASSOCIATION_LATEST
 TARGET_LAG = '7 days'
 WAREHOUSE = compute_xsmall
 AS
@@ -24,8 +24,8 @@ AS
     JOIN
         latest_unique_rows
     ON
-        filehandleassociationsnapshots.filehandleid = latest_unique_filehandles.filehandleid
+        filehandleassociationsnapshots.filehandleid = latest_unique_rows.filehandleid
     AND
-        filehandleassociationsnapshots.associateid = latest_unique_filehandles.associateid
+        filehandleassociationsnapshots.associateid = latest_unique_rows.associateid
     AND
-        filehandleassociationsnapshots.timestamp = latest_unique_filehandles.latest_timestamp;
+        filehandleassociationsnapshots.timestamp = latest_unique_rows.latest_timestamp;
