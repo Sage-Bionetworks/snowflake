2090fadb5245f0a1be9733755ba913c7ecc55829
Jenny Medina <medina.stsci@gmail.com>
2024-10-23 16:34:22 -0400

    .

--- admin/clone_db.sql
+++ admin/clone_db.sql
@@ -2,6 +2,8 @@
 
 USE ROLE DATA_ENGINEER;
 
+SET DB_NAME = CLONED_DB_NAME
+
 -- We clone the DB using the syntax seen here:
 -- https://docs.snowflake.com/en/sql-reference/sql/create-clone#databases-schemas
-CREATE DATABASE IDENTIFIER('${CLONED_DB_NAME}') CLONE SYNAPSE_DATA_WAREHOUSE_DEV;
+CREATE DATABASE IDENTIFIER(DB_NAME) CLONE SYNAPSE_DATA_WAREHOUSE_DEV;
