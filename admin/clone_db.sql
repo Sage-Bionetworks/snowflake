ca53ddd7735179f18cd15ab07648a367ab258c97
Jenny Medina <medina.stsci@gmail.com>
2024-10-23 13:55:57 -0400

    dynamically change the name of the clone DB

--- admin/clone_db.sql
+++ admin/clone_db.sql
@@ -2,4 +2,4 @@ USE ROLE DATA_ENGINEER;
 
 -- We clone the DB using the syntax seen here:
 -- https://docs.snowflake.com/en/sql-reference/sql/create-clone#databases-schemas
-CREATE DATABASE TEST_CLONE_DB CLONE SYNAPSE_DATA_WAREHOUSE_DEV;
+CREATE DATABASE IDENTIFIER($CLONED_DB_NAME) CLONE SYNAPSE_DATA_WAREHOUSE_DEV;
