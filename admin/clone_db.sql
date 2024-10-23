64bdc7d1b1184754f0b00e6d1c4929810c8c8d31
Jenny Medina <medina.stsci@gmail.com>
2024-10-23 14:24:36 -0400

    set var sub true

--- admin/clone_db.sql
+++ admin/clone_db.sql
@@ -1,5 +1,7 @@
+!set variable_substitution=true; --noqa: PRS
+
 USE ROLE DATA_ENGINEER;
 
 -- We clone the DB using the syntax seen here:
 -- https://docs.snowflake.com/en/sql-reference/sql/create-clone#databases-schemas
-CREATE DATABASE IDENTIFIER(&CLONED_DB_NAME) CLONE SYNAPSE_DATA_WAREHOUSE_DEV;
+CREATE DATABASE IDENTIFIER('&CLONED_DB_NAME') CLONE SYNAPSE_DATA_WAREHOUSE_DEV;
