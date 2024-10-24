9a89e938a68f111467e29dbc101e6ee1bcb2845d
Jenny Medina <medina.stsci@gmail.com>
2024-10-24 15:08:35 -0400

    remove clone_db.sql

--- admin/clone_db.sql
+++
@@ -1,7 +0,0 @@
-!set variable_substitution=true;
-
-USE ROLE DATA_ENGINEER;
-
--- We clone the DB using the syntax seen here:
--- https://docs.snowflake.com/en/sql-reference/sql/create-clone#databases-schemas
-CREATE DATABASE IDENTIFIER($cloned_db_name) CLONE SYNAPSE_DATA_WAREHOUSE_DEV;
