423133b82fe4c31a64bd25d533500b950ca6fe4f
Jenny Medina <medina.stsci@gmail.com>
2024-11-13 10:31:19 -0500

    simplify query

--- synapse_data_warehouse/synapse/dynamic_tables/V2.21.0__verificationsubmission_latest.sql
+++ synapse_data_warehouse/synapse/dynamic_tables/V2.21.0__verificationsubmission_latest.sql
@@ -4,25 +4,11 @@ CREATE DYNAMIC TABLE IF NOT EXISTS VERIFICATIONSUBMISSION_LATEST
     TARGET_LAG = '1 day'
     WAREHOUSE = compute_xsmall
     AS
-    WITH latest_rows AS (
-        SELECT
-            id AS latest_id,
-            MAX(snapshot_timestamp) AS latest_timestamp
-        FROM
-            verificationsubmissionsnapshots
-        GROUP BY
-            latest_id
-    ),
-    latest_unique_rows AS (
+    WITH latest_unique_rows AS (
         SELECT
             verificationsubmissionsnapshots.*,
         FROM
-            verificationsubmissionsnapshots
-        JOIN
-            latest_rows
-        ON
-            verificationsubmissionsnapshots.id = latest_rows.latest_id
-            AND verificationsubmissionsnapshots.snapshot_timestamp = latest_rows.latest_timestamp
+            {{database_name}}.synapse_raw.verificationsubmissionsnapshots --noqa: TMP
         QUALIFY ROW_NUMBER() OVER (
                 PARTITION BY id
                 ORDER BY snapshot_timestamp DESC
