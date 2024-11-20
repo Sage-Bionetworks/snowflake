05ff362a930047f4ea043f2b1c13013d871d8138
Jenny Medina <medina.stsci@gmail.com>
2024-11-20 10:10:06 -0500

    indent query

--- synapse_data_warehouse/synapse/dynamic_tables/V2.23.0__accessrequirement_latest.sql
+++ synapse_data_warehouse/synapse/dynamic_tables/V2.23.0__accessrequirement_latest.sql
@@ -4,23 +4,23 @@ CREATE DYNAMIC TABLE IF NOT EXISTS ACCESSREQUIREMENT_LATEST
     TARGET_LAG = '1 day'
     WAREHOUSE = compute_xsmall
     AS
-    WITH latest_unique_rows AS (
+        WITH latest_unique_rows AS (
+            SELECT
+                *
+            FROM
+                {{database_name}}.synapse_raw.accessrequirementsnapshots --noqa: TMP
+            WHERE
+                SNAPSHOT_TIMESTAMP >= CURRENT_TIMESTAMP - INTERVAL '14 DAYS'
+            QUALIFY ROW_NUMBER() OVER (
+                    PARTITION BY id
+                    ORDER BY change_timestamp DESC, snapshot_timestamp DESC
+                ) = 1
+        )
         SELECT
             *
         FROM
-            {{database_name}}.synapse_raw.accessrequirementsnapshots --noqa: TMP
+            latest_unique_rows
         WHERE
-            SNAPSHOT_TIMESTAMP >= CURRENT_TIMESTAMP - INTERVAL '14 DAYS'
-        QUALIFY ROW_NUMBER() OVER (
-                PARTITION BY id
-                ORDER BY change_timestamp DESC, snapshot_timestamp DESC
-            ) = 1
-    )
-    SELECT
-        *
-    FROM
-        latest_unique_rows
-    WHERE
-        CHANGE_TYPE != 'DELETE'
-    ORDER BY
-        latest_unique_rows.id ASC;
+            CHANGE_TYPE != 'DELETE'
+        ORDER BY
+            latest_unique_rows.id ASC;
