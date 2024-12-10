2cf2c899f81a5de9cf46a6fe0bd6f0b1709ec648
Jenny Medina <medina.stsci@gmail.com>
2024-12-10 10:17:22 -0500

    Removing schema declaration in queries that dont need it

--- synapse_data_warehouse/synapse/tables/V2.26.3__drop_node_latest.sql
+++ synapse_data_warehouse/synapse/tables/V2.26.3__drop_node_latest.sql
@@ -1,4 +1,4 @@
 USE SCHEMA {{database_name}}.synapse; --noqa: JJ01,PRS,TMP
 
 -- Drop the ``NODE_LATEST`` table so that a new dynamic table with the same name can be created in its place
-DROP TABLE {{database_name}}.synapse.NODE_LATEST; --noqa: JJ01,PRS,TMP
+DROP TABLE NODE_LATEST;
