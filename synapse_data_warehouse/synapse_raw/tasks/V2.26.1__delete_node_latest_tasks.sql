a222f74a82bb94b8e7c4f8272799c68704727da8
Jenny Medina <medina.stsci@gmail.com>
2024-12-11 10:43:14 -0500

    Suspend tasks before dropping

--- synapse_data_warehouse/synapse_raw/tasks/V2.26.1__delete_node_latest_tasks.sql
+++ synapse_data_warehouse/synapse_raw/tasks/V2.26.1__delete_node_latest_tasks.sql
@@ -1,4 +1,11 @@
 USE SCHEMA {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP
 
+-- Momentarily suspend the ROOT task so we can remove the child tasks
+ALTER TASK IF EXISTS REFRESH_SYNAPSE_WAREHOUSE_S3_STAGE_TASK SUSPEND;
+
+-- Remove the tasks in question
 DROP TASK IF EXISTS UPSERT_TO_NODE_LATEST_TASK;
 DROP TASK IF EXISTS REMOVE_DELETE_NODES_TASK;
+
+-- Resume the ROOT task and its child tasks
+SELECT SYSTEM$TASK_DEPENDENTS_ENABLE( 'REFRESH_SYNAPSE_WAREHOUSE_S3_STAGE_TASK' );
