1e5712542fff304beaadb45ff3f22a9a4cea5444
Jenny Medina <medina.stsci@gmail.com>
2024-12-18 13:26:08 -0500

    Add new column

---
+++ synapse_data_warehouse/synapse_raw/tables/V2.28.0__add_is_two_factor_auth_enabled_to_userprofilesnapshots.sql
@@ -0,0 +1,7 @@
+-- Configure environment
+USE SCHEMA {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP,CP02
+
+-- Add `version_history` column
+ALTER TABLE userprofilesnapshot
+  ADD COLUMN is_two_factor_auth_enabled BOOLEAN
+  COMMENT 'Indicates if the user had two factor authentication enabled when the snapshot was captured.';
