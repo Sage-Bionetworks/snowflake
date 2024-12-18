25d16c756980a460c8283b1342ecaa86850e66e7
Jenny Medina <medina.stsci@gmail.com>
2024-12-18 14:34:31 -0500

    refresh the entire table

---
+++ synapse_data_warehouse/synapse_raw/tables/V2.28.2__refresh_userprofilesnapshots.sql
@@ -0,0 +1,36 @@
+-- Configure environment
+USE SCHEMA {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP,CP02
+
+-- Drop all records from this table but keep structure and permissions intact
+TRUNCATE TABLE userprofilesnapshot;
+
+-- 
+COPY INTO
+        userprofilesnapshot
+FROM (
+    SELECT
+        $1:change_type as change_type,
+        $1:change_timestamp as change_timestamp,
+        $1:change_user_id as change_user_id,
+        $1:snapshot_timestamp as snapshot_timestamp,
+        $1:id as id,
+        $1:user_name as user_name,
+        $1:first_name as first_name,
+        $1:last_name as last_name,
+        $1:email as email,
+        $1:location as location,
+        $1:company as company,
+        $1:position as position,
+        $1:is_two_factor_auth_enabled as is_two_factor_auth_enabled,
+        NULLIF(
+            REGEXP_REPLACE(
+                metadata$filename,
+                '.*userprofilesnapshots\/snapshot_date\=(.*)\/.*', '\\1'
+            ),
+            '__HIVE_DEFAULT_PARTITION__'
+        ) as snapshot_date,
+        $1:created_on as created_on
+    from
+        @synapse_prod_warehouse_s3_stage/userprofilesnapshots --noqa: TMP
+)
+pattern = '.*userprofilesnapshots/snapshot_date=.*/.*';
