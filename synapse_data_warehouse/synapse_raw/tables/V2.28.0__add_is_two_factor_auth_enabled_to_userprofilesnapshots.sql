4e0530a64d7e7029ad2df0a367ff1c51f042632e
Jenny V Medina <jenny.medina@sagebase.org>
2024-12-20 15:01:10 -0500

    [SNOW-60,162,165] Add new columns to `userprofilesnapshots` (#95)

---
+++ synapse_data_warehouse/synapse_raw/tables/V2.28.0__add_is_two_factor_auth_enabled_to_userprofilesnapshots.sql
@@ -0,0 +1,7 @@
+-- Configure environment
+USE SCHEMA {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP,CP02
+
+-- Add new column
+ALTER TABLE userprofilesnapshot ADD COLUMN is_two_factor_auth_enabled BOOLEAN COMMENT 'Indicates if the user had two factor authentication enabled when the snapshot was captured.';
+ALTER TABLE userprofilesnapshot ADD COLUMN industry STRING COMMENT 'The industry/discipline that this person is associated with.';
+ALTER TABLE userprofilesnapshot ADD COLUMN tos_agreements VARIANT COMMENT 'Contains the list of all the term of service that the user agreed to, with their agreed on date and version.';
