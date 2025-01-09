103a6227c81584ba3d55df22db8f6c391479447b
Jenny Medina <medina.stsci@gmail.com>
2025-01-09 12:08:58 -0500

    Update to deduplication method

--- synapse_data_warehouse/synapse/dynamic_tables/V2.28.4__certifiedquizquestion_latest.sql
+++ synapse_data_warehouse/synapse/dynamic_tables/V2.28.4__certifiedquizquestion_latest.sql
@@ -10,9 +10,9 @@ CREATE DYNAMIC TABLE IF NOT EXISTS CERTIFIEDQUIZQUESTION_LATEST
             FROM
                 {{database_name}}.synapse_raw.certifiedquizquestionsnapshots --noqa: TMP
             WHERE
-                SNAPSHOT_TIMESTAMP >= CURRENT_TIMESTAMP - INTERVAL '14 DAYS'
+                SNAPSHOT_TIMESTAMP >= CURRENT_TIMESTAMP - INTERVAL '30 DAYS'
             QUALIFY ROW_NUMBER() OVER (
-                    PARTITION BY id
+                    PARTITION BY response_id, question_index, change_user_id
                     ORDER BY change_timestamp DESC, snapshot_timestamp DESC
                 ) = 1
         )
@@ -21,4 +21,4 @@ CREATE DYNAMIC TABLE IF NOT EXISTS CERTIFIEDQUIZQUESTION_LATEST
         FROM
             latest_unique_rows
         ORDER BY
-            latest_unique_rows.id ASC;
+            latest_unique_rows.response_id ASC;
