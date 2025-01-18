299a738cec3f2fc74e39c2f204184dccbe7b3f5f
Jenny Medina <medina.stsci@gmail.com>
2025-01-18 11:06:56 -0500

    project_storage_usage

--- synapse_data_warehouse/synapse_raw/V2.32.0__add_project_metadata_columns_nodesnapshots.sql
+++ synapse_data_warehouse/synapse_raw/V2.32.0__add_project_metadata_columns_nodesnapshots.sql
@@ -2,6 +2,4 @@
 USE SCHEMA {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP,CP02
 
 -- Add the new columns
-ALTER TABLE nodesnapshots ADD COLUMN maxAllowedFileBytes <data-type> COMMENT '<column description>';
-ALTER TABLE nodesnapshots ADD COLUMN storageLocationID <data-type> COMMENT '<column description>';
-ALTER TABLE nodesnapshots ADD COLUMN isOverLimit <data-type> COMMENT '<column description>';
+ALTER TABLE nodesnapshots ADD COLUMN project_storage_usage <data-type> COMMENT 'For nodes of type project includes the project storage usage data for each storage location in the project.';
