22bc223776cb50faa6fe9139730f03843d349976
danlu1 <dan.lu@sagebase.org>
2025-01-25 11:09:31 -0800

    convert file latest to dynamic table

---
+++ synapse_data_warehouse/synapse/tables/V2.33.0__add_file_latest_backup.sql
@@ -0,0 +1,4 @@
+-- Backup the original latest table 
+USE SCHEMA {{database_name}}.synapse; --noqa: JJ01,PRS,TMP
+-- Clone the FILE_LATEST table to ``FILE_LATEST_BACKUP`` for validation purposes
+CREATE OR REPLACE TABLE FILE_LATEST_BACKUP CLONE FILE_LATEST;
