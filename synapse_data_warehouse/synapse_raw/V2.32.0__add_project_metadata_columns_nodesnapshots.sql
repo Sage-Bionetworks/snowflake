8e95691670e13a7bfb2b9720abe2983b50242d25
Jenny Medina <medina.stsci@gmail.com>
2025-01-27 13:35:35 -0500

    move files. update column comment

--- synapse_data_warehouse/synapse_raw/V2.32.0__add_project_metadata_columns_nodesnapshots.sql
+++
@@ -1,5 +0,0 @@
--- Configure environment
-USE SCHEMA {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP,CP02
-
--- Add the new columns
-ALTER TABLE nodesnapshots ADD COLUMN project_storage_usage VARIANT COMMENT 'For nodes of type project includes the project storage usage data for each storage location in the project.';
