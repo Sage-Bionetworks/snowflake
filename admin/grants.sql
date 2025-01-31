0529eb2a322e754c3339aa4dcf2ba919ce2cf020
Jenny V Medina <jenny.medina@sagebase.org>
2025-01-31 14:49:13 -0500

    remove redundancy grant

--- admin/grants.sql
+++ admin/grants.sql
@@ -179,7 +179,6 @@ GRANT ROLE DATA_ANALYTICS TO USER "lisa.pasquale@sagebase.org";
 GRANT ROLE DATA_ANALYTICS TO USER "joni.harker@sagebase.org";
 GRANT ROLE DATA_ANALYTICS TO USER "jo.scanlan@sagebase.org";
 GRANT ROLE DATA_ANALYTICS TO USER "trisha.zintel@sagebase.org";
-GRANT ROLE DATA_ANALYTICS TO USER "christine.suver@sagebase.org";
 GRANT ROLE DATA_ANALYTICS TO USER "anthony.pena@sagebase.org";
 GRANT ROLE DATA_ANALYTICS TO USER "hayley.sanchez@sagebase.org";
 GRANT ROLE DATA_ANALYTICS TO USER AD_SERVICE;
