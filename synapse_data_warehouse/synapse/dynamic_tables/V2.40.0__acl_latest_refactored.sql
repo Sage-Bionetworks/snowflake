29bd363dc29b61b002f23fe45ef2197db8bc551c
Jenny Medina <medina.stsci@gmail.com>
2025-02-14 13:48:50 -0500

    parse json after dedup

--- synapse_data_warehouse/synapse/dynamic_tables/V2.40.0__acl_latest_refactored.sql
+++ synapse_data_warehouse/synapse/dynamic_tables/V2.40.0__acl_latest_refactored.sql
@@ -7,9 +7,15 @@ CREATE OR REPLACE DYNAMIC TABLE ACL_LATEST
     AS 
     WITH dedup_acl AS (
         SELECT
-            *,
-            parse_json(resource_access) as acl,
-        FROM {{database_name}}.SYNAPSE_RAW.ACLSNAPSHOTS --noqa: TMP
+            CHANGE_TIMESTAMP,
+            CHANGE_TYPE,
+            CREATED_ON,
+            OWNER_ID,
+            OWNER_TYPE,
+            SNAPSHOT_DATE,
+            SNAPSHOT_TIMESTAMP,
+            RESOURCE_ACCESS
+        FROM synapse_data_warehouse.SYNAPSE_RAW.ACLSNAPSHOTS --noqa: TMP
         WHERE
             SNAPSHOT_DATE >= CURRENT_TIMESTAMP - INTERVAL '14 days'
         AND
@@ -19,27 +25,27 @@ CREATE OR REPLACE DYNAMIC TABLE ACL_LATEST
                 PARTITION BY OWNER_ID
                 ORDER BY CHANGE_TIMESTAMP DESC, SNAPSHOT_TIMESTAMP DESC
             ) = 1
-)
-SELECT
-    CHANGE_TIMESTAMP,
-    CHANGE_TYPE,
-    CREATED_ON,
-    OWNER_ID,
-    OWNER_TYPE,
-    SNAPSHOT_DATE,
-    SNAPSHOT_TIMESTAMP,
-    COALESCE(
-        array_sort(value:"accesstype"::variant),
-        array_sort(value:"accessType"::variant),
-        array_sort(value:"accesstype#1"::variant),
-        array_sort(value:"accesstype#2"::variant),
-        array_sort(value:"accesstype#3"::variant)
-    ) AS access_type,
-    COALESCE(
-        value:"principalId"::number,
-        value:"principalid"::number,
-        value:"principalid#1"::number
-    ) AS principal_id
-FROM 
-    dedup_acl,
-    LATERAL FLATTEN(acl, outer=>TRUE)
+    )
+    SELECT
+        CHANGE_TIMESTAMP,
+        CHANGE_TYPE,
+        CREATED_ON,
+        OWNER_ID,
+        OWNER_TYPE,
+        SNAPSHOT_DATE,
+        SNAPSHOT_TIMESTAMP,
+        COALESCE(
+            array_sort(value:"accesstype"::variant),
+            array_sort(value:"accessType"::variant),
+            array_sort(value:"accesstype#1"::variant),
+            array_sort(value:"accesstype#2"::variant),
+            array_sort(value:"accesstype#3"::variant)
+        ) AS access_type,
+        COALESCE(
+            value:"principalId"::number,
+            value:"principalid"::number,
+            value:"principalid#1"::number
+        ) AS principal_id
+    FROM 
+        dedup_acl,
+        LATERAL FLATTEN(PARSE_JSON(RESOURCE_ACCESS));
