bf6cb416a19702ab8f7eda204cd90fe45f6d0c72
Dan Lu <90745557+danlu1@users.noreply.github.com>
2025-02-28 13:33:01 -0800

    [SNOW-253] update table comments for userprofile_latest, file_latest, and teammember_latest dynamic tables (#158)
    * update table comments for userprofile_latest, file_latest, and teammember_latest dynamic tables

---
+++ synapse_data_warehouse/synapse/dynamic_tables/V2.43.0__update_dynamic_table_comments.sql
@@ -0,0 +1,9 @@
+-- Update table comments for userprofile_latest, file_latest, and teammember_latest dynamic table
+USE SCHEMA {{database_name}}.synapse; --noqa: JJ01,PRS,TMP
+
+-- Table comments
+ALTER DYNAMIC TABLE USERPROFILE_LATEST SET COMMENT = 'This dynamic table, indexed by the ID column, contains the most recent snapshot of user profiles. Since snapshotting does not capture DELETE events, records may still be retained if a user was deleted within the past 14 days.'; 
+
+ALTER DYNAMIC TABLE FILE_LATEST SET COMMENT = 'This dynamic table, indexed by the ID column, contains the most recent snapshot of file handles.';
+
+ALTER DYNAMIC TABLE TEAMMEMBER_LATEST SET COMMENT = 'This dynamic table, indexed by the TEAM_ID and MEMBER_ID columns, contains the most recent snapshot of team members. Since snapshotting does not capture DELETE events, records may still be retained if a team member was deleted within the past 14 days.';
