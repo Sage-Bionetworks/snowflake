944c34fef86b3d695619dccc16394ee6a6452f30
Dan Lu <90745557+danlu1@users.noreply.github.com>
2025-02-28 10:41:04 -0800

    Update V2.43.0__update_dynamic_table_comments.sql
    Reorder the sentence.

--- synapse_data_warehouse/synapse/dynamic_tables/V2.43.0__update_dynamic_table_comments.sql
+++ synapse_data_warehouse/synapse/dynamic_tables/V2.43.0__update_dynamic_table_comments.sql
@@ -2,7 +2,7 @@
 USE SCHEMA {{database_name}}.synapse; --noqa: JJ01,PRS,TMP
 
 -- Table comments
-ALTER DYNAMIC TABLE USERPROFILE_LATEST SET COMMENT = 'This dynamic table contains the most recent snapshot of user profiles from the past 14 days. The latest snapshots are determined by the most recent CHANGE_TIMESTAMP and SNAPSHOT_TIMESTAMP. If multiple snapshots exist within this period, only the latest snapshot for each ID is retained. For easier querying, NULL values in the LOCATION, COMPANY, POSITION, and INDUSTRY columns are replaced with empty strings. Since snapshotting does not capture DELETE changes, records may still be retained if a user was deleted within the past 14 days.'; 
+ALTER DYNAMIC TABLE USERPROFILE_LATEST SET COMMENT = 'This dynamic table contains the most recent snapshot of user profiles from the past 14 days. The latest snapshots are determined by the most recent CHANGE_TIMESTAMP and SNAPSHOT_TIMESTAMP. If multiple snapshots exist within this period, only the latest snapshot for each ID is retained. Since snapshotting does not capture DELETE changes, records may still be retained if a user was deleted within the past 14 days. For easier querying, NULL values in the LOCATION, COMPANY, POSITION, and INDUSTRY columns are replaced with empty strings.'; 
 
 ALTER DYNAMIC TABLE FILE_LATEST SET COMMENT = 'This dynamic table contains the most recent snapshot of files from the past 30 days. The latest snapshots are determined by the most recent CHANGE_TIMESTAMP and SNAPSHOT_TIMESTAMP. If multiple snapshots exist within this period, only the latest snapshot for each ID is retained.';
 
