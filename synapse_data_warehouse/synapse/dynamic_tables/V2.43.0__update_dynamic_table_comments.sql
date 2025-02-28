ef5befdde590bf302a03d7026be9361a971f330c
Dan Lu <90745557+danlu1@users.noreply.github.com>
2025-02-28 12:41:20 -0800

    Update V2.43.0__update_dynamic_table_comments.sql

--- synapse_data_warehouse/synapse/dynamic_tables/V2.43.0__update_dynamic_table_comments.sql
+++ synapse_data_warehouse/synapse/dynamic_tables/V2.43.0__update_dynamic_table_comments.sql
@@ -2,8 +2,8 @@
 USE SCHEMA {{database_name}}.synapse; --noqa: JJ01,PRS,TMP
 
 -- Table comments
-ALTER DYNAMIC TABLE USERPROFILE_LATEST SET COMMENT = 'This dynamic table, indexed by the ID column, contains the most recent snapshot of user profiles. Since snapshotting does not capture DELETE changes events, records may still be retained if a user was deleted within the past 14 days.'; 
+ALTER DYNAMIC TABLE USERPROFILE_LATEST SET COMMENT = 'This dynamic table, indexed by the ID column, contains the most recent snapshot of user profiles. Since snapshotting does not capture DELETE events, records may still be retained if a user was deleted within the past 14 days.'; 
 
 ALTER DYNAMIC TABLE FILE_LATEST SET COMMENT = 'This dynamic table, indexed by the ID column, contains the most recent snapshot of file handles.';
 
-ALTER DYNAMIC TABLE TEAMMEMBER_LATEST SET COMMENT = 'This dynamic table, indexed by the TEAM_ID and MEMBER_ID columns, contains the most recent snapshot of team members. Since snapshotting does not capture DELETE changes events, records may still be retained if a team member was deleted within the past 14 days.';
+ALTER DYNAMIC TABLE TEAMMEMBER_LATEST SET COMMENT = 'This dynamic table, indexed by the TEAM_ID and MEMBER_ID columns, contains the most recent snapshot of team members. Since snapshotting does not capture DELETE events, records may still be retained if a team member was deleted within the past 14 days.';
