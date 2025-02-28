1a197d58ffcceb69cb60ee66f4e9ad9706db564c
danlu1 <dan.lu@sagebase.org>
2025-02-28 12:39:39 -0800

    modify comments as PR comments

--- synapse_data_warehouse/synapse/dynamic_tables/V2.43.0__update_dynamic_table_comments.sql
+++ synapse_data_warehouse/synapse/dynamic_tables/V2.43.0__update_dynamic_table_comments.sql
@@ -2,8 +2,8 @@
 USE SCHEMA {{database_name}}.synapse; --noqa: JJ01,PRS,TMP
 
 -- Table comments
-ALTER DYNAMIC TABLE USERPROFILE_LATEST SET COMMENT = 'This dynamic table contains the most recent snapshot of user profiles from the past 14 days. The latest snapshots are determined by the most recent CHANGE_TIMESTAMP and SNAPSHOT_TIMESTAMP. If multiple snapshots exist within this period, only the latest snapshot for each ID is retained. Since snapshotting does not capture DELETE changes, records may still be retained if a user was deleted within the past 14 days. For easier querying, NULL values in the LOCATION, COMPANY, POSITION, and INDUSTRY columns are replaced with empty strings.'; 
+ALTER DYNAMIC TABLE USERPROFILE_LATEST SET COMMENT = 'This dynamic table, indexed by the ID column, contains the most recent snapshot of user profiles. Since snapshotting does not capture DELETE changes events, records may still be retained if a user was deleted within the past 14 days.'; 
 
-ALTER DYNAMIC TABLE FILE_LATEST SET COMMENT = 'This dynamic table contains the most recent snapshot of files from the past 30 days. The latest snapshots are determined by the most recent CHANGE_TIMESTAMP and SNAPSHOT_TIMESTAMP. If multiple snapshots exist within this period, only the latest snapshot for each ID is retained.';
+ALTER DYNAMIC TABLE FILE_LATEST SET COMMENT = 'This dynamic table, indexed by the ID column, contains the most recent snapshot of file handles.';
 
-ALTER DYNAMIC TABLE TEAMMEMBER_LATEST SET COMMENT = 'This dynamic table contains the most recent snapshot of team members from the past 14 days. The latest snapshots are determined by the most recent CHANGE_TIMESTAMP and SNAPSHOT_TIMESTAMP. If multiple snapshots exist within this period, only the latest snapshot for each TEAM_ID and MEMBER_ID pair is retained. Since snapshotting does not capture DELETE changes, records may still be retained if a team member was deleted within the past 14 days.';
+ALTER DYNAMIC TABLE TEAMMEMBER_LATEST SET COMMENT = 'This dynamic table, indexed by the TEAM_ID and MEMBER_ID columns, contains the most recent snapshot of team members. Since snapshotting does not capture DELETE changes events, records may still be retained if a team member was deleted within the past 14 days.';
