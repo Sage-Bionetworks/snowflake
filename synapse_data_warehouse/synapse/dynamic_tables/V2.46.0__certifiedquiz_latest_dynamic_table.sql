117a054fe777ef0ae44cb711bf6737c1f6d458f4
rxu17 <26471741+rxu17@users.noreply.github.com>
2025-03-14 01:41:56 -0700

    initial creation of certifiedquiz_latest dynamic table

---
+++ synapse_data_warehouse/synapse/dynamic_tables/V2.46.0__certifiedquiz_latest_dynamic_table.sql
@@ -0,0 +1,50 @@
+USE SCHEMA {{database_name}}.SYNAPSE; --noqa: JJ01,PRS,TMP
+
+CREATE OR REPLACE DYNAMIC TABLE CERTIFIEDQUIZ_LATEST_DYNAMIC
+    TARGET_LAG = '1 day'
+    WAREHOUSE = compute_xsmall
+    COMMENT = 'This table, indexed by USER_ID, contains the latest snapshots of the certification quiz submissions by a Synapse user.'
+    (
+        CHANGE_TYPE VARCHAR(10) COMMENT 'The change type is always as CREATE since each instance of a user submitting a quiz results in a new submission of the quiz.',
+        CHANGE_TIMESTAMP TIMESTAMP_NTZ(9) COMMENT 'The time when the user submitted the quiz.',
+        SNAPSHOT_TIMESTAMP TIMESTAMP_NTZ(9) COMMENT 'The time when the snapshot was taken (It is usually after the change happened).',
+        RESPONSE_ID NUMBER(38,0) COMMENT 'The unique identifier of a response wherein a user submitted a set of answers while participating in the quiz.',
+        USER_ID NUMBER(38,0) COMMENT 'The unique identifier of the user who submitted the quiz.',
+        PASSED BOOLEAN COMMENT 'If true, the user passed the quiz.',
+        PASSED_ON TIMESTAMP_NTZ(9) COMMENT 'The date on which the user submit the quiz, regardless of whether user passed or failed the test.',
+        STACK VARCHAR(10) COMMENT 'The stack (prod, dev) on which the quiz record was processed.',
+        INSTANCE VARCHAR(10) COMMENT 'The version of the stack that processed the quiz record.',
+        SNAPSHOT_DATE DATE COMMENT 'The snapshot_timestamp field is converted into a date and stored in the snapshot_date field.',
+        REVOKED BOOLEAN COMMENT 'If true, the record was revoked by an ACT member.',
+        REVOKED_ON TIMESTAMP_NTZ(9) COMMENT 'The date/time when the record was revoked, can be null if the record was never revoked.',
+        CERTIFIED BOOLEAN COMMENT 'If true the user is certified through this record, can be true if passed is true and revoked is false.'
+    )
+    AS
+        WITH latest_unique_rows AS (
+            SELECT
+                CHANGE_TYPE,
+                CHANGE_TIMESTAMP,
+                SNAPSHOT_TIMESTAMP,
+                RESPONSE_ID,
+                USER_ID,
+                PASSED,
+                PASSED_ON,
+                STACK,
+                INSTANCE,
+                SNAPSHOT_DATE,
+                REVOKED,
+                REVOKED_ON,
+                CERTIFIED
+            FROM
+                {{database_name}}.SYNAPSE_RAW.CERTIFIEDQUIZSNAPSHOTS --noqa: TMP
+            WHERE
+                SNAPSHOT_TIMESTAMP >= CURRENT_TIMESTAMP - INTERVAL '14 DAYS'
+            QUALIFY ROW_NUMBER() OVER (
+                    PARTITION BY USER_ID
+                    ORDER BY INSTANCE DESC, RESPONSE_ID DESC
+                ) = 1
+        )
+        SELECT
+            *
+        FROM
+            latest_unique_rows;
