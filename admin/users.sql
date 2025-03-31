382086a12853dbafaae502f280b302fb112ea05e
Phil Snyder <subfivemile@gmail.com>
2025-03-31 15:21:56 -0700

    Merge main back into dev (#188)
    * Disable secondary roles by default for all users
    
    PR feedback
    
    * Quote user identifier
    
    * Clean users (#168)
    
    * Include passphrase in env for Snowflake CLI
    
    * Convert username to lowercase
    
    * Comment out create user instead of previous commit
    
    * Only ALTER ROLE if it exists
    
    * Rewrite proxy admin role creation
    
    * Update references to old proxy admin role
    
    * remove snowsql variable sub indicator
    
    * Fix casing of user name
    
    * bug fixes in admin/grants.sql
    
    * Increase max cluster count for compute_xsmall
    
    * Add in serghei
    
    ---------
    
    Co-authored-by: Thomas Yu <thomas.yu@sagebase.org>

--- admin/users.sql
+++ admin/users.sql
@@ -13,7 +13,9 @@ CREATE USER IF NOT EXISTS "jay.hodgson@sagebase.org";
 CREATE USER IF NOT EXISTS "nick.grosenbacher@sagebase.org";
 CREATE USER IF NOT EXISTS "khai.do@sagebase.org";
 CREATE USER IF NOT EXISTS "thomas.schaffter@sagebase.org";
-CREATE USER IF NOT EXISTS "joni.harker@sagebase.org";
+-- This user is managed by Jumpcloud, thus they exist outside
+-- the usual user creation process.
+-- CREATE USER IF NOT EXISTS "joni.harker@sagebase.org";
 
 // Cancer Bio
 CREATE USER IF NOT EXISTS "adam.taylor@sagebase.org";
@@ -22,7 +24,6 @@ CREATE USER IF NOT EXISTS "xindi.guo@sagebase.org";
 CREATE USER IF NOT EXISTS "alexander.paynter@sagebase.org";
 CREATE USER IF NOT EXISTS "angie.bowen@sagebase.org";
 CREATE USER IF NOT EXISTS "ashley.clayton@sagebase.org";
-CREATE USER IF NOT EXISTS "lakaija.johnson@sagebase.org";
 CREATE USER IF NOT EXISTS "amber.nelson@sagebase.org";
 CREATE USER IF NOT EXISTS "tiara.adams@sagebase.org";
 CREATE USER IF NOT EXISTS "jessica.vera@sagebase.org";
@@ -32,13 +33,11 @@ CREATE USER IF NOT EXISTS "james.moon@sagebase.org";
 CREATE USER IF NOT EXISTS "orion.banks@sagebase.org";
 
 // ADTR
-CREATE USER IF NOT EXISTS "abby.vanderlinden@sagebase.org";
 CREATE USER IF NOT EXISTS "victor.baham@sagebase.org";
 CREATE USER IF NOT EXISTS "jessica.malenfant@sagebase.org";
 CREATE USER IF NOT EXISTS "jessica.britton@sagebase.org";
 CREATE USER IF NOT EXISTS "zoe.leanza@sagebase.org";
 CREATE USER IF NOT EXISTS "milan.vu@sagebase.org";
-CREATE USER IF NOT EXISTS "richard.yaxley@sagebase.org";
 CREATE USER IF NOT EXISTS "william.poehlman@sagebase.org";
 CREATE USER IF NOT EXISTS "jo.scanlan@sagebase.org";
 CREATE USER IF NOT EXISTS "trisha.zintel@sagebase.org";
@@ -55,7 +54,6 @@ CREATE USER IF NOT EXISTS "aditi.gopalan@sagebase.org";
 // Digital Health
 CREATE USER IF NOT EXISTS "solly.sieberts@sagebase.org";
 CREATE USER IF NOT EXISTS "meghasyam@sagebase.org";
-CREATE USER IF NOT EXISTS "pranav.anbarasu@sagebase.org";
 CREATE USER IF NOT EXISTS "elias.chaibub.neto@sagebase.org";
 CREATE USER IF NOT EXISTS "arti.singh@sagebase.org";
 CREATE USER IF NOT EXISTS "sonia.carlson@sagebase.org";
@@ -75,14 +73,12 @@ CREATE USER IF NOT EXISTS "verena.chung@sagebase.org";
 CREATE USER IF NOT EXISTS "rchai@sagebase.org";
 CREATE USER IF NOT EXISTS "maria.diaz@sagebase.org";
 CREATE USER IF NOT EXISTS "gaia.andreoletti@sagebase.org";
+CREATE USER IF NOT EXISTS "serghei.mangul@sagebase.org";
 
-// FAIR
+// TECH
 CREATE USER IF NOT EXISTS "anthony.williams@sagebase.org";
 CREATE USER IF NOT EXISTS "loren.wolfe@sagebase.org";
-CREATE USER IF NOT EXISTS "lingling.peng@sagebase.org";
-CREATE USER IF NOT EXISTS "gianna.jordan@sagebase.org";
 CREATE USER IF NOT EXISTS "mieko.hashimoto@sagebase.org";
-CREATE USER IF NOT EXISTS "andrew.lamb@sagebase.org";
 CREATE USER IF NOT EXISTS "milen.nikolov@sagebase.org";
 CREATE USER IF NOT EXISTS "amy.heiser@sagebase.org";
 
@@ -95,20 +91,24 @@ CREATE USER IF NOT EXISTS "jenny.medina@sagebase.org";
 CREATE USER IF NOT EXISTS "phil.snyder@sagebase.org";
 CREATE USER IF NOT EXISTS "sophia.jobe@sagebase.org";
 CREATE USER IF NOT EXISTS "dan.lu@sagebase.org";
+CREATE USER IF NOT EXISTS "lingling.peng@sagebase.org";
+CREATE USER IF NOT EXISTS "gianna.jordan@sagebase.org";
+CREATE USER IF NOT EXISTS "andrew.lamb@sagebase.org";
 
 // LT
-CREATE USER IF NOT EXISTS "kim.baggett@sagebase.org";
 CREATE USER IF NOT EXISTS "luca.foschini@sagebase.org";
 CREATE USER IF NOT EXISTS "alberto.pepe@sagebase.org";
 CREATE USER IF NOT EXISTS "susheel.varma@sagebase.org";
 CREATE USER IF NOT EXISTS "christine.suver@sagebase.org";
-CREATE USER IF NOT EXISTS "anna.greenwood@sagebase.org";
 CREATE USER IF NOT EXISTS "mackenzie.wildman@sagebase.org";
-CREATE USER IF NOT EXISTS "drew.duglan@sagebase.org";
 CREATE USER IF NOT EXISTS "andrea.varsavsky@sagebase.org";
 
 // finance
 CREATE USER IF NOT EXISTS "brandon.morgan@sagebase.org";
+CREATE USER IF NOT EXISTS "barry.webb@sagebase.org";
+CREATE USER IF NOT EXISTS "sarah.mansfield@sagebase.org";
+CREATE USER IF NOT EXISTS "ranell.nystrom@sagebase.org";
+
 
 // SERVICE users
 CREATE USER IF NOT EXISTS DBT_SERVICE
@@ -127,7 +127,41 @@ CREATE USER IF NOT EXISTS RECOVER_SERVICE
     MUST_CHANGE_PASSWORD = TRUE;
 
 CREATE USER IF NOT EXISTS ADMIN_SERVICE
-    TYPE = SERVICE;
+    TYPE = SERVICE; --noqa: LT02, PRS
 
 CREATE USER IF NOT EXISTS DEVELOPER_SERVICE
     TYPE = SERVICE;
+
+-- Set DEFAULT_SECONDARY_ROLES to [] (do not use secondary roles by default)
+-- for all users
+EXECUTE IMMEDIATE $$
+DECLARE
+    updated_users ARRAY DEFAULT ARRAY_CONSTRUCT(); -- users we updated
+    username STRING; -- a user identifier
+    dsr STRING; -- default secondary role setting
+    user_cursor CURSOR FOR 
+        SELECT "name", "default_secondary_roles" 
+        FROM TABLE(RESULT_SCAN(LAST_QUERY_ID())) 
+        WHERE "name" <> 'SNOWFLAKE'; -- A cursor over our users
+BEGIN
+    SHOW USERS;
+    OPEN user_cursor;
+    LOOP
+        FETCH user_cursor INTO username, dsr;
+
+        -- exit condition
+        IF (username IS NULL) THEN
+            BREAK;
+        END IF;
+
+        -- loop condition
+        IF (dsr != '[]') THEN
+            LET quoted_username STRING := '"' || :username || '"';
+            ALTER USER IDENTIFIER(:quoted_username) SET DEFAULT_SECONDARY_ROLES=();
+            updated_users := ARRAY_APPEND(updated_users, username);
+        END IF;
+    END LOOP;
+    CLOSE user_cursor;
+    RETURN updated_users;
+END;
+$$;
