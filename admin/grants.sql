66380aa5d7e3223674d763c4b114ec867f55de7a
Thomas Yu <thomas.yu@sagebase.org>
2025-04-22 21:26:55 -0700

    Create users

--- admin/grants.sql
+++ admin/grants.sql
@@ -184,6 +184,9 @@ GRANT ROLE DATA_ANALYTICS TO USER "hayley.sanchez@sagebase.org";
 GRANT ROLE DATA_ANALYTICS TO USER "bishoy.kamel@sagebase.org";
 GRANT ROLE DATA_ANALYTICS TO USER "andrea.varsavsky@sagebase.org";
 GRANT ROLE DATA_ANALYTICS TO USER "serghei.mangul@sagebase.org";
+GRANT ROLE DATA_ANALYTICS TO USER "jaclyn.beck@sagebase.org";
+GRANT ROLE DATA_ANALYTICS TO USER "ziwei.pan@sagebase.org";
+
 GRANT ROLE DATA_ANALYTICS TO USER AD_SERVICE;
 
 // Leaders
