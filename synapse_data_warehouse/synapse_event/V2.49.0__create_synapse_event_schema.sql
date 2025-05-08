f94ff51acaa11a840f3bd0dbb54fa2b80516f4ef
Phil Snyder <subfivemile@gmail.com>
2025-05-08 10:06:12 -0700

    Modify comment of synapse_event schema

--- synapse_data_warehouse/synapse_event/V2.49.0__create_synapse_event_schema.sql
+++ synapse_data_warehouse/synapse_event/V2.49.0__create_synapse_event_schema.sql
@@ -1,4 +1,4 @@
 USE DATABASE {{ database_name }}; --noqa: JJ01,PRS,TMP
 
 CREATE OR REPLACE SCHEMA SYNAPSE_EVENT
-    COMMENT = 'A schema for Synapse event data. Event data is derived from raw data by retaining only the most recent snapshot for each index.';
+    COMMENT = 'Event data is derived from raw data by retaining only the most recent snapshot for each distinct event. The identifier of an event is determined by a subset of columns which uniquely identifies the occurrence or instance of an event.';
