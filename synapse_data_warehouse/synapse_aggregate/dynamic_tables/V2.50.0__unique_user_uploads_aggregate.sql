ecd91c41bea3760a9b02ac4a774a91cf86334922
Jenny V Medina <jenny.medina@sagebase.org>
2025-05-13 21:18:46 -0400

    [SNOW-258] Introduce O1KR1 Aggregate Table (#201)
    * introduce sql script
    
    * fix columns, descriptions, and table description
    
    * target_lag typo\
    
    * rename to year/month/day for now
    
    * create a subquery for MAX agg in WHERE clause
    
    * bump up version
    
    * programmatically feed db names
    
    * out of scope: grant read visibility across entire db to clone admin role
    
    * oos: no such thing as synapse_raw_all_analyst
    
    * oos: fix typo on future dynamic table grant and comment out extra step
    
    * remove step granting all_analyst db role to de
    
    * declare composite pk
    
    * remove inline pk calls
    
    * attempt inline pk declaration
    
    * declare pk in column comments
    
    * update agg period start/stop column comments
    
    * remove extra comma
    
    * attempt inline pk declaration
    
    * remove inline attempt
    
    * rename columns with agg_ prefix
    
    * finish table and col renaming
    
    * agg_period_end
    
    * new quarterly rollup and new ordering
    
    * remove order by in final query
    
    * reorder cols. use QUARTER
    
    * fix order in table declaration
    
    * fix indentation
    
    * remove comma
    
    * add ALL_TIME grain. attempt ordering again
    
    * remove ordering again. ordering in cte doesnt fix it. grand total -> all time

---
+++ synapse_data_warehouse/synapse_aggregate/dynamic_tables/V2.50.0__unique_user_uploads_aggregate.sql
@@ -0,0 +1,105 @@
+USE SCHEMA {{database_name}}.synapse_aggregate; --noqa: JJ01,PRS,TMP
+
+CREATE OR REPLACE DYNAMIC TABLE USER_UPLOADS
+    (
+        AGG_PERIOD VARCHAR(16777216) COMMENT 'The dimension of the aggregate (e.g., YEARLY, MONTHLY, DAILY). Part of composite PK.',
+        AGG_YEAR NUMBER(38,0) COMMENT 'Year of the aggregation period.',
+        AGG_QUARTER NUMBER(38,0) COMMENT 'Quarter of the aggregation period.',
+        AGG_MONTH NUMBER(38,0) COMMENT 'Month of the aggregation period.',
+        AGG_DAY NUMBER(38,0) COMMENT 'Day of the aggregation period.',
+        AGG_PERIOD_START DATE COMMENT 'The start date of the aggregation period. Part of composite PK.',
+        AGG_PERIOD_END DATE COMMENT 'The stop date of the aggregation period. Part of composite PK.',
+        AGG_PERIOD_IS_COMPLETE BOOLEAN COMMENT 'If true, then the aggregation period is complete.',
+        USER_COUNT INT COMMENT 'The number of distinct unique users uploading during the aggregation period.'
+    )
+    TARGET_LAG = '1 day'
+    WAREHOUSE = compute_xsmall
+    COMMENT = 'This table shows the total number of unique users uploading to Synapse across yearly, quarterly, monthly, and daily aggregate periods.'
+    AS
+    -- Step 1: Compute the aggregate user count and extract the aggregate dimensions
+    WITH user_count_rollup AS (
+
+        SELECT
+            -- Extracting our aggregate dimensions from ``record_date`` column
+            YEAR(record_date)  AS agg_year,
+            QUARTER(record_date) AS agg_quarter,
+            MONTH(record_date) AS agg_month,
+            DAY(record_date)   AS agg_day,
+
+            -- Computing our aggregate
+            COUNT(DISTINCT user_id) AS user_count,
+
+            -- Use GROUPING to determine which dimensions were rolled up for each row 
+            GROUPING(agg_day)   AS g_day,
+            GROUPING(agg_month) AS g_month,
+            GROUPING(agg_quarter) AS g_quarter,
+            GROUPING(agg_year)  AS g_year
+        FROM {{database_name}}.SYNAPSE.FILEUPLOAD --noqa: JJ01,PRS,TMP
+        -- exclude today’s partial data to avoid confusion in the final table
+        WHERE RECORD_DATE < (SELECT MAX(RECORD_DATE) FROM {{database_name}}.SYNAPSE.FILEUPLOAD) --noqa: JJ01,PRS,TMP
+        GROUP BY ROLLUP(agg_year, agg_quarter, agg_month, agg_day)
+
+        ),
+    -- Step 2: Determine the aggregate period (grain) and start and end dates
+    agg_period_calculations AS (
+
+        SELECT
+
+            -- 1. Grab the relevant original columns
+            user_count,
+            agg_year,
+            agg_quarter,
+            agg_month,
+            agg_day,
+
+            -- 2. Create `agg_period` column...
+            --    Determine granularity based on which dimensions were rolled up
+            CASE
+                WHEN g_year = 1 AND g_quarter = 1 AND g_month = 1 AND g_day = 1 THEN 'ALL TIME'
+                WHEN g_year = 0 AND g_quarter = 1 AND g_month = 1 AND g_day = 1 THEN 'YEARLY'
+                WHEN g_year = 0 AND g_quarter = 0 AND g_month = 1 AND g_day = 1 THEN 'QUARTERLY'
+                WHEN g_year = 0 AND g_quarter = 0 AND g_month = 0 AND g_day = 1 THEN 'MONTHLY'
+                WHEN g_day = 0 THEN 'DAILY'
+            END AS agg_period,
+
+            -- 3. Create `agg_period_start` column
+            CASE
+                -- First day of the year
+                WHEN g_year = 0 AND g_quarter = 1 AND g_month = 1 AND g_day = 1 THEN DATE_FROM_PARTS(agg_year, 1, 1)
+                -- First day of the quarter
+                WHEN g_year = 0 AND g_quarter = 0 AND g_month = 1 AND g_day = 1 THEN DATE_FROM_PARTS(agg_year, (agg_quarter * 3) - 2, 1)
+                -- First day of the month
+                WHEN g_year = 0 AND g_quarter = 0 AND g_month = 0 AND g_day = 1 THEN DATE_FROM_PARTS(agg_year, agg_month, 1)
+                -- Date
+                WHEN g_day = 0 THEN DATE_FROM_PARTS(agg_year, agg_month, agg_day)
+            END AS agg_period_start,
+
+            -- 4. Create `agg_period_end` column (inclusive)
+            CASE
+                -- Last day of the year
+                WHEN g_year = 0 AND g_quarter = 1 AND g_month = 1 AND g_day = 1 THEN LAST_DAY(DATE_FROM_PARTS(agg_year, 1, 1), 'YEAR')
+                -- Last day of the quarter
+                WHEN g_year = 0 AND g_quarter = 0 AND g_month = 1 AND g_day = 1 THEN LAST_DAY(DATE_FROM_PARTS(agg_year, agg_quarter * 3, 1))
+                -- Last day of the month
+                WHEN g_year = 0 AND g_quarter = 0 AND g_month = 0 AND g_day = 1 THEN LAST_DAY(DATE_FROM_PARTS(agg_year, agg_month, 1))
+                -- Date
+                WHEN g_day = 0 THEN DATE_FROM_PARTS(agg_year, agg_month, agg_day)
+            END AS agg_period_end
+
+        FROM user_count_rollup)
+    -- Step 3: Compose the final table by ordering the columns for easier reading and adding the completion column
+    SELECT
+        agg_period,
+        agg_year,
+        agg_quarter,
+        agg_month,
+        agg_day,
+        agg_period_start,
+        agg_period_end,
+    
+        -- 5. Mark the period as complete once today's date is past the stop
+        (CURRENT_DATE > agg_period_end) AS agg_period_is_complete,
+
+        user_count
+
+    FROM agg_period_calculations;
