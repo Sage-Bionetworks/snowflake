2a152d0b1cced225a20b322c8d403e205797e78a
Phil Snyder <subfivemile@gmail.com>
2025-05-23 11:13:07 -0700

    PR feedback

--- synapse_data_warehouse/synapse_event/dynamic_tables/V2.53.0__node_event_table.sql
+++ synapse_data_warehouse/synapse_event/dynamic_tables/V2.53.0__node_event_table.sql
@@ -6,32 +6,31 @@ CREATE OR REPLACE DYNAMIC TABLE node_event
     AS
         WITH unique_events AS (
             SELECT
+                ID,
+                VERSION_NUMBER,
                 CHANGE_TYPE,
                 CHANGE_TIMESTAMP,
                 CHANGE_USER_ID,
-                SNAPSHOT_TIMESTAMP,
-                ID,
+                NODE_TYPE,
+                PARENT_ID,
                 BENEFACTOR_ID,
                 PROJECT_ID,
-                PARENT_ID,
-                NODE_TYPE,
                 CREATED_ON,
                 CREATED_BY,
                 MODIFIED_ON,
                 MODIFIED_BY,
-                VERSION_NUMBER,
                 FILE_HANDLE_ID,
                 NAME,
                 IS_PUBLIC,
                 IS_CONTROLLED,
                 IS_RESTRICTED,
-                SNAPSHOT_DATE,
                 EFFECTIVE_ARS,
                 ANNOTATIONS,
+                INTERNAL_ANNOTATIONS,
                 DERIVED_ANNOTATIONS,
                 VERSION_COMMENT,
                 VERSION_LABEL,
-                ALIAS,
+                VERSION_HISTORY,
                 ACTIVITY_ID,
                 COLUMN_MODEL_IDS,
                 SCOPE_IDS,
@@ -39,9 +38,10 @@ CREATE OR REPLACE DYNAMIC TABLE node_event
                 REFERENCE,
                 IS_SEARCH_ENABLED,
                 DEFINING_SQL,
-                INTERNAL_ANNOTATIONS,
-                VERSION_HISTORY,
-                PROJECT_STORAGE_USAGE
+                ALIAS,
+                PROJECT_STORAGE_USAGE,
+                SNAPSHOT_DATE,
+                SNAPSHOT_TIMESTAMP
             FROM
                 {{ database_name }}.synapse_raw.nodesnapshots --noqa: TMP
             WHERE
@@ -50,7 +50,7 @@ CREATE OR REPLACE DYNAMIC TABLE node_event
                 and change_type is not null
             QUALIFY ROW_NUMBER() OVER (
                     -- `id` and `version_number` provide at least one record for each Synapse entity version.
-                    -- `change_type` ensures that simultaneous CREATE+UPDATE events are both retained.
+                    -- `change_type` ensures that simultaneous events are retained, regardless of their `change_type`.
                     -- `modified_on` enables us to capture updates to Synapse Table entities and other
                     -- update scenarios where the `version_number` does not change (like when we update annotations
                     -- without incrementing the version). 
@@ -60,41 +60,6 @@ CREATE OR REPLACE DYNAMIC TABLE node_event
                 ) = 1
         )
         SELECT
-            ID,
-            VERSION_NUMBER,
-            CHANGE_TYPE,
-            CHANGE_TIMESTAMP,
-            CHANGE_USER_ID,
-            NODE_TYPE,
-            PARENT_ID,
-            BENEFACTOR_ID,
-            PROJECT_ID,
-            CREATED_ON,
-            CREATED_BY,
-            MODIFIED_ON,
-            MODIFIED_BY,
-            FILE_HANDLE_ID,
-            NAME,
-            IS_PUBLIC,
-            IS_CONTROLLED,
-            IS_RESTRICTED,
-            EFFECTIVE_ARS,
-            ANNOTATIONS,
-            INTERNAL_ANNOTATIONS,
-            DERIVED_ANNOTATIONS,
-            VERSION_COMMENT,
-            VERSION_LABEL,
-            VERSION_HISTORY,
-            ACTIVITY_ID,
-            COLUMN_MODEL_IDS,
-            SCOPE_IDS,
-            ITEMS,
-            REFERENCE,
-            IS_SEARCH_ENABLED,
-            DEFINING_SQL,
-            ALIAS,
-            PROJECT_STORAGE_USAGE,
-            SNAPSHOT_DATE,
-            SNAPSHOT_TIMESTAMP
+            *
         FROM
             unique_events;
