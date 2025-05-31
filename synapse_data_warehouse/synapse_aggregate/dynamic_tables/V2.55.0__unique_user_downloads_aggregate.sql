98c06e4e737e2dae09452cb69d548cace52f82f8
Phil Snyder <subfivemile@gmail.com>
2025-05-30 19:12:03 -0700

    Rollup by grouping sets

--- synapse_data_warehouse/synapse_aggregate/dynamic_tables/V2.55.0__unique_user_downloads_aggregate.sql
+++ synapse_data_warehouse/synapse_aggregate/dynamic_tables/V2.55.0__unique_user_downloads_aggregate.sql
@@ -1,105 +1,180 @@
 USE SCHEMA {{database_name}}.synapse_aggregate; --noqa: JJ01,PRS,TMP
 
--- CREATE OR REPLACE DYNAMIC TABLE DOWNLOAD 
---     (
---         AGG_PERIOD VARCHAR(16777216) COMMENT 'The dimension of the aggregate (e.g., YEARLY, MONTHLY, DAILY).',
---         AGG_YEAR NUMBER(38,0) COMMENT 'Primary key (composite). Year of the aggregation period.',
---         AGG_QUARTER NUMBER(38,0) COMMENT 'Primary key (composite). Quarter of the aggregation period.',
---         AGG_MONTH NUMBER(38,0) COMMENT 'Primary key (composite). Month of the aggregation period.',
---         AGG_DAY NUMBER(38,0) COMMENT 'Primary key (composite). Day of the aggregation period.',
---         AGG_PERIOD_START DATE COMMENT 'The start date of the aggregation period.',
---         AGG_PERIOD_END DATE COMMENT 'The stop date of the aggregation period.',
---         AGG_PERIOD_IS_COMPLETE BOOLEAN COMMENT 'If true, then the aggregation period is complete.',
---         USERS INT COMMENT 'The number of unique users that have generated a pre-signed URL for this entity during the aggregation period. This is an approximation of a download.'
---     )
---     TARGET_LAG = '1 day'
---     WAREHOUSE = compute_xsmall
---     COMMENT = 'This table shows the total number of unique users uploading to Synapse across yearly, quarterly, monthly, and daily aggregate periods.'
---     AS
---     -- Step 1: Compute the aggregate user count and extract the aggregate dimensions
---     WITH user_count_rollup AS (
+CREATE OR REPLACE DYNAMIC TABLE DOWNLOAD_AGGREGATE 
+    (
+        AGG_PERIOD VARCHAR COMMENT 'The dimension of the aggregate (e.g., YEARLY, MONTHLY, DAILY).',
+        AGG_YEAR NUMBER COMMENT 'Primary key (composite). Year of the aggregation period.',
+        AGG_QUARTER NUMBER COMMENT 'Primary key (composite). Quarter of the aggregation period.',
+        AGG_MONTH NUMBER COMMENT 'Primary key (composite). Month of the aggregation period.',
+        AGG_DAY NUMBER COMMENT 'Primary key (composite). Day of the aggregation period.',
+        AGG_PROJECT_ID NUMBER COMMENT 'Primary key (composite). The unique identifier of the Synapse project where the entity or object resides. Applicable only when `object_type` is FileEntity or TableEntity.',
+        AGG_OBJECT_TYPE VARCHAR COMMENT 'Primary key (composite). The type of the Synapse entity or object.',
+        AGG_OBJECT_ID NUMBER COMMENT 'Primary key (composite). The unique identifier of the Synapse entity or object.',
+        AGG_PERIOD_START DATE COMMENT 'The start date of the aggregation period.',
+        AGG_PERIOD_END DATE COMMENT 'The stop date of the aggregation period.',
+        AGG_PERIOD_IS_COMPLETE BOOLEAN COMMENT 'If true, then the aggregation period is complete.',
+        USER_DOWNLOAD_COUNT NUMBER COMMENT 'The number of unique users that have generated a pre-signed URL for this entity during the aggregation period. This approximates a download.'
+    )
+    TARGET_LAG = '1 day'
+    WAREHOUSE = compute_xsmall
+    COMMENT = 'This table contains download aggregates across yearly, quarterly, monthly, and daily periods. Aggregates for these periods are computed for most combinations of the project, object type, and object identifier dimensions. Some combinations are omitted because they are either nonsensical or redundant with a more specific combination of dimensions.'
+    AS
+    -- Step 1: Compute the aggregate user count and extract the aggregate dimensions
+    WITH user_download_rollup AS (
 
---         SELECT
---             -- Extracting our aggregate dimensions from ``record_date`` column
---             YEAR(record_date)  AS agg_year,
---             QUARTER(record_date) AS agg_quarter,
---             MONTH(record_date) AS agg_month,
---             DAY(record_date)   AS agg_day,
+        SELECT
+            -- Extracting our aggregate dimensions from ``record_date`` column
+            YEAR(record_date)  AS agg_year,
+            QUARTER(record_date) AS agg_quarter,
+            MONTH(record_date) AS agg_month,
+            DAY(record_date)   AS agg_day,
 
---             -- Computing our aggregate
---             COUNT(DISTINCT user_id) AS user_count,
+            -- Use GROUPING to determine which dimensions were rolled up for each row 
+            GROUPING(agg_day)   AS g_day,
+            GROUPING(agg_month) AS g_month,
+            GROUPING(agg_quarter) AS g_quarter,
+            GROUPING(agg_year)  AS g_year,
 
---             -- Use GROUPING to determine which dimensions were rolled up for each row 
---             GROUPING(agg_day)   AS g_day,
---             GROUPING(agg_month) AS g_month,
---             GROUPING(agg_quarter) AS g_quarter,
---             GROUPING(agg_year)  AS g_year
---         FROM {{database_name}}.SYNAPSE_RAW.FILEUPLOAD --noqa: JJ01,PRS,TMP
---         -- exclude today’s partial data to avoid confusion in the final table
---         WHERE RECORD_DATE < (SELECT MAX(RECORD_DATE) FROM {{database_name}}.SYNAPSE_RAW.FILEUPLOAD) --noqa: JJ01,PRS,TMP
---         GROUP BY ROLLUP(agg_year, agg_quarter, agg_month, agg_day)
+            -- Additional rollup columns
+            project_id,
+            association_object_id,
+            association_object_type,
 
---         ),
---     -- Step 2: Determine the aggregate period (grain) and start and end dates
---     agg_period_calculations AS (
+            -- Computing our aggregate
+            COUNT(DISTINCT user_id) AS user_download_count
+        FROM {{database_name}}.synapse_event.filedownload --noqa: JJ01,PRS,TMP
+        -- exclude today’s partial data to avoid confusion in the final table
+        WHERE RECORD_DATE < (SELECT MAX(RECORD_DATE) FROM {{database_name}}.synapse_event.filedownload) --noqa: JJ01,PRS,TMP
+        GROUP BY
+            ROLLUP(agg_year, agg_quarter, agg_month, agg_day),
+            GROUPING SETS (
+                (),                                                 -- 1) aggregates across every object
+                (project_id),                                       -- 2) aggregates across every object in this project
+                (association_object_type),                          -- 3) aggregates across every object of this type
+                (project_id, association_object_type),              -- 4) aggregates across every object of this type in this project
+                (association_object_type, association_object_id),   -- 5) aggregates across projects for this object
+                (project_id, association_object_type, association_object_id)  -- 6) aggregates for a specific object in a specific project
+            )
+        ORDER BY
+            agg_year, agg_quarter, agg_month, agg_day,
+            project_id, association_object_type, association_object_id
+    ),
+    -- Step 2: Determine the aggregate period (grain) and start and end dates
+    agg_period_calculations AS (
 
---         SELECT
+        SELECT
+            -- 1. Grab the relevant original columns
+            agg_year,
+            agg_quarter,
+            agg_month,
+            agg_day,
 
---             -- 1. Grab the relevant original columns
---             user_count,
---             agg_year,
---             agg_quarter,
---             agg_month,
---             agg_day,
+            -- 2. Create `agg_period` column...
+            --    Determine granularity based on which dimensions were rolled up
+            CASE
+                WHEN g_year = 1 AND g_quarter = 1 AND g_month = 1 AND g_day = 1 THEN 'ALL TIME'
+                WHEN g_year = 0 AND g_quarter = 1 AND g_month = 1 AND g_day = 1 THEN 'YEARLY'
+                WHEN g_year = 0 AND g_quarter = 0 AND g_month = 1 AND g_day = 1 THEN 'QUARTERLY'
+                WHEN g_year = 0 AND g_quarter = 0 AND g_month = 0 AND g_day = 1 THEN 'MONTHLY'
+                WHEN g_day = 0 THEN 'DAILY'
+            END AS agg_period,
 
---             -- 2. Create `agg_period` column...
---             --    Determine granularity based on which dimensions were rolled up
---             CASE
---                 WHEN g_year = 1 AND g_quarter = 1 AND g_month = 1 AND g_day = 1 THEN 'ALL TIME'
---                 WHEN g_year = 0 AND g_quarter = 1 AND g_month = 1 AND g_day = 1 THEN 'YEARLY'
---                 WHEN g_year = 0 AND g_quarter = 0 AND g_month = 1 AND g_day = 1 THEN 'QUARTERLY'
---                 WHEN g_year = 0 AND g_quarter = 0 AND g_month = 0 AND g_day = 1 THEN 'MONTHLY'
---                 WHEN g_day = 0 THEN 'DAILY'
---             END AS agg_period,
+            -- 3. Create `agg_period_start` column
+            CASE
+                -- First day of the year
+                WHEN g_year = 0 AND g_quarter = 1 AND g_month = 1 AND g_day = 1 THEN DATE_FROM_PARTS(agg_year, 1, 1)
+                -- First day of the quarter
+                WHEN g_year = 0 AND g_quarter = 0 AND g_month = 1 AND g_day = 1 THEN DATE_FROM_PARTS(agg_year, (agg_quarter * 3) - 2, 1)
+                -- First day of the month
+                WHEN g_year = 0 AND g_quarter = 0 AND g_month = 0 AND g_day = 1 THEN DATE_FROM_PARTS(agg_year, agg_month, 1)
+                -- Date
+                WHEN g_day = 0 THEN DATE_FROM_PARTS(agg_year, agg_month, agg_day)
+            END AS agg_period_start,
 
---             -- 3. Create `agg_period_start` column
---             CASE
---                 -- First day of the year
---                 WHEN g_year = 0 AND g_quarter = 1 AND g_month = 1 AND g_day = 1 THEN DATE_FROM_PARTS(agg_year, 1, 1)
---                 -- First day of the quarter
---                 WHEN g_year = 0 AND g_quarter = 0 AND g_month = 1 AND g_day = 1 THEN DATE_FROM_PARTS(agg_year, (agg_quarter * 3) - 2, 1)
---                 -- First day of the month
---                 WHEN g_year = 0 AND g_quarter = 0 AND g_month = 0 AND g_day = 1 THEN DATE_FROM_PARTS(agg_year, agg_month, 1)
---                 -- Date
---                 WHEN g_day = 0 THEN DATE_FROM_PARTS(agg_year, agg_month, agg_day)
---             END AS agg_period_start,
+            -- 4. Create `agg_period_end` column (inclusive)
+            CASE
+                -- Last day of the year
+                WHEN g_year = 0 AND g_quarter = 1 AND g_month = 1 AND g_day = 1 THEN LAST_DAY(DATE_FROM_PARTS(agg_year, 1, 1), 'YEAR')
+                -- Last day of the quarter
+                WHEN g_year = 0 AND g_quarter = 0 AND g_month = 1 AND g_day = 1 THEN LAST_DAY(DATE_FROM_PARTS(agg_year, agg_quarter * 3, 1))
+                -- Last day of the month
+                WHEN g_year = 0 AND g_quarter = 0 AND g_month = 0 AND g_day = 1 THEN LAST_DAY(DATE_FROM_PARTS(agg_year, agg_month, 1))
+                -- Date
+                WHEN g_day = 0 THEN DATE_FROM_PARTS(agg_year, agg_month, agg_day)
+            END AS agg_period_end,
+            project_id,
+            association_object_id,
+            association_object_type,
+            user_download_count
+        FROM user_download_rollup
+        -- Drop records which contain (association_object_type, association_object_id) aggregates for those
+        -- project-specific objects (e.g., file and table entities) which have only ever been downloaded
+        -- from a single project. Over 99% of aggregates over this grouping set are redundant because most
+        -- project-specific objects exist in just one project. This is a nearly 25% reduction in records!
+        WHERE
+            -- Our strategy here is to match any aggregates which fit the description above
+            -- and exclude them from the query results.
+            NOT (
+                association_object_id IS NOT NULL
+                AND association_object_type IS NOT NULL
+                AND project_id IS NULL
+                -- Up to this point, we are matching all (association_object_type, association_object_id) aggregates
+                AND NOT (
+                    -- Exclude from our match objects which have downloads across more than a single project
+                    -- and objects which don't associate with a project. This NOT (A OR B) approach requires our
+                    -- query to do set operations on 20x fewer records than the more direct approach.
+                    -- Check out De Morgan's law if u a real nerd and want to write this like (NOT A) AND (NOT B).
 
---             -- 4. Create `agg_period_end` column (inclusive)
---             CASE
---                 -- Last day of the year
---                 WHEN g_year = 0 AND g_quarter = 1 AND g_month = 1 AND g_day = 1 THEN LAST_DAY(DATE_FROM_PARTS(agg_year, 1, 1), 'YEAR')
---                 -- Last day of the quarter
---                 WHEN g_year = 0 AND g_quarter = 0 AND g_month = 1 AND g_day = 1 THEN LAST_DAY(DATE_FROM_PARTS(agg_year, agg_quarter * 3, 1))
---                 -- Last day of the month
---                 WHEN g_year = 0 AND g_quarter = 0 AND g_month = 0 AND g_day = 1 THEN LAST_DAY(DATE_FROM_PARTS(agg_year, agg_month, 1))
---                 -- Date
---                 WHEN g_day = 0 THEN DATE_FROM_PARTS(agg_year, agg_month, agg_day)
---             END AS agg_period_end
+                    -- Exclude from our match objects which have downloads across more than a single project.
+                    -- Objects which have downloads across more than one project will evaluate to TRUE
+                    -- Objects which have downloads in just one project will evaluate to FALSE
+                    -- Objects which don't associate with a project will evaluate to FALSE
+                    association_object_id IN (
+                        SELECT 
+                            association_object_id
+                        FROM 
+                            {{database_name}}.synapse_event.filedownload --noqa: JJ01,PRS,TMP
+                        WHERE
+                            project_id IS NOT NULL
+                        GROUP BY 
+                            association_object_id
+                        HAVING 
+                            COUNT(DISTINCT project_id) > 1
+                    )
 
---         FROM user_count_rollup)
---     -- Step 3: Compose the final table by ordering the columns for easier reading and adding the completion column
---     SELECT
---         agg_period,
---         agg_year,
---         agg_quarter,
---         agg_month,
---         agg_day,
---         agg_period_start,
---         agg_period_end,
-    
---         -- 5. Mark the period as complete once today's date is past the stop
---         (CURRENT_DATE > agg_period_end) AS agg_period_is_complete,
+                    OR 
 
---         user_count
-
---     FROM agg_period_calculations;
+                    -- Exclude from our match objects which don't associate with a project.
+                    -- Objects which have downloads across more than one project will evaluate to FALSE
+                    -- Objects which appear in just one project will evaluate to FALSE
+                    -- Objects which don't associate with a project will evaluate to TRUE.
+                    association_object_id IN (
+                        SELECT 
+                            distinct association_object_id
+                        FROM 
+                            {{database_name}}.synapse_event.filedownload --noqa: JJ01,PRS,TMP
+                        WHERE
+                            project_id IS NULL
+                    )
+                    -- Only objects which appear in a single project will evaluate to FALSE both times!
+                ) 
+            )
+            -- Hence they are the only (association_object_type, association_object_id) aggregates
+            -- which will be excluded from the results of this CTE.
+    )
+    -- Step 3: Compose the final table by ordering the columns for easier reading and adding the completion column
+    SELECT
+        agg_period,
+        agg_year,
+        agg_quarter,
+        agg_month,
+        agg_day,
+        agg_project_id,
+        association_object_type as agg_object_type,
+        association_object_id as agg_object_id,
+        agg_period_start,
+        agg_period_end,
+        -- 5. Mark the period as complete once today's date is past the stop
+        (CURRENT_DATE > agg_period_end) AS agg_period_is_complete,
+        user_download_count
+    FROM agg_period_calculations;
