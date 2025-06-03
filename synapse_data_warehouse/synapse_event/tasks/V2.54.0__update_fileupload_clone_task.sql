f50683e47e4f807f26bf3b079a93e24e32a6e676
Jenny Medina <medina.stsci@gmail.com>
2025-06-03 14:37:33 -0400

    typo: execute task

--- synapse_data_warehouse/synapse_event/tasks/V2.54.0__update_fileupload_clone_task.sql
+++ synapse_data_warehouse/synapse_event/tasks/V2.54.0__update_fileupload_clone_task.sql
@@ -18,7 +18,7 @@ alter task synapse_raw.fileupload_task resume;
 alter task synapse_raw.refresh_synapse_warehouse_s3_stage_task resume;
 
 -- Step 4) Execute the task of interest to generate the table
-execute synapse_raw.clone_fileupload_task;
+execute task synapse_raw.clone_fileupload_task;
 
 -- Step 4) Add a primary key to the event table
 alter table synapse_event.fileupload_event
