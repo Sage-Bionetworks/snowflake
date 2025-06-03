29b09bd226f1e59a52ee73bbd5fd77767367680b
Jenny Medina <medina.stsci@gmail.com>
2025-06-03 13:53:25 -0400

    syntax

--- synapse_data_warehouse/synapse_event/tasks/V2.54.0__update_fileupload_clone_task.sql
+++ synapse_data_warehouse/synapse_event/tasks/V2.54.0__update_fileupload_clone_task.sql
@@ -1,23 +1,23 @@
-use schema {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP
+use database {{database_name}}; --noqa: JJ01,PRS,TMP
 
 -- Step 1) Pause the upstream tasks & the task of interest before updating
-alter task refresh_synapse_warehouse_s3_stage_task suspend;
-alter task fileupload_task suspend;
-alter task clone_fileupload_task suspend;
+alter task synapse_raw.refresh_synapse_warehouse_s3_stage_task suspend;
+alter task synapse_raw.fileupload_task suspend;
+alter task synapse_raw.clone_fileupload_task suspend;
 
 -- Step 2) Replace the task of interest
-create or replace task clone_fileupload_task
-    after fileupload_task
-    as
-        create or replace table {{database_name}}.synapse_event.fileupload_event --noqa: TMP
-        clone fileupload;
+create or replace task synapse_raw.clone_fileupload_task
+after synapse_raw.fileupload_task
+as
+    create or replace table synapse_event.fileupload_event
+    clone synapse_raw.fileupload;
 
 -- Step 3) Resume the upstream tasks & the task of interest
-alter task clone_fileupload_task resume;
-alter task fileupload_task resume;
-alter task refresh_synapse_warehouse_s3_stage_task resume;
+alter task synapse_raw.clone_fileupload_task resume;
+alter task synapse_raw.fileupload_task resume;
+alter task synapse_raw.refresh_synapse_warehouse_s3_stage_task resume;
 
 -- Step 4) Add a primary key to the event table
-alter table {{database_name}}.synapse_event.fileupload_event --noqa: JJ01,PRS,TMP
-    add constraint fileupload_event_primary_key
-    primary key (user_id, file_handle_id);
+alter table synapse_event.fileupload_event
+add constraint fileupload_event_primary_key
+primary key (user_id, file_handle_id);
