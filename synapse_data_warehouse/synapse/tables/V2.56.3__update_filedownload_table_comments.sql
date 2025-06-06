b2263de172a29d2143942de7507a4b8dcd035baf
danlu1 <dan.lu@sagebase.org>
2025-06-06 15:09:42 -0700

    reformat comment

--- synapse_data_warehouse/synapse/tables/V2.56.3__update_filedownload_table_comments.sql
+++ synapse_data_warehouse/synapse/tables/V2.56.3__update_filedownload_table_comments.sql
@@ -3,4 +3,4 @@ USE SCHEMA {{database_name}}.synapse; --noqa: JJ01,PRS,TMP
 ALTER TABLE filedownload
   SET COMMENT = 
   '[DEPRECATION NOTICE] This table is being deprecated and will be replaced by ``synapse_event.objectdownload_event`` in the near future. Please transition any dependencies accordingly.
-    The table contain records of all the downloads of the Synapse, e.g., file, zip/package, attachments. The events are recorded only after the pre-signed url for requested download entity is generated.';
+  The table contain records of all the downloads of the Synapse, e.g., file, zip/package, attachments. The events are recorded only after the pre-signed url for requested download entity is generated.';
