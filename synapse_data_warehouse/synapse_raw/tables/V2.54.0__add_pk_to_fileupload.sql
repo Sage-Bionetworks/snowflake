f6663b6ca8879a7b829845a58e530d0027de343a
Jenny Medina <medina.stsci@gmail.com>
2025-06-05 20:21:33 -0400

    add PK to synapse_raw.fileupload

---
+++ synapse_data_warehouse/synapse_raw/tables/V2.54.0__add_pk_to_fileupload.sql
@@ -0,0 +1,16 @@
+use schema {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP
+
+-- Step 1) Add the composite primary key to the fileupload table
+alter table fileupload
+add constraint fileupload_pk
+primary key (user_id, file_handle_id, timestamp);
+
+-- Step 2: Update column comments for PK columns (one at a time)
+alter table fileupload
+alter COLUMN user_id set COMMENT = 'PRIMARY KEY (Composite). The id of the user who requested the upload.';
+
+alter table fileupload
+alter COLUMN file_handle_id set COMMENT = 'PRIMARY KEY (Composite). The unique identifier of the file handle.';
+
+alter table fileupload
+alter COLUMN timestamp set COMMENT = 'PRIMARY KEY (Composite). The time when the upload event is pushed to the queue, after a successful upload of a file or change in the existing table.';
