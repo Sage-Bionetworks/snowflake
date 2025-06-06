8d1af10a4fa39be3421dbda09f1d3a7eb31486b4
Jenny Medina <medina.stsci@gmail.com>
2025-06-05 20:40:15 -0400

    processedaccess does not get a PK

--- synapse_data_warehouse/synapse_raw/tables/V2.54.0__add_pk_to_fileupload.sql
+++ synapse_data_warehouse/synapse_raw/tables/V2.54.0__add_pk_to_fileupload.sql
@@ -1,15 +1,10 @@
 use schema {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP
 
--- Step 1) Add the composite primary key to the fileupload table
+-- Add the composite primary key & column commments to the fileupload table
 alter table fileupload
 add constraint fileupload_pk
 primary key (user_id, file_handle_id, timestamp);
 
--- Step 2) Update column comments for PK columns (one at a time)
 COMMENT ON COLUMN fileupload.user_id IS 'PRIMARY KEY (Composite). The id of the user who requested the upload.';
 COMMENT ON COLUMN fileupload.file_handle_id IS 'PRIMARY KEY (Composite). The unique identifier of the file handle.';
-COMMENT ON COLUMN timestamp IS 'PRIMARY KEY (Composite). The time when the upload event is pushed to the queue, after a successful upload of a file or change in the existing table.';
-
--- Step 3) Add the composite primary key to the processedaccess table
-
--- Step 4) Update column comments for PK columns (one at a time)
+COMMENT ON COLUMN fileupload.timestamp IS 'PRIMARY KEY (Composite). The time when the upload event is pushed to the queue, after a successful upload of a file or change in the existing table.';
