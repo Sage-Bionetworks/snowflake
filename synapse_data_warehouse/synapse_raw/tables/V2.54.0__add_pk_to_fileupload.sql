aba5b8187b014764b75d82e54da81d48fb0863dc
Jenny Medina <medina.stsci@gmail.com>
2025-06-05 20:36:29 -0400

    move file, add paccess comment

--- synapse_data_warehouse/synapse_raw/tables/V2.54.0__add_pk_to_fileupload.sql
+++ synapse_data_warehouse/synapse_raw/tables/V2.54.0__add_pk_to_fileupload.sql
@@ -9,3 +9,7 @@ primary key (user_id, file_handle_id, timestamp);
 COMMENT ON COLUMN fileupload.user_id IS 'PRIMARY KEY (Composite). The id of the user who requested the upload.';
 COMMENT ON COLUMN fileupload.file_handle_id IS 'PRIMARY KEY (Composite). The unique identifier of the file handle.';
 COMMENT ON COLUMN timestamp IS 'PRIMARY KEY (Composite). The time when the upload event is pushed to the queue, after a successful upload of a file or change in the existing table.';
+
+-- Step 3) Add the composite primary key to the processedaccess table
+
+-- Step 4) Update column comments for PK columns (one at a time)
