c23fcb702d32245a78061c64e114db7fe48c5891
Phil Snyder <subfivemile@gmail.com>
2025-06-11 14:55:32 -0700

    [SNOW-279] Create node_event table
    commit bfec74939888d51682b4514935424fa5ea28623f
    Author: Phil Snyder <subfivemile@gmail.com>
    Date:   Fri May 23 11:13:07 2025 -0700
    
        PR feedback
    
    commit 0677a3a9290801cb4ed4eb50c4620b2291aca4f4
    Author: Phil Snyder <subfivemile@gmail.com>
    Date:   Wed May 21 13:45:12 2025 -0700
    
        Rename table in CTE
    
    commit 122c96d4bd702c8f61a23cd15e38fe06b8fce8cb
    Author: Phil Snyder <subfivemile@gmail.com>
    Date:   Wed May 21 13:39:36 2025 -0700
    
        bump schemachange script version
    
    commit d9fc4b677cccbaf0c023693a20a451e8076aac94
    Author: Phil Snyder <subfivemile@gmail.com>
    Date:   Wed May 21 13:29:16 2025 -0700
    
        Modify partition columns of node_event table
    
    commit f41a38e92488933e70914730b265e7bcd6f838ae
    Author: Phil Snyder <subfivemile@gmail.com>
    Date:   Thu May 15 13:45:13 2025 -0700
    
        syntax fix
    
    commit aec9603b53be3cc851869d4349e1d97d63830939
    Author: Phil Snyder <subfivemile@gmail.com>
    Date:   Thu May 15 13:25:36 2025 -0700
    
        bump schemachange script version
    
    commit 5093b7b0702109b48d6dbbc1c26d744f0129d968
    Author: Phil Snyder <subfivemile@gmail.com>
    Date:   Thu May 15 13:22:54 2025 -0700
    
        Add change_type to partition criteria
    
    commit 6ccc18b60691b48d00ddc56439ab49f3a1a80e52
    Author: Phil Snyder <subfivemile@gmail.com>
    Date:   Wed May 14 14:43:07 2025 -0700
    
        syntax fix
    
    commit ae2dd4d34c7cbe2b32748ffeda057f5a59411cc8
    Author: Phil Snyder <subfivemile@gmail.com>
    Date:   Wed May 14 12:46:01 2025 -0700
    
        Add comments to node_event table
    
    commit 46874548a576c8cf324c5c0331a274e8ebc8f7cd
    Author: Phil Snyder <subfivemile@gmail.com>
    Date:   Wed May 14 12:10:07 2025 -0700
    
        add node_event table

---
+++ synapse_data_warehouse/synapse_event/dynamic_tables/V2.53.0__node_event_table.sql
@@ -0,0 +1,65 @@
+USE SCHEMA {{ database_name }}.SYNAPSE_EVENT;
+
+CREATE OR REPLACE DYNAMIC TABLE node_event
+    TARGET_LAG = '1 day'
+    WAREHOUSE = compute_xsmall
+    AS
+        WITH unique_events AS (
+            SELECT
+                ID,
+                VERSION_NUMBER,
+                CHANGE_TYPE,
+                MODIFIED_ON,
+                MODIFIED_BY,
+                NAME,
+                NODE_TYPE,
+                PARENT_ID,
+                BENEFACTOR_ID,
+                PROJECT_ID,
+                CREATED_ON,
+                CREATED_BY,
+                FILE_HANDLE_ID,
+                IS_PUBLIC,
+                IS_CONTROLLED,
+                IS_RESTRICTED,
+                EFFECTIVE_ARS,
+                ANNOTATIONS,
+                INTERNAL_ANNOTATIONS,
+                DERIVED_ANNOTATIONS,
+                VERSION_COMMENT,
+                VERSION_LABEL,
+                VERSION_HISTORY,
+                ACTIVITY_ID,
+                COLUMN_MODEL_IDS,
+                SCOPE_IDS,
+                ITEMS,
+                REFERENCE,
+                IS_SEARCH_ENABLED,
+                DEFINING_SQL,
+                ALIAS,
+                PROJECT_STORAGE_USAGE,
+                CHANGE_TIMESTAMP,
+                CHANGE_USER_ID,
+                SNAPSHOT_DATE,
+                SNAPSHOT_TIMESTAMP
+            FROM
+                {{ database_name }}.synapse_raw.nodesnapshots --noqa: TMP
+            WHERE
+                -- only `id`, `change_*`, and `snapshot_*` columns are defined regardless of `change_type`
+                id is not null
+                and change_type is not null
+            QUALIFY ROW_NUMBER() OVER (
+                    -- `id` and `version_number` provide at least one record for each Synapse entity version.
+                    -- `change_type` ensures that simultaneous events are retained, regardless of their `change_type`.
+                    -- `modified_on` enables us to capture updates to Synapse Table entities and other
+                    -- update scenarios where the `version_number` does not change (like when we update annotations
+                    -- without incrementing the version). 
+                    PARTITION BY id, version_number, change_type, modified_on
+                    -- Ordering by `snapshot_timestamp` ascending means we keep the first snapshot of this event
+                    ORDER BY snapshot_timestamp
+                ) = 1
+        )
+        SELECT
+            *
+        FROM
+            unique_events;
