0031e748ffdf51fc485efa3c66dc7f584bbdeafd
Jenny Medina <medina.stsci@gmail.com>
2025-07-10 11:11:10 -0400

    use schema instead of db

--- synapse_data_warehouse/synapse_raw/tasks/V2.60.1__task_for_table_desc_fileupload_processedaccess.sql
+++ synapse_data_warehouse/synapse_raw/tasks/V2.60.1__task_for_table_desc_fileupload_processedaccess.sql
@@ -1,21 +1,21 @@
-USE DATABASE {{database_name}};  -- noqa: JJ01,PRS,TMP
+USE SCHEMA {{database_name}}.synapse_raw;  -- noqa: JJ01,PRS,TMP
 
 -- Step 1) Pause the entire task tree
-ALTER TASK synapse_raw.refresh_synapse_warehouse_s3_stage_task SUSPEND;
-ALTER TASK synapse_raw.fileupload_task SUSPEND;
-ALTER TASK synapse_raw.processedaccess_task SUSPEND;
-ALTER TASK synapse_raw.clone_fileupload_task SUSPEND;
-ALTER TASK synapse_raw.clone_process_access_task SUSPEND;
+ALTER TASK refresh_synapse_warehouse_s3_stage_task SUSPEND;
+ALTER TASK fileupload_task SUSPEND;
+ALTER TASK processedaccess_task SUSPEND;
+ALTER TASK clone_fileupload_task SUSPEND;
+ALTER TASK clone_process_access_task SUSPEND;
 
 -- Step 2) Add deprecation notice to clone_fileupload_task
-CREATE OR REPLACE TASK synapse_raw.clone_fileupload_task
-  AFTER synapse_raw.fileupload_task
+CREATE OR REPLACE TASK clone_fileupload_task
+  AFTER fileupload_task
 AS
   EXECUTE IMMEDIATE $$
-    CREATE OR REPLACE TABLE synapse.fileupload
-      CLONE synapse_raw.fileupload;
+    CREATE OR REPLACE TABLE {{database_name}}.synapse.fileupload -- noqa: JJ01,PRS,TMP
+      CLONE fileupload;
     
-    ALTER TABLE synapse.fileupload
+    ALTER TABLE {{database_name}}.synapse.fileupload -- noqa: JJ01,PRS,TMP
       SET COMMENT =
       '[DEPRECATION NOTICE] This table is being deprecated, and therefore is no longer actively updated. It will be replaced by ``synapse_event.fileupload_event`` in the near future. Please transition any dependencies accordingly.
 
@@ -23,14 +23,14 @@ AS
   $$;
 
 -- Step 3) Add deprecation notice to clone_process_access_task
-CREATE OR REPLACE TASK synapse_raw.clone_process_access_task
-  AFTER synapse_raw.processedaccess_task
+CREATE OR REPLACE TASK clone_process_access_task
+  AFTER processedaccess_task
 AS
   EXECUTE IMMEDIATE $$
-    CREATE OR REPLACE TABLE synapse.processedaccess
-      CLONE synapse_raw.processedaccess;
+    CREATE OR REPLACE TABLE {{database_name}}.synapse.processedaccess -- noqa: JJ01,PRS,TMP
+      CLONE processedaccess;
     
-    ALTER TABLE synapse.processedaccess
+    ALTER TABLE {{database_name}}.synapse.processedaccess -- noqa: JJ01,PRS,TMP
       SET COMMENT =
       '[DEPRECATION NOTICE] This table is being deprecated, and therefore is no longer actively updated. It will be replaced by ``synapse_event.access_event`` in the near future. Please transition any dependencies accordingly.
 
@@ -38,8 +38,8 @@ AS
   $$;
 
 -- Step 4) Resume everything
-ALTER TASK synapse_raw.refresh_synapse_warehouse_s3_stage_task RESUME;
-ALTER TASK synapse_raw.fileupload_task RESUME;
-ALTER TASK synapse_raw.processedaccess_task RESUME;
-ALTER TASK synapse_raw.clone_fileupload_task RESUME;
-ALTER TASK synapse_raw.clone_process_access_task RESUME;
+ALTER TASK refresh_synapse_warehouse_s3_stage_task RESUME;
+ALTER TASK fileupload_task RESUME;
+ALTER TASK processedaccess_task RESUME;
+ALTER TASK clone_fileupload_task RESUME;
+ALTER TASK clone_process_access_task RESUME;
