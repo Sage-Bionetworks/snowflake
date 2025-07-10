653340f1a90d61109262b407b0b97601a9011e7c
Jenny Medina <medina.stsci@gmail.com>
2025-07-10 17:42:30 -0400

    use exec imm dynamic statement, 2 ddls

--- synapse_data_warehouse/synapse_raw/tasks/V2.60.1__task_for_table_desc_fileupload_processedaccess.sql
+++ synapse_data_warehouse/synapse_raw/tasks/V2.60.1__task_for_table_desc_fileupload_processedaccess.sql
@@ -11,29 +11,33 @@ ALTER TASK synapse_raw.clone_process_access_task SUSPEND;
 CREATE OR REPLACE TASK synapse_raw.clone_fileupload_task
   AFTER synapse_raw.fileupload_task
 AS
-BEGIN
-    CREATE OR REPLACE TABLE synapse.fileupload
-        CLONE synapse_raw.fileupload;
-    
-    ALTER TABLE synapse.fileupload
-        SET COMMENT = '[DEPRECATION NOTICE] This table is being deprecated, and therefore is no longer actively updated. It will be replaced by ``synapse_event.fileupload_event`` in the near future. Please transition any dependencies accordingly.
+EXECUTE IMMEDIATE $$
+    BEGIN
+        CREATE OR REPLACE TABLE synapse.fileupload
+            CLONE synapse_raw.fileupload;
+        
+        ALTER TABLE synapse.fileupload
+            SET COMMENT = '[DEPRECATION NOTICE] This table is being deprecated, and therefore is no longer actively updated. It will be replaced by ``synapse_event.fileupload_event`` in the near future. Please transition any dependencies accordingly.
 
-                        This table contains upload records for FileEntity (e.g. a new file creation, upload or update to an existing file) and TableEntity (e.g. an appended row set to an existing table, uploaded file to an existing table). The events are recorded only after the file or change to a table is successfully uploaded.';
-END;
+                            This table contains upload records for FileEntity (e.g. a new file creation, upload or update to an existing file) and TableEntity (e.g. an appended row set to an existing table, uploaded file to an existing table). The events are recorded only after the file or change to a table is successfully uploaded.';
+    END;
+$$;
 
 -- Step 3) Add deprecation notice to clone_process_access_task
 CREATE OR REPLACE TASK synapse_raw.clone_process_access_task
   AFTER synapse_raw.processedaccess_task
 AS
-BEGIN
-    CREATE OR REPLACE TABLE synapse.processedaccess
-        CLONE synapse_raw.processedaccess;
-    
-    ALTER TABLE synapse.processedaccess
-        SET COMMENT = '[DEPRECATION NOTICE] This table is being deprecated, and therefore is no longer actively updated. It will be replaced by ``synapse_event.access_event`` in the near future. Please transition any dependencies accordingly.
+EXECUTE IMMEDIATE $$
+    BEGIN
+        CREATE OR REPLACE TABLE synapse.processedaccess
+            CLONE synapse_raw.processedaccess;
+        
+        ALTER TABLE synapse.processedaccess
+            SET COMMENT = '[DEPRECATION NOTICE] This table is being deprecated, and therefore is no longer actively updated. It will be replaced by ``synapse_event.access_event`` in the near future. Please transition any dependencies accordingly.
 
-                       The table contains access records. Each record reflects a single API request received by the Synapse server. The recorded data is useful for audits and to analyse API performance such as delays, errors or success rates.';
-END;
+                        The table contains access records. Each record reflects a single API request received by the Synapse server. The recorded data is useful for audits and to analyse API performance such as delays, errors or success rates.';
+    END;
+$$;
 
 -- Step 4) Resume everything
 ALTER TASK synapse_raw.clone_process_access_task RESUME;
