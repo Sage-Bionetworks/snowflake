e6fb05e9b2e3743f9728c4b130c21c94e6ab3d8b
Jenny V Medina <jenny.medina@sagebase.org>
2025-10-31 13:44:39 -0400

    remove comma

--- synapse_data_warehouse/synapse_raw/tables/V5.00.0__create_temp_table.sql
+++ synapse_data_warehouse/synapse_raw/tables/V5.00.0__create_temp_table.sql
@@ -3,5 +3,5 @@ USE SCHEMA {{database_name}}.synapse_raw; --noqa: JJ01,PRS,TMP
 -- * SNOW-11 add user profile snapshots
 CREATE TABLE IF NOT EXISTS temp_validation_table (
     temp_validation_type STRING,
-    temp_validation_id NUMBER,
+    temp_validation_id NUMBER
 );
