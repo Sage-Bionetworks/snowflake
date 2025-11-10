e9eadf3783a2b378687761d5f70436b15ffce898
Phil Snyder <subfivemile@gmail.com>
2025-11-10 15:06:30 -0800

    Move application role grants to dedicated deploy step

--- admin/grants.sql
+++ admin/grants.sql
@@ -798,15 +798,7 @@ GRANT ROLE SAGE_CITATIONS_ADMIN
 	TO ROLE SAGE_ADMIN;
 
 -- GOOGLE_ANALYTICS grants
--- Gives the admin of the GOOGLE_ANALYTICS_AGGREGATE schema the
--- privileges to invoke any stored procedures (like CONFIGURE_REPORT)
-GRANT APPLICATION ROLE GOOGLE_ANALYTICS.ADMIN
-	TO ROLE GOOGLE_ANALYTICS_AGGREGATE_ADMIN;
--- Developers should have view privileges upon the application
--- Developers won't be able to see procedures because that requires USAGE
-GRANT APPLICATION ROLE GOOGLE_ANALYTICS.VIEWER
-	TO ROLE DATA_ENGINEER;
--- The usual DB admin > schema admin > analyst hierarchy
+-- The usual DB admin > schema admin
 GRANT ROLE GOOGLE_ANALYTICS_AGGREGATE_ADMIN
 	TO ROLE SAGE_ADMIN;
 -- Grant the analyst role to our developers and analysts
