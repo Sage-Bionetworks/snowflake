2687e5f28bec58700554bbe5db02959c06a752af
rxu17 <26471741+rxu17@users.noreply.github.com>
2025-12-15 19:11:31 -0800

    Revert "Merge branch 'main' into gen-2263-add-genie-test-db"
    This reverts commit 5ac69d11b81f38a9f7403d50dc75442f2724afb0, reversing
    changes made to 90e1fab3a170b0677c9f9d230a0121cd0f95be3d.

--- admin/warehouses/V1.21.0__increase_max_cluster_count.sql
+++
@@ -1,4 +0,0 @@
--- A temporary increase in max clusters to accomodate Snowflake workshop --
-USE ROLE SYSADMIN;
-ALTER WAREHOUSE IF EXISTS COMPUTE_XSMALL
-    SET MAX_CLUSTER_COUNT = 50;
--- sage/citations/V1.0.0__datacite_doi_table.sql
+++ sage/citations/V1.0.0__datacite_doi_table.sql
@@ -1,12 +1,13 @@
 USE SCHEMA {{ database_name }}.citations;
 
-CREATE OR REPLACE TABLE synapse_doi_raw (
+-- do stuff
+CREATE TABLE synapse_doi_raw (
   RECORD_ID VARCHAR DEFAULT UUID_STRING() NOT NULL UNIQUE COMMENT 'AUTO-GENERATED. Unique UUID4 identifier.',
   CREATED_ON TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP COMMENT 'AUTO-GENERATED. When this record was added to this table.',
   SOURCE_TYPE VARCHAR COMMENT 'Identifies which pipeline or tool generated the data. Possible values: ID_MINER, PUBMED_CRAWLER, DATACITE.',
   TARGET_SYNAPSE_ID VARCHAR COMMENT 'Links the DOI data to a specific Synapse entity.',
   RAW_DATA VARIANT COMMENT 'Stores the complete, original JSON data provided by the source.'
 );
-COMMENT ON TABLE synapse_doi_raw IS 'This table contains Synapse DOI data. DOI data is collected from three potential sources: ID_MINER, PUBMED_CRAWLER, and DATACITE.
+COMMENT ON TABLE synapse_doi IS 'This table contains Synapse DOI data. DOI data is collected from three potential sources: ID_MINER, PUBMED_CRAWLER, and DATACITE.
 
 Records are appended for each run of a source\'s associated workflow. The data is intended to be deduplicated before being used in any analyses.';
