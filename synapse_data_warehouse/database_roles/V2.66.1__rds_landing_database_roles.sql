8e8b4ef2a77291bfe81fbe4b0c2421682b214a5d
Jenny Medina <medina.stsci@gmail.com>
2026-01-29 10:41:00 -0500

    assign ownership of archetype database roles accordingly

--- synapse_data_warehouse/database_roles/V2.66.1__rds_landing_database_roles.sql
+++ synapse_data_warehouse/database_roles/V2.66.1__rds_landing_database_roles.sql
@@ -1,3 +1,6 @@
+-- Steps are created following the RBAC design pattern for schemas documented here:
+-- https://sagebionetworks.jira.com/wiki/spaces/DPE/pages/4115366084/Managing+Object+Privileges#Schemas
+
 USE DATABASE {{ database_name }}; --noqa: JJ01,PRS,TMP
 
 -----------------------------------------------
@@ -9,3 +12,13 @@ CREATE OR REPLACE DATABASE ROLE RDS_LANDING_ALL_ADMIN;
 CREATE OR REPLACE DATABASE ROLE RDS_LANDING_ALL_ANALYST;
 -- <SCHEMA>_ALL_DEVELOPER database role which will have read access to all the objects
 CREATE OR REPLACE DATABASE ROLE RDS_LANDING_ALL_DEVELOPER;
+
+
+-------------------------------------------------------------------------------------------
+-- Step 2) Assign ownership between these database roles and the appropriate account roles:
+-------------------------------------------------------------------------------------------
+-- <SCHEMA>_ALL_ADMIN database role owns the other archetype database roles
+GRANT OWNERSHIP ON DATABASE ROLE RDS_LANDING_ALL_ANALYST TO DATABASE ROLE RDS_LANDING_ALL_ADMIN;
+GRANT OWNERSHIP ON DATABASE ROLE RDS_LANDING_ALL_DEVELOPER TO DATABASE ROLE RDS_LANDING_ALL_ADMIN;
+-- <DATABASE>_PROXY_ADMIN account role owns the <SCHEMA>_ALL_ADMIN database role
+GRANT OWNERSHIP ON DATABASE ROLE RDS_LANDING_ALL_ADMIN TO ROLE {{ database_name }}_PROXY_ADMIN;
