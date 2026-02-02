fb35118cd7ce69de43d417eb03b0944c3d702008
Jenny Medina <medina.stsci@gmail.com>
2026-02-02 10:28:37 -0500

    remove support for stages

--- admin/future_grants/V1.26.0__rds_raw_future_tables.sql
+++ admin/future_grants/V1.26.0__rds_raw_future_tables.sql
@@ -3,56 +3,36 @@
 -- https://sagebionetworks.jira.com/wiki/spaces/DPE/pages/4115366084/Managing+Object+Privileges#Schemas
 
 --------------------------------------------------------------------------------------------------------
--- Step 9) Grant ownership of future tables/stages in PROD database to <SCHEMA>_ALL_ADMIN database role:
+-- Step 9) Grant ownership of future tables in PROD database to <SCHEMA>_ALL_ADMIN database role:
 --------------------------------------------------------------------------------------------------------
 GRANT OWNERSHIP
 	ON FUTURE TABLES
 	IN SCHEMA SYNAPSE_DATA_WAREHOUSE.RDS_RAW
 	TO DATABASE ROLE SYNAPSE_DATA_WAREHOUSE.RDS_RAW_ALL_ADMIN;
 
-GRANT OWNERSHIP
-    ON FUTURE STAGES
-    IN SCHEMA SYNAPSE_DATA_WAREHOUSE.RDS_RAW
-    TO DATABASE ROLE SYNAPSE_DATA_WAREHOUSE.RDS_RAW_ALL_ADMIN;
-
 
 --------------------------------------------------------------------------------------------------------
--- Step 10) Grant ownership of future tables/stages in DEV database to <SCHEMA>_ALL_ADMIN database role:
+-- Step 10) Grant ownership of future tables in DEV database to <SCHEMA>_ALL_ADMIN database role:
 --------------------------------------------------------------------------------------------------------
 GRANT OWNERSHIP
 	ON FUTURE TABLES
 	IN SCHEMA SYNAPSE_DATA_WAREHOUSE_DEV.RDS_RAW
 	TO DATABASE ROLE SYNAPSE_DATA_WAREHOUSE_DEV.RDS_RAW_ALL_ADMIN;
 
-GRANT OWNERSHIP
-    ON FUTURE STAGES
-    IN SCHEMA SYNAPSE_DATA_WAREHOUSE_DEV.RDS_RAW
-    TO DATABASE ROLE SYNAPSE_DATA_WAREHOUSE_DEV.RDS_RAW_ALL_ADMIN;
-
 
 -----------------------------------------------------------------------------------------------------------------
--- Step 11) Grant read access on future tables/stages in PROD database to appropriate read access database roles:
+-- Step 11) Grant read access on future tables in PROD database to appropriate read access database roles:
 -----------------------------------------------------------------------------------------------------------------
 GRANT SELECT, REFERENCES
 	ON FUTURE TABLES
 	IN SCHEMA SYNAPSE_DATA_WAREHOUSE.RDS_RAW
 	TO DATABASE ROLE SYNAPSE_DATA_WAREHOUSE.RDS_RAW_TABLE_READ;
 
-GRANT USAGE, READ
-	ON FUTURE STAGES
-	IN SCHEMA SYNAPSE_DATA_WAREHOUSE.RDS_RAW
-	TO DATABASE ROLE SYNAPSE_DATA_WAREHOUSE.RDS_RAW_STAGE_READ;
-
 
 -----------------------------------------------------------------------------------------------------------------
--- Step 12) Grant read access on future tables/stages in DEV database to appropriate read access database roles:
+-- Step 12) Grant read access on future tables in DEV database to appropriate read access database roles:
 -----------------------------------------------------------------------------------------------------------------
 GRANT SELECT, REFERENCES
 	ON FUTURE TABLES
 	IN SCHEMA SYNAPSE_DATA_WAREHOUSE_DEV.RDS_RAW
 	TO DATABASE ROLE SYNAPSE_DATA_WAREHOUSE_DEV.RDS_RAW_TABLE_READ;
-
-GRANT USAGE, READ
-	ON FUTURE STAGES
-	IN SCHEMA SYNAPSE_DATA_WAREHOUSE_DEV.RDS_RAW
-	TO DATABASE ROLE SYNAPSE_DATA_WAREHOUSE_DEV.RDS_RAW_STAGE_READ;
