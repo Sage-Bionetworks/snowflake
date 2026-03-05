ba347a02cf35e617a66fcbfae388db9322e4a24f
Jenny Medina <medina.stsci@gmail.com>
2026-03-05 13:22:16 -0500

    fix missing ; in syntax

--- admin/integrations.sql
+++ admin/integrations.sql
@@ -5,13 +5,13 @@ CREATE OR REPLACE STORAGE INTEGRATION synapse_snapshots_dev
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = 'S3'
   ENABLED = TRUE
-  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::766808016710:role/snowflake-role-synapse-snowflake-rds-snapshots-dev'
+  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::766808016710:role/snowflake-role-synapse-snowflake-rds-snapshots-dev';
 
 CREATE OR REPLACE STORAGE INTEGRATION synapse_snapshots_prod
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = 'S3'
   ENABLED = TRUE
-  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::766808016710:role/snowflake-role-synapse-snowflake-rds-snapshots-prod'
+  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::766808016710:role/snowflake-role-synapse-snowflake-rds-snapshots-prod';
 
 -- * Integration to prod (SNOW-14)
 CREATE STORAGE INTEGRATION IF NOT EXISTS synapse_prod_warehouse_s3
