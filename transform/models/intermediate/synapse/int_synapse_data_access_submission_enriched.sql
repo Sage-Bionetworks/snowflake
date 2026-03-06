e1108a6b44a90b261db4d2860a5263f465a63a05
Phil Snyder <subfivemile@gmail.com>
2026-03-06 13:39:32 -0800

    PR feedback: consolidate CTE statements

--- transform/models/intermediate/synapse/int_synapse_data_access_submission_enriched.sql
+++ transform/models/intermediate/synapse/int_synapse_data_access_submission_enriched.sql
@@ -18,23 +18,12 @@ WITH base AS (
         data_access_submission_raw
     FROM {{ ref('int_synapse_data_access_submission') }}
 ),
-created_by_names AS (
+principal_usernames AS (
     SELECT
-        base.data_access_submission_id,
-        principal_alias.alias_display AS created_by_user_name
-    FROM base 
-    LEFT JOIN {{ ref('stg_synapse__principal_alias') }} principal_alias
-        ON base.created_by = principal_alias.principal_id
-    where principal_alias.alias_type = 'USER_NAME'
-),
-state_modified_by_names AS (
-    SELECT
-        base.data_access_submission_id,
-        principal_alias.alias_display AS state_modified_by_user_name
-    FROM base 
-    LEFT JOIN {{ ref('stg_synapse__principal_alias') }} principal_alias
-        ON base.state_modified_by = principal_alias.principal_id
-    where principal_alias.alias_type = 'USER_NAME'
+        principal_id,
+        alias_display
+    FROM {{ ref('stg_synapse__principal_alias') }}
+    WHERE alias_type = 'USER_NAME'
 )
 SELECT
     base.data_access_submission_id,
@@ -43,17 +32,17 @@ SELECT
     base.access_requirement_version,
     base.research_project_id,
     base.created_by,
-    created_by_names.created_by_user_name,
+    pa_created.alias_display AS created_by_user_name,
     base.created_on,
     base.state_modified_by,
-    state_modified_by_names.state_modified_by_user_name,
+    pa_modified.alias_display AS state_modified_by_user_name,
     base.state_modified_on,
     base.state,
     base.state_reason,
     base.accessor_changes,
     base.data_access_submission_raw
 FROM base
-LEFT JOIN created_by_names 
-    ON base.data_access_submission_id = created_by_names.data_access_submission_id
-LEFT JOIN state_modified_by_names 
-    ON base.data_access_submission_id = state_modified_by_names.data_access_submission_id
+LEFT JOIN principal_usernames pa_created
+    ON base.created_by = pa_created.principal_id
+LEFT JOIN principal_usernames pa_modified
+    ON base.state_modified_by = pa_modified.principal_id
