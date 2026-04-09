678dc32864321bf413a13ad080aaeccd0419cfc4
Jenny V Medina <jenny.medina@sagebase.org>
2026-04-09 09:29:45 -0400

    Change CREATE OR REPLACE to CREATE IF NOT EXISTS

--- admin/integrations.sql
+++ admin/integrations.sql
@@ -1,14 +1,14 @@
 USE ROLE ACCOUNTADMIN;
 
 -- Storage integrations (dev & prod) for 2026 Synapse RDS -> Snowflake pipeline (SNOW-392)
-CREATE OR REPLACE STORAGE INTEGRATION synapse_snapshots_dev
+CREATE STORAGE INTEGRATION IF NOT EXISTS synapse_snapshots_dev
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = 'S3'
   ENABLED = TRUE
   STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::766808016710:role/snowflake-role-synapse-snowflake-rds-snapshots-dev'
   STORAGE_ALLOWED_LOCATIONS = ('s3://synapse-snowflake-rds-snapshots-dev');
 
-CREATE OR REPLACE STORAGE INTEGRATION synapse_snapshots_prod
+CREATE STORAGE INTEGRATION IF NOT EXISTS synapse_snapshots_prod
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = 'S3'
   ENABLED = TRUE
