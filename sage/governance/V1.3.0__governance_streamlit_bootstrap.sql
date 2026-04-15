9eced7f867e13b1fb1d32a7f1d473c361cdc4f9f
Phil Snyder <subfivemile@gmail.com>
2026-04-15 14:44:41 -0700

    Assume schema admin role before creating Streamlit

--- sage/governance/V1.3.0__governance_streamlit_bootstrap.sql
+++ sage/governance/V1.3.0__governance_streamlit_bootstrap.sql
@@ -1,5 +1,9 @@
 USE SCHEMA {{ database_name }}.governance;
 
+-- STREAMLIT objects do not support (future) ownership grants
+-- We will need to create them as their intended owner
+USE ROLE SAGE_GOVERNANCE_ADMIN;
+
 -- This DDL is a formality:
 -- File population and Streamlit object lifecycle are managed by Snowflake CLI in CI.
 CREATE OR REPLACE STREAMLIT data_access_dashboard;
