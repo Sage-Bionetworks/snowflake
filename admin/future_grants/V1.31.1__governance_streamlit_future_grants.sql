9466c87bcedacf598fa1928d3b584efdb6e30f66
Phil Snyder <subfivemile@gmail.com>
2026-04-15 14:51:21 -0700

    [hotfix] do not omit future USAGE grant for STREAMLIT objects

---
+++ admin/future_grants/V1.31.1__governance_streamlit_future_grants.sql
@@ -0,0 +1,6 @@
+-- Future usage of Streamlit objects in SAGE.GOVERNANCE for the analyst role.
+-- Future ownership grants are not yet supported for Streamlit objects
+GRANT USAGE
+    ON FUTURE STREAMLITS
+    IN SCHEMA SAGE.GOVERNANCE
+    TO ROLE SAGE_GOVERNANCE_ANALYST;
