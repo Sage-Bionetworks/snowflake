02ec9e4bbf954f824fcd659f0ae9a24e2bed4aee
Phil Snyder <subfivemile@gmail.com>
2026-05-21 12:19:16 -0700

    [SNOW-460] Re-add schemachange_admin SNOW-460 migrations (#334)

---
+++ admin/future_grants/V1.32.13__grant_future_grants_upon_sage_ad.sql
@@ -0,0 +1,6 @@
+-- Future usage of Streamlit objects in SAGE.AD for the analyst role.
+-- Future ownership grants are not yet supported for Streamlit objects
+GRANT USAGE
+    ON FUTURE STREAMLITS
+    IN SCHEMA SAGE.AD
+    TO ROLE SAGE_AD_ANALYST;
