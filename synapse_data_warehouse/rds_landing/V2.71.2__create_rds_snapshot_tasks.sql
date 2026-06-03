e8c71971ad0d5ce1dbe53412f6d0853a7fb57b7f
Jenny Medina <medina.stsci@gmail.com>
2026-06-03 12:58:51 -0400

    unpack the parquet files in COPY INTO DDL based on CURRENT agreed-upon data structure for each record type

--- synapse_data_warehouse/rds_landing/V2.71.2__create_rds_snapshot_tasks.sql
+++ synapse_data_warehouse/rds_landing/V2.71.2__create_rds_snapshot_tasks.sql
@@ -33,114 +33,214 @@ CREATE OR REPLACE TASK COPY_ACCESS_APPROVAL_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO access_approval
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO access_approval FROM (
+        SELECT
+            $1:id::BIGINT                  AS id,
+            $1:requirement_id::BIGINT      AS requirement_id,
+            $1:requirement_version::BIGINT AS requirement_version,
+            $1:created_by::BIGINT          AS created_by,
+            $1:created_on::BIGINT          AS created_on,
+            $1:modified_by::BIGINT         AS modified_by,
+            $1:modified_on::BIGINT         AS modified_on,
+            $1:submitter_id::BIGINT        AS submitter_id,
+            $1:accessor_id::BIGINT         AS accessor_id,
+            $1:expired_on::BIGINT          AS expired_on,
+            $1:state::VARCHAR              AS state,
+            $1:etag::VARCHAR               AS etag
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     -- TODO: Finalize file naming convention with Platform and update regex pattern as needed.
     PATTERN = '.*ACCESS_APPROVAL/.*\.parquet$';
 CREATE OR REPLACE TASK COPY_ACL_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO acl
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO acl FROM (
+        SELECT
+            $1:id::BIGINT          AS id,
+            $1:owner_id::BIGINT    AS owner_id,
+            $1:owner_type::VARCHAR AS owner_type,
+            $1:created_on::BIGINT  AS created_on,
+            $1:etag::VARCHAR       AS etag
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     PATTERN = '.*[^_]ACL/.*\.parquet$';
 CREATE OR REPLACE TASK COPY_ACL_RESOURCE_ACCESS_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO acl_resource_access
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO acl_resource_access FROM (
+        SELECT
+            $1:id::BIGINT       AS id,
+            $1:owner_id::BIGINT AS owner_id,
+            $1:group_id::BIGINT AS group_id
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     PATTERN = '.*ACL_RESOURCE_ACCESS/.*\.parquet$';
 CREATE OR REPLACE TASK COPY_ACL_RESOURCE_ACCESS_TYPE_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO acl_resource_access_type
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO acl_resource_access_type FROM (
+        SELECT
+            $1:id_oid::BIGINT      AS id_oid,
+            $1:string_ele::VARCHAR AS string_ele,
+            $1:owner_id::BIGINT    AS owner_id
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     PATTERN = '.*ACL_RESOURCE_ACCESS_TYPE/.*\.parquet$';
 CREATE OR REPLACE TASK COPY_DATA_ACCESS_SUBMISSION_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO data_access_submission
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO data_access_submission FROM (
+        SELECT
+            $1:id::BIGINT                         AS id,
+            $1:access_requirement_id::BIGINT      AS access_requirement_id,
+            $1:data_access_request_id::BIGINT     AS data_access_request_id,
+            $1:research_project_id::BIGINT        AS research_project_id,
+            $1:created_by::BIGINT                 AS created_by,
+            $1:created_on::BIGINT                 AS created_on,
+            $1:access_requirement_version::BIGINT AS access_requirement_version,
+            $1:etag::VARCHAR                      AS etag,
+            $1:submission_serialized::BINARY      AS submission_serialized
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     PATTERN = '.*DATA_ACCESS_SUBMISSION/.*\.parquet$';
 CREATE OR REPLACE TASK COPY_DATA_ACCESS_SUBMISSION_ACCESSOR_CHANGES_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO data_access_submission_accessor_changes
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO data_access_submission_accessor_changes FROM (
+        SELECT
+            $1:submission_id::BIGINT AS submission_id,
+            $1:accessor_id::BIGINT   AS accessor_id,
+            $1:access_type::VARCHAR  AS access_type
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     PATTERN = '.*DATA_ACCESS_SUBMISSION_ACCESSOR_CHANGES/.*\.parquet$';
 CREATE OR REPLACE TASK COPY_DATA_ACCESS_SUBMISSION_STATUS_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO data_access_submission_status
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO data_access_submission_status FROM (
+        SELECT
+            $1:submission_id::BIGINT AS submission_id,
+            $1:created_by::BIGINT    AS created_by,
+            $1:created_on::BIGINT    AS created_on,
+            $1:modified_by::BIGINT   AS modified_by,
+            $1:modified_on::BIGINT   AS modified_on,
+            $1:state::VARCHAR        AS state,
+            $1:reason::BINARY        AS reason
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     PATTERN = '.*DATA_ACCESS_SUBMISSION_STATUS/.*\.parquet$';
 CREATE OR REPLACE TASK COPY_DATA_ACCESS_SUBMISSION_SUBMITTER_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO data_access_submission_submitter
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO data_access_submission_submitter FROM (
+        SELECT
+            $1:id::BIGINT                    AS id,
+            $1:access_requirement_id::BIGINT AS access_requirement_id,
+            $1:submitter_id::BIGINT          AS submitter_id,
+            $1:current_submission_id::BIGINT AS current_submission_id,
+            $1:etag::VARCHAR                 AS etag
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     PATTERN = '.*DATA_ACCESS_SUBMISSION_SUBMITTER/.*\.parquet$';
 CREATE OR REPLACE TASK COPY_DATA_ACCESS_REQUEST_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO data_access_request
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO data_access_request FROM (
+        SELECT
+            $1:id::BIGINT                    AS id,
+            $1:access_requirement_id::BIGINT AS access_requirement_id,
+            $1:research_project_id::BIGINT   AS research_project_id,
+            $1:created_by::BIGINT            AS created_by,
+            $1:created_on::BIGINT            AS created_on,
+            $1:modified_by::BIGINT           AS modified_by,
+            $1:modified_on::BIGINT           AS modified_on,
+            $1:etag::VARCHAR                 AS etag,
+            $1:request_serialized::BINARY    AS request_serialized
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     PATTERN = '.*DATA_ACCESS_REQUEST/.*\.parquet$';
 CREATE OR REPLACE TASK COPY_ACCESS_REQUIREMENT_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO access_requirement
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO access_requirement FROM (
+        SELECT
+            $1:id::BIGINT                  AS id,
+            $1:name::VARCHAR               AS name,
+            $1:concrete_type::VARCHAR      AS concrete_type,
+            $1:created_by::BIGINT          AS created_by,
+            $1:created_on::BIGINT          AS created_on,
+            $1:current_rev_num::BIGINT     AS current_rev_num,
+            $1:is_two_fa_required::BOOLEAN AS is_two_fa_required,
+            $1:access_type::VARCHAR        AS access_type,
+            $1:etag::VARCHAR               AS etag
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     PATTERN = '.*ACCESS_REQUIREMENT/.*\.parquet$';
 CREATE OR REPLACE TASK COPY_ACCESS_REQUIREMENT_PROJECT_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO access_requirement_project
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO access_requirement_project FROM (
+        SELECT
+            $1:ar_id::BIGINT      AS ar_id,
+            $1:project_id::BIGINT AS project_id
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     PATTERN = '.*ACCESS_REQUIREMENT_PROJECT/.*\.parquet$';
 CREATE OR REPLACE TASK COPY_ACCESS_REQUIREMENT_REVISION_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO access_requirement_revision
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO access_requirement_revision FROM (
+        SELECT
+            $1:owner_id::BIGINT          AS owner_id,
+            $1:number::BIGINT            AS number,
+            $1:modified_by::BIGINT       AS modified_by,
+            $1:modified_on::BIGINT       AS modified_on,
+            $1:serialized_entity::BINARY AS serialized_entity
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     PATTERN = '.*ACCESS_REQUIREMENT_REVISION/.*\.parquet$';
 CREATE OR REPLACE TASK COPY_DATA_ACCESS_NOTIFICATION_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO data_access_notification
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO data_access_notification FROM (
+        SELECT
+            $1:id::BIGINT                 AS id,
+            $1:notification_type::VARCHAR AS notification_type,
+            $1:requirement_id::BIGINT     AS requirement_id,
+            $1:recipient_id::BIGINT       AS recipient_id,
+            $1:access_approval_id::BIGINT AS access_approval_id,
+            $1:sent_on::TIMESTAMP_NTZ(9)  AS sent_on,
+            $1:message_id::BIGINT         AS message_id,
+            $1:etag::VARCHAR              AS etag
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     PATTERN = '.*DATA_ACCESS_NOTIFICATION/.*\.parquet$';
 CREATE OR REPLACE TASK COPY_PRINCIPAL_ALIAS_TASK
     WAREHOUSE = compute_xsmall
     AFTER PROXY_TASK_A
 AS
-    COPY INTO principal_alias
-    FROM @RDS_SNAPSHOTS_STAGE
-    FILE_FORMAT = (TYPE = PARQUET BINARY_AS_TEXT = FALSE)
+    COPY INTO principal_alias FROM (
+        SELECT
+            $1:id::BIGINT             AS id,
+            $1:principal_id::BIGINT   AS principal_id,
+            $1:alias_unique::VARCHAR  AS alias_unique,
+            $1:alias_display::VARCHAR AS alias_display,
+            $1:type::VARCHAR          AS type,
+            $1:etag::VARCHAR          AS etag
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot
+    )
     PATTERN = '.*PRINCIPAL_ALIAS/.*\.parquet$';
 
 -- ============================================================
