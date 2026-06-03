54b7433a03d3e3211373a58be212d7791eb258ed
Jenny Medina <medina.stsci@gmail.com>
2026-06-03 14:16:29 -0400

    retrieve root task and timestamp from task_runtime_info func

--- synapse_data_warehouse/rds_landing/V2.71.3__create_rds_snapshot_finalizer_task.sql
+++ synapse_data_warehouse/rds_landing/V2.71.3__create_rds_snapshot_finalizer_task.sql
@@ -1,15 +1,17 @@
 use schema {{database_name}}.rds_landing; --noqa: JJ01,PRS,TMP
 
 -- Suspend root before modifying the graph.
-alter task {{database_name}}.rds_landing.refresh_stage_task suspend; --noqa: TMP
+alter task refresh_stage_task suspend; --noqa: TMP
 
 -- Create the finalizer task.
-create or replace task {{database_name}}.rds_landing.rds_snapshot_finalizer_task --noqa: TMP
+create or replace task rds_snapshot_finalizer_task --noqa: TMP
     warehouse = 'COMPUTE_XSMALL'
-    finalize = '{{database_name}}.rds_landing.refresh_stage_task' --noqa: TMP
+    finalize = 'refresh_stage_task' --noqa: TMP
 as
 declare
     v_graph_status  varchar;
+    v_root_task_id  varchar;
+    v_start_time    timestamp_ltz;
     v_loaded        integer default 0;
     v_failed        integer default 0;
     v_total_rows    integer default 0;
@@ -17,10 +19,27 @@ declare
     v_run_date      varchar;
     v_message       varchar;
 begin
-    let graph_json variant := parse_json(
-        system$get_task_graph_return_value('{{database_name}}.rds_landing.refresh_stage_task') --noqa: TMP
+    v_root_task_id := (
+        select system$task_runtime_info('CURRENT_ROOT_TASK_UUID')
     );
-    v_graph_status := graph_json:state::varchar;
+    v_start_time := (
+        select system$task_runtime_info('CURRENT_TASK_GRAPH_ORIGINAL_SCHEDULED_TIMESTAMP')::timestamp_ltz
+    );
+
+    select
+        case
+            when count_if(upper(state) like 'FAIL%') > 0 then 'failed'
+            when count_if(upper(state) like 'CANCEL%') > 0 then 'cancelled'
+            when count_if(upper(state) like 'SUCCEED%') > 0 then 'succeeded'
+            else 'unknown'
+        end
+    into :v_graph_status
+    from table(information_schema.task_history(
+        root_task_id => :v_root_task_id,
+        scheduled_time_range_start => :v_start_time,
+        scheduled_time_range_end => current_timestamp()
+    ));
+
     v_run_date     := to_varchar(current_date(), 'MM/DD/YYYY');
 
     if (v_graph_status != 'succeeded') then
@@ -58,7 +77,7 @@ begin
     return :v_message;
 end;
 
-alter task {{database_name}}.rds_landing.rds_snapshot_finalizer_task resume; --noqa: TMP
+alter task rds_snapshot_finalizer_task resume; --noqa: TMP
 
 -- Re-enable the full RDS snapshot task graph from the root.
-select system$task_dependents_enable('{{database_name}}.rds_landing.refresh_stage_task'); --noqa: JJ01,PRS,TMP
+select system$task_dependents_enable('refresh_stage_task'); --noqa: JJ01,PRS,TMP
