d8217851b5f139758a0b4519925b011d5cde599b
Jenny Medina <medina.stsci@gmail.com>
2026-06-11 15:29:53 -0400

    rds-snapshot -> rds-snapshot-lambda

--- synapse_data_warehouse/rds_landing/V2.71.2__create_rds_snapshot_tasks.sql
+++ synapse_data_warehouse/rds_landing/V2.71.2__create_rds_snapshot_tasks.sql
@@ -47,7 +47,7 @@ AS
             $1:EXPIRED_ON::BIGINT          AS expired_on,
             $1:STATE::VARCHAR              AS state,
             $1:ETAG::VARCHAR               AS etag
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACCESS_APPROVAL
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/ACCESS_APPROVAL
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 CREATE OR REPLACE TASK COPY_ACL_TASK
@@ -61,7 +61,7 @@ AS
             $1:OWNER_TYPE::VARCHAR AS owner_type,
             $1:CREATED_ON::BIGINT  AS created_on,
             $1:ETAG::VARCHAR       AS etag
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACL
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/ACL
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 CREATE OR REPLACE TASK COPY_ACL_RESOURCE_ACCESS_TASK
@@ -73,7 +73,7 @@ AS
             $1:ID::BIGINT       AS id,
             $1:OWNER_ID::BIGINT AS owner_id,
             $1:GROUP_ID::BIGINT AS group_id
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACL_RESOURCE_ACCESS
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/ACL_RESOURCE_ACCESS
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 CREATE OR REPLACE TASK COPY_ACL_RESOURCE_ACCESS_TYPE_TASK
@@ -85,7 +85,7 @@ AS
             $1:ID_OID::BIGINT      AS id_oid,
             $1:STRING_ELE::VARCHAR AS string_ele,
             $1:OWNER_ID::BIGINT    AS owner_id
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACL_RESOURCE_ACCESS_TYPE
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/ACL_RESOURCE_ACCESS_TYPE
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 CREATE OR REPLACE TASK COPY_DATA_ACCESS_SUBMISSION_TASK
@@ -103,7 +103,7 @@ AS
             $1:ACCESS_REQUIREMENT_VERSION::BIGINT AS access_requirement_version,
             $1:ETAG::VARCHAR                      AS etag,
             $1:SUBMISSION_SERIALIZED::BINARY      AS submission_serialized
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/DATA_ACCESS_SUBMISSION
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/DATA_ACCESS_SUBMISSION
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 CREATE OR REPLACE TASK COPY_DATA_ACCESS_SUBMISSION_ACCESSOR_CHANGES_TASK
@@ -115,7 +115,7 @@ AS
             $1:SUBMISSION_ID::BIGINT AS submission_id,
             $1:ACCESSOR_ID::BIGINT   AS accessor_id,
             $1:ACCESS_TYPE::VARCHAR  AS access_type
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/DATA_ACCESS_SUBMISSION_ACCESSOR_CHANGES
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/DATA_ACCESS_SUBMISSION_ACCESSOR_CHANGES
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 CREATE OR REPLACE TASK COPY_DATA_ACCESS_SUBMISSION_STATUS_TASK
@@ -131,7 +131,7 @@ AS
             $1:MODIFIED_ON::BIGINT   AS modified_on,
             $1:STATE::VARCHAR        AS state,
             $1:REASON::BINARY        AS reason
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/DATA_ACCESS_SUBMISSION_STATUS
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/DATA_ACCESS_SUBMISSION_STATUS
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 CREATE OR REPLACE TASK COPY_DATA_ACCESS_SUBMISSION_SUBMITTER_TASK
@@ -145,7 +145,7 @@ AS
             $1:SUBMITTER_ID::BIGINT          AS submitter_id,
             $1:CURRENT_SUBMISSION_ID::BIGINT AS current_submission_id,
             $1:ETAG::VARCHAR                 AS etag
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/DATA_ACCESS_SUBMISSION_SUBMITTER
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/DATA_ACCESS_SUBMISSION_SUBMITTER
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 CREATE OR REPLACE TASK COPY_DATA_ACCESS_REQUEST_TASK
@@ -163,7 +163,7 @@ AS
             $1:MODIFIED_ON::BIGINT           AS modified_on,
             $1:ETAG::VARCHAR                 AS etag,
             $1:REQUEST_SERIALIZED::BINARY    AS request_serialized
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/DATA_ACCESS_REQUEST
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/DATA_ACCESS_REQUEST
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 CREATE OR REPLACE TASK COPY_ACCESS_REQUIREMENT_TASK
@@ -181,7 +181,7 @@ AS
             $1:IS_TWO_FA_REQUIRED::BOOLEAN AS is_two_fa_required,
             $1:ACCESS_TYPE::VARCHAR        AS access_type,
             $1:ETAG::VARCHAR               AS etag
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACCESS_REQUIREMENT
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/ACCESS_REQUIREMENT
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 CREATE OR REPLACE TASK COPY_ACCESS_REQUIREMENT_PROJECT_TASK
@@ -192,7 +192,7 @@ AS
         SELECT
             $1:AR_ID::BIGINT      AS ar_id,
             $1:PROJECT_ID::BIGINT AS project_id
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACCESS_REQUIREMENT_PROJECT
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/ACCESS_REQUIREMENT_PROJECT
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 CREATE OR REPLACE TASK COPY_ACCESS_REQUIREMENT_REVISION_TASK
@@ -206,7 +206,7 @@ AS
             $1:MODIFIED_BY::BIGINT       AS modified_by,
             $1:MODIFIED_ON::BIGINT       AS modified_on,
             $1:SERIALIZED_ENTITY::BINARY AS serialized_entity
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACCESS_REQUIREMENT_REVISION
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/ACCESS_REQUIREMENT_REVISION
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 CREATE OR REPLACE TASK COPY_DATA_ACCESS_NOTIFICATION_TASK
@@ -223,7 +223,7 @@ AS
             $1:SENT_ON::TIMESTAMP_NTZ(9)  AS sent_on,
             $1:MESSAGE_ID::BIGINT         AS message_id,
             $1:ETAG::VARCHAR              AS etag
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/DATA_ACCESS_NOTIFICATION
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/DATA_ACCESS_NOTIFICATION
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 CREATE OR REPLACE TASK COPY_PRINCIPAL_ALIAS_TASK
@@ -238,7 +238,7 @@ AS
             $1:ALIAS_DISPLAY::VARCHAR AS alias_display,
             $1:TYPE::VARCHAR          AS type,
             $1:ETAG::VARCHAR          AS etag
-        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/PRINCIPAL_ALIAS
+        FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot-lambda/PRINCIPAL_ALIAS
     )
     PATTERN = '.*/[0-9]+/[0-9]{4}-[0-9]{2}-[0-9]{2}/.*\.gz\.parquet';
 
