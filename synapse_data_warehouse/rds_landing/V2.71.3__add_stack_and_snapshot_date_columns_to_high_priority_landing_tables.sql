97f22546cdaed92b80a21cf1e3eceec90fc9d49a
Jenny Medina <medina.stsci@gmail.com>
2026-06-18 14:28:02 -0400

    new column: filename. autopopulate stack, snapshot_date, filename based on filename metadata

--- synapse_data_warehouse/rds_landing/V2.71.3__add_stack_and_snapshot_date_columns_to_high_priority_landing_tables.sql
+++ synapse_data_warehouse/rds_landing/V2.71.3__add_stack_and_snapshot_date_columns_to_high_priority_landing_tables.sql
@@ -1,43 +1,57 @@
 USE SCHEMA {{database_name}}.RDS_LANDING; --noqa: JJ01,PRS,TMP
 
-ALTER TABLE access_approval ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE access_approval ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE access_approval ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE access_approval ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
 
-ALTER TABLE acl ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE acl ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE acl ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE acl ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
 
-ALTER TABLE acl_resource_access ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE acl_resource_access ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE acl_resource_access ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE acl_resource_access ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
 
-ALTER TABLE acl_resource_access_type ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE acl_resource_access_type ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE acl_resource_access_type ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE acl_resource_access_type ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
 
-ALTER TABLE data_access_submission ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE data_access_submission ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE data_access_submission ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE data_access_submission ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
 
-ALTER TABLE data_access_submission_accessor_changes ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE data_access_submission_accessor_changes ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE data_access_submission_accessor_changes ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE data_access_submission_accessor_changes ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
 
-ALTER TABLE data_access_submission_status ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE data_access_submission_status ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE data_access_submission_status ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE data_access_submission_status ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
 
-ALTER TABLE data_access_submission_submitter ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE data_access_submission_submitter ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE data_access_submission_submitter ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE data_access_submission_submitter ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
 
-ALTER TABLE data_access_request ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE data_access_request ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE data_access_request ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE data_access_request ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
 
-ALTER TABLE access_requirement ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE access_requirement ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE access_requirement ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE access_requirement ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
 
-ALTER TABLE access_requirement_project ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE access_requirement_project ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE access_requirement_project ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE access_requirement_project ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
 
-ALTER TABLE access_requirement_revision ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE access_requirement_revision ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE access_requirement_revision ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE access_requirement_revision ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
 
-ALTER TABLE data_access_notification ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE data_access_notification ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE data_access_notification ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE data_access_notification ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
 
-ALTER TABLE principal_alias ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE principal_alias ADD COLUMN stack INTEGER COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
 ALTER TABLE principal_alias ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE principal_alias ADD COLUMN filename VARCHAR COMMENT 'The S3 path of the parquet file from which this record was loaded';
--- synapse_data_warehouse/rds_landing/V2.71.4__create_rds_snapshot_tasks.sql
+++ synapse_data_warehouse/rds_landing/V2.71.4__create_rds_snapshot_tasks.sql
@@ -45,7 +45,10 @@ AS
             $1:ACCESSOR_ID::BIGINT         AS accessor_id,
             $1:EXPIRED_ON::BIGINT          AS expired_on,
             $1:STATE::VARCHAR              AS state,
-            $1:ETAG::VARCHAR               AS etag
+            $1:ETAG::VARCHAR               AS etag,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACCESS_APPROVAL/
     )
     PATTERN = '.*\/[0-9]+\/[0-9]{4}-[0-9]{2}-[0-9]{2}\/.*\.gz\.parquet';
@@ -59,7 +62,10 @@ AS
             $1:OWNER_ID::BIGINT    AS owner_id,
             $1:OWNER_TYPE::VARCHAR AS owner_type,
             $1:CREATED_ON::BIGINT  AS created_on,
-            $1:ETAG::VARCHAR       AS etag
+            $1:ETAG::VARCHAR       AS etag,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACL/
     )
     PATTERN = '.*\/[0-9]+\/[0-9]{4}-[0-9]{2}-[0-9]{2}\/.*\.gz\.parquet';
@@ -71,7 +77,10 @@ AS
         SELECT
             $1:ID::BIGINT       AS id,
             $1:OWNER_ID::BIGINT AS owner_id,
-            $1:GROUP_ID::BIGINT AS group_id
+            $1:GROUP_ID::BIGINT AS group_id,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACL_RESOURCE_ACCESS/
     )
     PATTERN = '.*\/[0-9]+\/[0-9]{4}-[0-9]{2}-[0-9]{2}\/.*\.gz\.parquet';
@@ -83,7 +92,10 @@ AS
         SELECT
             $1:ID_OID::BIGINT      AS id_oid,
             $1:STRING_ELE::VARCHAR AS string_ele,
-            $1:OWNER_ID::BIGINT    AS owner_id
+            $1:OWNER_ID::BIGINT    AS owner_id,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACL_RESOURCE_ACCESS_TYPE/
     )
     PATTERN = '.*\/[0-9]+\/[0-9]{4}-[0-9]{2}-[0-9]{2}\/.*\.gz\.parquet';
@@ -101,7 +113,10 @@ AS
             $1:CREATED_ON::BIGINT                 AS created_on,
             $1:ACCESS_REQUIREMENT_VERSION::BIGINT AS access_requirement_version,
             $1:ETAG::VARCHAR                      AS etag,
-            $1:SUBMISSION_SERIALIZED::BINARY      AS submission_serialized
+            $1:SUBMISSION_SERIALIZED::BINARY      AS submission_serialized,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/DATA_ACCESS_SUBMISSION/
     )
     -- The parquet files for this data type will contain one or more binary columns
@@ -116,7 +131,10 @@ AS
         SELECT
             $1:SUBMISSION_ID::BIGINT AS submission_id,
             $1:ACCESSOR_ID::BIGINT   AS accessor_id,
-            $1:ACCESS_TYPE::VARCHAR  AS access_type
+            $1:ACCESS_TYPE::VARCHAR  AS access_type,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/DATA_ACCESS_SUBMISSION_ACCESSOR_CHANGES/
     )
     PATTERN = '.*\/[0-9]+\/[0-9]{4}-[0-9]{2}-[0-9]{2}\/.*\.gz\.parquet';
@@ -132,7 +150,10 @@ AS
             $1:MODIFIED_BY::BIGINT   AS modified_by,
             $1:MODIFIED_ON::BIGINT   AS modified_on,
             $1:STATE::VARCHAR        AS state,
-            $1:REASON::BINARY        AS reason
+            $1:REASON::BINARY        AS reason,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/DATA_ACCESS_SUBMISSION_STATUS/
     )
     -- The parquet files for this data type will contain one or more binary columns
@@ -149,7 +170,10 @@ AS
             $1:ACCESS_REQUIREMENT_ID::BIGINT AS access_requirement_id,
             $1:SUBMITTER_ID::BIGINT          AS submitter_id,
             $1:CURRENT_SUBMISSION_ID::BIGINT AS current_submission_id,
-            $1:ETAG::VARCHAR                 AS etag
+            $1:ETAG::VARCHAR                 AS etag,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/DATA_ACCESS_SUBMISSION_SUBMITTER/
     )
     PATTERN = '.*\/[0-9]+\/[0-9]{4}-[0-9]{2}-[0-9]{2}\/.*\.gz\.parquet';
@@ -167,7 +191,10 @@ AS
             $1:MODIFIED_BY::BIGINT           AS modified_by,
             $1:MODIFIED_ON::BIGINT           AS modified_on,
             $1:ETAG::VARCHAR                 AS etag,
-            $1:REQUEST_SERIALIZED::BINARY    AS request_serialized
+            $1:REQUEST_SERIALIZED::BINARY    AS request_serialized,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/DATA_ACCESS_REQUEST/
     )
     -- The parquet files for this data type will contain one or more binary columns
@@ -186,9 +213,12 @@ AS
             $1:CREATED_BY::BIGINT          AS created_by,
             $1:CREATED_ON::BIGINT          AS created_on,
             $1:CURRENT_REV_NUM::BIGINT     AS current_rev_num,
-            $1:IS_TWO_FA_REQUIRED::BIGINT AS is_two_fa_required,
+            $1:IS_TWO_FA_REQUIRED::BIGINT  AS is_two_fa_required,
             $1:ACCESS_TYPE::VARCHAR        AS access_type,
-            $1:ETAG::VARCHAR               AS etag
+            $1:ETAG::VARCHAR               AS etag,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACCESS_REQUIREMENT/
     )
     PATTERN = '.*\/[0-9]+\/[0-9]{4}-[0-9]{2}-[0-9]{2}\/.*\.gz\.parquet';
@@ -199,7 +229,10 @@ AS
     COPY INTO access_requirement_project FROM (
         SELECT
             $1:AR_ID::BIGINT      AS ar_id,
-            $1:PROJECT_ID::BIGINT AS project_id
+            $1:PROJECT_ID::BIGINT AS project_id,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACCESS_REQUIREMENT_PROJECT/
     )
     PATTERN = '.*\/[0-9]+\/[0-9]{4}-[0-9]{2}-[0-9]{2}\/.*\.gz\.parquet';
@@ -213,7 +246,10 @@ AS
             $1:NUMBER::BIGINT            AS number,
             $1:MODIFIED_BY::BIGINT       AS modified_by,
             $1:MODIFIED_ON::BIGINT       AS modified_on,
-            $1:SERIALIZED_ENTITY::BINARY AS serialized_entity
+            $1:SERIALIZED_ENTITY::BINARY AS serialized_entity,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/ACCESS_REQUIREMENT_REVISION/
     )
     -- The parquet files for this data type will contain one or more binary columns
@@ -233,7 +269,10 @@ AS
             $1:ACCESS_APPROVAL_ID::BIGINT AS access_approval_id,
             $1:SENT_ON::TIMESTAMP_NTZ(9)  AS sent_on,
             $1:MESSAGE_ID::BIGINT         AS message_id,
-            $1:ETAG::VARCHAR              AS etag
+            $1:ETAG::VARCHAR              AS etag,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/DATA_ACCESS_NOTIFICATION/
     )
     PATTERN = '.*\/[0-9]+\/[0-9]{4}-[0-9]{2}-[0-9]{2}\/.*\.gz\.parquet';
@@ -248,7 +287,10 @@ AS
             $1:ALIAS_UNIQUE::VARCHAR  AS alias_unique,
             $1:ALIAS_DISPLAY::VARCHAR AS alias_display,
             $1:TYPE::VARCHAR          AS type,
-            $1:ETAG::VARCHAR          AS etag
+            $1:ETAG::VARCHAR          AS etag,
+            SPLIT_PART(METADATA$FILENAME, '/', 3)::BIGINT AS stack,
+            SPLIT_PART(METADATA$FILENAME, '/', 4)::DATE    AS snapshot_date,
+            METADATA$FILENAME                              AS filename
         FROM @RDS_SNAPSHOTS_STAGE/rds-snapshot/PRINCIPAL_ALIAS/
     )
     PATTERN = '.*\/[0-9]+\/[0-9]{4}-[0-9]{2}-[0-9]{2}\/.*\.gz\.parquet';
