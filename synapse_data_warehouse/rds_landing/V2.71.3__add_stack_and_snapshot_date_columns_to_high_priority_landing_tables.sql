7ca891ca29d9c2ad4f45abca0784843cc18e7183
Jenny Medina <medina.stsci@gmail.com>
2026-06-18 13:59:45 -0400

    update ALTER TABLE ddl

--- synapse_data_warehouse/rds_landing/V2.71.3__add_stack_and_snapshot_date_columns_to_high_priority_landing_tables.sql
+++ synapse_data_warehouse/rds_landing/V2.71.3__add_stack_and_snapshot_date_columns_to_high_priority_landing_tables.sql
@@ -1,57 +1,43 @@
 USE SCHEMA {{database_name}}.RDS_LANDING; --noqa: JJ01,PRS,TMP
 
-ALTER TABLE access_approval
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE access_approval ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE access_approval ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
 
-ALTER TABLE acl
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE acl ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE acl ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
 
-ALTER TABLE acl_resource_access
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE acl_resource_access ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE acl_resource_access ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
 
-ALTER TABLE acl_resource_access_type
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE acl_resource_access_type ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE acl_resource_access_type ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
 
-ALTER TABLE data_access_submission
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE data_access_submission ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE data_access_submission ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
 
-ALTER TABLE data_access_submission_accessor_changes
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE data_access_submission_accessor_changes ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE data_access_submission_accessor_changes ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
 
-ALTER TABLE data_access_submission_status
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE data_access_submission_status ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE data_access_submission_status ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
 
-ALTER TABLE data_access_submission_submitter
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE data_access_submission_submitter ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE data_access_submission_submitter ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
 
-ALTER TABLE data_access_request
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE data_access_request ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE data_access_request ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
 
-ALTER TABLE access_requirement
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE access_requirement ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE access_requirement ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
 
-ALTER TABLE access_requirement_project
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE access_requirement_project ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE access_requirement_project ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
 
-ALTER TABLE access_requirement_revision
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE access_requirement_revision ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE access_requirement_revision ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
 
-ALTER TABLE data_access_notification
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE data_access_notification ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE data_access_notification ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
 
-ALTER TABLE principal_alias
-    ADD COLUMN stack         VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken',
-    ADD COLUMN snapshot_date DATE    COMMENT 'The date on which the RDS snapshot was taken';
+ALTER TABLE principal_alias ADD COLUMN stack VARCHAR COMMENT 'The Synapse stack number from which this RDS snapshot was taken';
+ALTER TABLE principal_alias ADD COLUMN snapshot_date DATE COMMENT 'The date on which the RDS snapshot was taken';
