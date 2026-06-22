218c548bcd95f33dbddbdfb2e1370a0432b70e75
Jenny Medina <medina.stsci@gmail.com>
2026-06-22 14:59:23 -0400

    caps lock

--- synapse_data_warehouse/rds_landing/V2.71.7__update_tasks_rbac.sql
+++ synapse_data_warehouse/rds_landing/V2.71.7__update_tasks_rbac.sql
@@ -1,7 +1,7 @@
 use database {{database_name}}; --noqa: JJ01,PRS,TMP
 
 -- Grant MONITOR privilege on all existing RDS_LANDING tasks to the task read database role.
-grant monitor
+grant MONITOR
     on all tasks
-    in schema rds_landing
-    to database role rds_landing_task_read;
+    in schema RDS_LANDING
+    to database role RDS_LANDING_TASK_READ;
