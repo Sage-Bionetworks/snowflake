619b3553c15a4fe95ec7f33123e560fb89342fc9
Phil Snyder <subfivemile@gmail.com>
2026-07-20 11:24:01 -0700

    Remove pretty-printed comment banners from RDS_RAW masked future grants script

--- admin/future_grants/V1.37.0__rds_raw_future_tables_masked_read.sql
+++ admin/future_grants/V1.37.0__rds_raw_future_tables_masked_read.sql
@@ -2,18 +2,11 @@
 -- and are following the RBAC design pattern for schemas documented here:
 -- https://sagebionetworks.jira.com/wiki/spaces/DPE/pages/4115366084/Managing+Object+Privileges#Schemas
 
------------------------------------------------------------------------------------------------------------------
--- Grant read access on future tables in PROD database to the masked read access database role:
------------------------------------------------------------------------------------------------------------------
 GRANT SELECT, REFERENCES
 	ON FUTURE TABLES
 	IN SCHEMA SYNAPSE_DATA_WAREHOUSE.RDS_RAW
 	TO DATABASE ROLE SYNAPSE_DATA_WAREHOUSE.RDS_RAW_TABLE_READ_MASKED;
 
-
------------------------------------------------------------------------------------------------------------------
--- Grant read access on future tables in DEV database to the masked read access database role:
------------------------------------------------------------------------------------------------------------------
 GRANT SELECT, REFERENCES
 	ON FUTURE TABLES
 	IN SCHEMA SYNAPSE_DATA_WAREHOUSE_DEV.RDS_RAW
