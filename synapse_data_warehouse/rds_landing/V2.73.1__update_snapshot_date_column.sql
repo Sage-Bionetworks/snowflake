f7e57f5da37df7fc0b9a2a916a4651e05bb8ed0b
Jenny Medina <medina.stsci@gmail.com>
2026-07-22 22:37:16 -0400

    update RDS_LANDING tables documenting the timezone that snapshot_date is derived from

---
+++ synapse_data_warehouse/rds_landing/V2.73.1__update_snapshot_date_column.sql
@@ -0,0 +1,16 @@
+USE SCHEMA {{database_name}}.RDS_LANDING; --noqa: JJ01,PRS,TMP
+
+ALTER TABLE access_approval ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
+ALTER TABLE acl ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
+ALTER TABLE acl_resource_access ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
+ALTER TABLE acl_resource_access_type ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
+ALTER TABLE data_access_submission ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
+ALTER TABLE data_access_submission_accessor_changes ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
+ALTER TABLE data_access_submission_status ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
+ALTER TABLE data_access_submission_submitter ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
+ALTER TABLE data_access_request ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
+ALTER TABLE access_requirement ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
+ALTER TABLE access_requirement_project ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
+ALTER TABLE access_requirement_revision ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
+ALTER TABLE data_access_notification ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
+ALTER TABLE principal_alias ADD COLUMN snapshot_date DATE COMMENT 'Date the RDS snapshot was taken (in UTC)';
