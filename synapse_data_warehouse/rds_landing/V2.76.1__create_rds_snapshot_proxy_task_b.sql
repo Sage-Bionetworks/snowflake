84044d319a7897ee1458ff5959833a0f91197cae
Phil Snyder <subfivemile@gmail.com>
2026-09-03 14:12:34 -0700

    [hotfix] suspend root task before updating graph

--- synapse_data_warehouse/rds_landing/V2.76.1__create_rds_snapshot_proxy_task_b.sql
+++ synapse_data_warehouse/rds_landing/V2.76.1__create_rds_snapshot_proxy_task_b.sql
@@ -1,5 +1,9 @@
 USE SCHEMA {{database_name}}.RDS_LANDING; --noqa: JJ01,PRS,TMP
 
+-- Adding a task to this graph requires the root task to be suspended first;
+-- re-enabled via SYSTEM$TASK_DEPENDENTS_ENABLE at the end of V2.76.3.
+ALTER TASK REFRESH_RDS_SNAPSHOTS_STAGE_TASK SUSPEND;
+
 CREATE OR REPLACE TASK PROXY_TASK_B
     COMMENT = 'No-op intermediary task. Snowflake limits a single node to 100 child tasks; this proxy fans out to the remaining COPY INTO tasks that do not fit under PROXY_TASK_A.'
     AFTER REFRESH_RDS_SNAPSHOTS_STAGE_TASK
